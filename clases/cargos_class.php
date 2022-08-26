cd2dd9dc7e7854bdb6693929296e0161d0febc21
Josue de la Cruz <arellanox0392@gmail.com>
2022-08-26 14:28:46 -0500

    Actualización cargos class
    Se implementan todos los métodos de master class

--- clases/cargos_class.php
+++ clases/cargos_class.php
@@ -13,15 +13,21 @@ class Cargos extends Master {
     public $intergers;
     public $strings;
     public $doubles;
+    public $validator_update;
+    public $interges_update;
+    public $strings_update;
+    public $doubles_update;
 
     function Cargos(){
         $this->tabla = "cargos";
         $this->master = new Master();
         $this->public_attributes = 3;
-        $this->validator = "s";
         $this->intergers = array();
         $this->strings = array(0);
         $this->doubles = array();
+        $this->intergers_update = array(1);
+        $this->strings_update = array(0);
+        $this->doubles_update = array();
     }
 
     function getAttributes(){
@@ -40,7 +46,7 @@ class Cargos extends Master {
 
     //esta funcion recibe un arreglo de datos
     function insert($values){
-        $return = $this->master->insert($this->tabla,$this->getAttributes(),$values,$this->validator,$this->intergers,$this->strings,$this->doubles);
+        $return = $this->master->insert($this->tabla,$this->getAttributes(),$values,$this->intergers,$this->strings,$this->doubles);
         return $return;
     }
 
@@ -52,7 +58,20 @@ class Cargos extends Master {
 
     // esta funcion recibe solo el id del cargo (int)
     function getById($id){
-        
+        $return = $this->master->getById($this->tabla,$id,$this->getAttributes());
+        return $return;
+    }
+
+    //recibe un array de datos con el id al final
+    function update($values){
+        $return = $this->master->update($this->tabla,$this->getAttributes(),$values,$this->intergers_update,$this->strings_update,$this->doubles_update);
+        return $return;
+    }
+
+    //recibe unicamente el id del registro que se va a eliminar
+    function delete($id){
+        $return = $this->master->delete($this->tabla,$this->getAttributes(),$id);
+        return $return;
     }
 }
 ?>
