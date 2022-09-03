9c8650c0d872a2dfb82068975b8a27a7c6ba9bf2
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-03 10:56:09 -0500

    creación clase y api permisos

---
+++ clases/permisos_class.php
@@ -0,0 +1,69 @@
+<?php
+include_once "master_class.php";
+
+class Permisos extends Master implements iMetodos{
+    public $id_permiso;#1
+    public $descripcion;#0
+    public $activo;
+    public $master;
+    private $public_attributes;
+    private $tabla;
+    private $intergers;
+    private $strings;
+    private $doubles;
+    private $intergers_update;
+    private $nulls;
+
+    function Permisos(){
+        $this->master = new Master();
+        $this->tabla = "permisos";
+        $this->public_attributes = 3;
+        $this->intergers = array();
+        $this->strings = array(0);
+        $this->doubles = array();
+        $this->intergers_update = array(1);
+        $this->nulls = array();
+    }
+
+    function getAttributes(){
+        $array = array();
+
+        $count = 0;
+        foreach($this as $key=>$value){
+            if($count<$this->public_attributes){
+                $array[] = $key;
+            }
+            $count = $count + 1;
+        }
+
+        return $array;
+    }
+
+    function insert($values){
+        $response = $this->master->insert($this->tabla,$this->getAttributes(),$values,$this->intergers,$this->strings,$this->doubles,$this->nulls);
+
+        return $response;
+    }
+
+    function getAll(){
+        $response = $this->master->getAll($this->tabla);
+
+        return $response;
+    }
+
+    function getById($id){
+        $response = $this->master->getById($this->tabla,$id,$this->getAttributes());
+        return $response;
+    }
+
+    function update($values){
+        $response = $this->master->update($this->tabla,$this->getAttributes(),$values,$this->intergers_update,$this->strings,$this->doubles,$this->nulls);
+        return $response;
+    }
+
+    function delete($id){
+        $response = $this->master->delete($this->tabla,$this->getAttributes(),$id);
+        return $response;
+    }
+}
+?>
