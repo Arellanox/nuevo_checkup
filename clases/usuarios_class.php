21aa2f7037a0f96f5d0c1af4ee1e9207df9dce69
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-03 12:50:55 -0500

    implementación master en usuarios
    se crean todos los metodos de master en la tabla usuarios

--- clases/usuarios_class.php
+++ clases/usuarios_class.php
@@ -1,27 +1,36 @@
 <?php
-include "../conexion.php";
-include "master_class.php";
+include_once "master_class.php";
 
-class Usuarios extends Master{
-    public $id;
-    public $cargo_id;
-    public $tipo_id;
-    public $nombre;
-    public $paterno;
-    public $materno;
-    public $usuario;
-    public $contrasenia;
-    public $profesion;
+class Usuarios extends Master implements iMetodos{
+    public $id_usuario;#8
+    public $cargo_id;#0
+    public $tipo_id;#1
+    public $nombre;#2
+    public $paterno;#3
+    public $materno;#4
+    public $usuario;#5
+    public $contrasenia;#6
+    public $profesion;#7
     public $activo;
     private $tabla;
     private $public_attributes;
-    private $master; 
+    public $master; 
+    private $intergers;
+    private $strings;
+    private $doubles;
+    private $intergers_update;
+    private $nulls;
 
     function Usuarios(){
-        $this->id = 1;
         $this->tabla = "usuarios";
         $this->public_attributes = 10;
         $this->master = new Master();
+        $this->intergers = array(0,1);
+        $this->strings = array(2,3,4,5,6,7);
+        $this->doubles = array();
+        $this->intergers_update(0,1,8);
+        $this->nulls = array(7);
+
     }
 
     function getAttributes(){
@@ -39,7 +48,28 @@ class Usuarios extends Master{
     }
 
     function insert($values){
-        $this->master->insert($this->tabla,$this->getAttributes(),$values);
+        $response = $this->master->insert($this->tabla,$this->getAttributes(),$values,$this->intergers,$this->strings,$this->doubles,$this->nulls);
+        return $response;
+    }
+
+    function getAll(){
+        $response = $this->master->getAll($this->tabla);
+        return $response;
+    }
+
+    function getById($id){
+        $response = $this->master->getById($this-tabla,$id,$this->getAttributes());
+        return $response;
+    }
+
+    function update($values){
+        $response = $this->master->update($this->tabla,$this->getAttributes(),$values,$this->intergers_update,$this->strings,$this->doubles,$this->nulls);
+        return $response;
+    }
+
+    function delete($id){
+        $response = $this->master->delete($this->tabla,$this->getAttributes(),$id);
+        return $response;
     }
 }
 ?>
