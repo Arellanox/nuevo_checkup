af1410a4f20065bd66b1fe142b0ca76a5e1a3b36
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-06 12:02:02 -0500

    update usuarios_permisos_class
    Implementación de los métodos generales de las clase maestra.

---
+++ clases/usuarios_permisos_class.php
@@ -0,0 +1,66 @@
+<?php
+include_once "master_class.php";
+
+class PermisosUsuarios extends Master implements iMetodos{
+    public $id_usuario_permiso;#2
+    public $usuario_id;#0
+    public $permiso_id;#1
+    public $activo;
+    public $master;
+    private $tabla;
+    private $public_attributes;
+    private $intergers;
+    private $strings;
+    private $doubles;
+    private $intergers_update;
+    private $nulls;
+
+    function PermisosUsuarios(){
+        $this->tabla = "usuarios_permisos";
+        $this->intergers = array(0,1);
+        $this->strings = array();
+        $this->doubles = array();
+        $this->nulls = array();
+        $this->intergers_update = array(0,1,2);
+    }
+
+    function getAttributes(){
+        $array = array();
+
+        $count = 0;
+        foreach($this as $key=>$value){
+            if($count<$this->public_attributes){
+                $array[] = $key;
+            }
+            $count = $count + 1;
+        }
+
+        return $array;
+    }
+
+    function insert($values){
+        $response = $this->master->insert($this->tabla,$this->getAttributes(),$values,$this->intergers,$this->strings,$this->doubles,$this->nulls);
+        return $response;
+    }
+
+    function getAll(){
+        $response = $this->master->getAll($this->tabla);
+        return $response;
+    }
+
+    function getById($id){
+        $response = $this->master->getById($this->tabla,$id,$this->getAttributes());
+        return $response;
+    }
+
+    function update($values){
+        $response = $this->master->update($this->tabla,$this->getAttributes(),$values,$this->intergers_update,$this->strings,$this->doubles,$this->nulls);
+        return $response;
+    }
+
+    function delete($id){
+        $response = $this->master->delete($this->tabla,$this->getAttributes(),$id);
+        return $response;
+    }
+}
+?>
