69658774bc44789b0294f5dedc93c78a2a3fef9a
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-08 14:28:32 -0500

    Creación precio api

---
+++ api/precios_api.php
@@ -0,0 +1,61 @@
+<?php
+include "../interfaces/iMetodos.php";
+include "../clases/precios_class.php";
+include "../clases/clientes_class.php";
+include "../clases/servicios_class.php";
+
+$precio = new Precios();
+$api = 1;
+
+switch ($api) {
+    case 1:
+        $new = array(1,1,350.0,100.0,450.0);
+        $response = $precio->insert($new);
+
+        if(is_numeric($response)){
+            echo json_encode(array("response"=>array("code"=>1,"lastId"=>$response)));
+        } else {
+            echo json_encode(array("response"=>array("code"=>2,"msj"=>$response)));
+        }
+        break;
+    case 2:
+        $response = $precio->getAll();
+
+        if(is_array($response)){
+            echo json_encode(array("response"=>array("code"=>1,"data"=>$response)));
+        } else {
+            echo json_encode(array("response"=>array("code"=>2,"msj"=>$response)));
+        }
+        break;
+    case 3:
+        $response = $precio->getById(1);
+        if(is_array($response)){
+            echo json_encode(array("response"=>array("code"=>1,"data"=>$response)));
+        } else {
+            echo json_encode(array("response"=>array("code"=>2,"msj"=>$response)));
+        }
+        break;
+    case 4:
+        $response = $precio->update($values);
+
+        if(is_numeric($response)){
+            echo json_encode(array("response"=>array("code"=>1,"affected"=>$response)));
+        } else {
+            echo json_encode(array("response"=>array("code"=>2,"msj"=>$response)));
+        }
+        break;
+
+    case 5:
+        $response = $precio->delete(1);
+        if(is_numeric($response)){
+            echo json_encode(array("response"=>array("code"=>1,"affected"=>$response)));
+        } else {
+            echo json_encode(array("response"=>array("code"=>2,"msj"=>$response)));
+        }
+        break;
+    
+    default:
+        # code...
+        break;
+}
+?>
