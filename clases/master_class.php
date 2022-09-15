b81957527bbfc5e561c4d4c6747b71f2b2ee23a1
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-15 15:39:23 -0500

    update master class
    se cambiaron los datos de conexion.

--- clases/master_class.php
+++ clases/master_class.php
@@ -22,11 +22,11 @@ class Master extends Miscelaneus{
 
     function connectDb(){
         //require_once 'pdoconfig.php';
-        $host = "localhost";
-        $dbname = "checkup";
-        $username = "root";
-        // $password = "bimo2022";
-        $password = "12345678";
+        $host = "212.1.208.201";
+        $dbname = "u808450138_checkup";
+        $username = "u808450138_bimo";
+        $password = "uJbr*Z7e";
+        //$password = "12345678";
         try {
             $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
             //echo "Connected to $dbname at $host successfully.";
