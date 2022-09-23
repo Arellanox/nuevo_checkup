8e3019bab60d8aa34dd9382ea6f9042e5f410ca4
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-23 10:43:52 -0500

    create laboratorio metodos api
    se implementan los cruds

---
+++ api/laboratorio_metodos_api.php
@@ -0,0 +1,111 @@
+<?php 
+include "../clases/master_class.php";
+
+$master = new Master();
+$api = $_POST['api'];
+
+switch ($api) {
+    case 1:
+        #insert
+        $response = $master->insertByProcedure('sp_laboratorio_metodos_g',$values);
+
+        if (is_numeric($response)) {
+            echo json_encode(array(
+                'response'=> array(
+                    'code'=> 1,
+                    'lastId'=>$response
+                )
+                ));
+        } else {
+            echo json_encode(array(
+                'response'=>array(
+                    'code'=>2,
+                    'msj'=> $response
+                )
+            ));
+        }
+        break;
+    case 2:
+        #getall
+        $response = $master->getByProcedure('sp_laboratorio_metodos_b',array());
+        if (is_array($response)) {
+            echo json_encode(array(
+                'response'=> array(
+                    'code'=> 1,
+                    'data'=>$response
+                )
+                ));
+        } else {
+            echo json_encode(array(
+                'response'=>array(
+                    'code'=>2,
+                    'msj'=> $response
+                )
+            ));
+        }
+        break;
+    case 3:
+        #getbyid
+        $response = $master->getByProcedure('sp_laboratorio_metodos_b',array($id));
+
+        if (is_array($response)) {
+            echo json_encode(array(
+                'response'=> array(
+                    'code'=> 1,
+                    'data'=>$response
+                )
+                ));
+        } else {
+            echo json_encode(array(
+                'response'=>array(
+                    'code'=>2,
+                    'msj'=> $response
+                )
+            ));
+        }
+        break;
+    case 4:
+        #update
+        $response = $master->updateByProcedure('sp_laboratorio_metodos_g',$values);
+
+        if (is_numeric($response)) {
+            echo json_encode(array(
+                'response'=> array(
+                    'code'=> 1,
+                    'affected'=>$response
+                )
+                ));
+        } else {
+            echo json_encode(array(
+                'response'=>array(
+                    'code'=>2,
+                    'msj'=> $response
+                )
+            ));
+        }
+        break;
+    case 5:
+        #delete
+        $response = $master->deleteByProcedure('sp_laboratorio_metodos_e',array($id));
+        
+        if (is_numeric($response)) {
+            echo json_encode(array(
+                'response'=> array(
+                    'code'=> 1,
+                    'affected'=>$response
+                )
+                ));
+        } else {
+            echo json_encode(array(
+                'response'=>array(
+                    'code'=>2,
+                    'msj'=> $response
+                )
+            ));
+        }
+        break;
+    default:
+        # code...
+        break;
+}
+?>
