b8b7171faf037b011c1d0bb73d0a3e4f1d7cf441
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-24 11:57:14 -0500

    create sat tipos api
    implementacion de cruds

---
+++ api/sat_tipo_api.php
@@ -0,0 +1,33 @@
+<?php 
+include "../clases/master_class.php";
+
+$master = new Master();
+
+$api = $_POST['api'];
+
+switch ($api) {
+    case 1:
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->insertByProcedure('sp_sat_tipos_g',$values));
+        break;
+    case 2:
+        echo $master->mis->returnApi($master->getByProcedure('sp_sat_tipos_b',array(null)));
+        break;
+    case 3:
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->getByProcedure('sp_sat_tipos_b',$values));
+        break;
+    case 4:
+        $values = $master->mis->getFormValues(array_slice($_POST,0,2));
+        echo $master->mis->returnApi($master->updateByProcedure('sp_sat_tipos_g',$values));
+        break;
+    case 5:
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->deleteByProcedure('sp_sat_tipos_e',$values));
+        break;
+    
+    default:
+        echo "Default";
+        break;
+}
+?>
