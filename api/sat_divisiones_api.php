cc83a3b1d985172d3f171e92fb7e2fc5a9a6849f
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-24 12:36:35 -0500

    create sat divisiones api
    implementacion del crud

---
+++ api/sat_divisiones_api.php
@@ -0,0 +1,39 @@
+<?php 
+include "../clases/master_class.php";
+
+$master = new Master();
+$api = $_POST['api'];
+
+switch ($api) {
+    case 1:
+        #insert 
+        
+        $values = $master->mis->getFormValues(array_slice($_POST,0,2));
+      
+        echo $master->mis->returnApi($master->insertByProcedure('sp_sat_divisiones_g',$values));
+        break;
+    case 2:
+        #getall
+        echo $master->mis->returnApi($master->getByProcedure('sp_sat_divisiones_b',array(null)));
+        break;
+    case 3:
+        #getbyid
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->getByProcedure('sp_sat_divisiones_b',$values));
+        break;
+    case 4:
+        #update
+        $values = $master->mis->getFormValues(array_slice($_POST,0,3));
+        echo $master->mis->returnApi($master->updateByProcedure('sp_sat_divisiones_g',$values));
+        break;
+    case 5:
+        #delete
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->deleteByProcedure('sp_sat_divisiones_e',$values));
+        break;
+    
+    default:
+        # code...
+        break;
+}
+?>
