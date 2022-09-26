de5e2a8553bcccf9f86fd0627de6601ff6999480
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-26 13:03:30 -0500

    creacion de api
    catalogo de grupo y clases sat

--- api/sat_grupos_api.php
+++ api/sat_grupos_api.php
@@ -8,7 +8,26 @@ switch ($api) {
     case 1:
         # insert
         $values = $master->mis->getFormValues(array_slice($_POST,0,2));
-        echo $master->mis->returnApi($master)
+        echo $master->mis->returnApi($master->insertByProcedure('sp_sat_grupos_g',$values));
+        break;
+    case 2:
+        #getall
+        echo $master->mis->returnApi($master->getByProcedure('sp_sat_grupos_b',array(null)));
+        break;
+    case 3:
+        #getById
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->updateByProcedure('sp_sat_grupos_g',$values));
+        break;
+    case 4:
+        #update
+        $values = $master->mis->getFormValues(array_slice($_POST,0,3));
+        echo $master->mis->returnApi($master->updateByProcedure('sp_sat_grupos_g',$values));
+        break;
+    case 5:
+        #delete
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->deleteByProcedure('sp_sat_grupos_e',$values));
         break;
     
     default:
