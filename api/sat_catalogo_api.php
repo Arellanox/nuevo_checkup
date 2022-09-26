981d0725c4f26b51f35716b8227e05e6022444f7
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-26 13:14:13 -0500

    update catalogo api

--- api/sat_catalogo_api.php
+++ api/sat_catalogo_api.php
@@ -7,32 +7,26 @@ $master = new Master();
 # En caso de que no se envie nada, toma la api 2 por default.
 $api = isset($_POST['api']) ?  $_POST['api'] : (isset($_GET['api']) ? $_GET['api'] : 2);
 
-if($api!=2){
-    if(is_null($_POST)){
-        echo "No se recibieron parámetros";
-        return;
-    }
-}
 
 switch ($api) {
     case 1:
-        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
-        echo $master->mis->returnApi($master->insertByProcedure('sp_sat_tipos_g',$values));
+        $values = $master->mis->getFormValues(array_slice($_POST,0,4));
+        echo $master->mis->returnApi($master->insertByProcedure('sp_catalogo_g',$values));
         break;
     case 2:
-        echo $master->mis->returnApi($master->getByProcedure('sp_sat_tipos_b',array(null)));
+        echo $master->mis->returnApi($master->getByProcedure('sp_catalogo_b',array(null)));
         break;
     case 3:
         $values = $master->mis->getFormValues(array_slice($_POST,0,1));
-        echo $master->mis->returnApi($master->getByProcedure('sp_sat_tipos_b',$values));
+        echo $master->mis->returnApi($master->getByProcedure('sp_catalogo_b',$values));
         break;
     case 4:
-        $values = $master->mis->getFormValues(array_slice($_POST,0,2));
-        echo $master->mis->returnApi($master->updateByProcedure('sp_sat_tipos_g',$values));
+        $values = $master->mis->getFormValues(array_slice($_POST,0,5));
+        echo $master->mis->returnApi($master->updateByProcedure('sp_catalogo_g',$values));
         break;
     case 5:
         $values = $master->mis->getFormValues(array_slice($_POST,0,1));
-        echo $master->mis->returnApi($master->deleteByProcedure('sp_sat_tipos_e',$values));
+        echo $master->mis->returnApi($master->deleteByProcedure('sp_catalogo_e',$values));
         break;
     
     default:
