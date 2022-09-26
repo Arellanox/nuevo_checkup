f04c0e2c13f56417867a6ff594c1959500a7e7fa
Josue de la Cruz <arellanox0392@gmail.com>
2022-09-26 13:04:47 -0500

    update log

---
+++ api/sat_catalogo_api.php
@@ -0,0 +1,42 @@
+<?php 
+include "../clases/master_class.php";
+
+$master = new Master();
+
+# Revisa el metodo por el que recibe la variable api.
+# En caso de que no se envie nada, toma la api 2 por default.
+$api = isset($_POST['api']) ?  $_POST['api'] : (isset($_GET['api']) ? $_GET['api'] : 2);
+
+if($api!=2){
+    if(is_null($_POST)){
+        echo "No se recibieron parámetros";
+        return;
+    }
+}
+
+switch ($api) {
+    case 1:
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->insertByProcedure('sp_sat_tipos_g',$values));
+        break;
+    case 2:
+        echo $master->mis->returnApi($master->getByProcedure('sp_sat_tipos_b',array(null)));
+        break;
+    case 3:
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->getByProcedure('sp_sat_tipos_b',$values));
+        break;
+    case 4:
+        $values = $master->mis->getFormValues(array_slice($_POST,0,2));
+        echo $master->mis->returnApi($master->updateByProcedure('sp_sat_tipos_g',$values));
+        break;
+    case 5:
+        $values = $master->mis->getFormValues(array_slice($_POST,0,1));
+        echo $master->mis->returnApi($master->deleteByProcedure('sp_sat_tipos_e',$values));
+        break;
+    
+    default:
+        echo "Default";
+        break;
+}
+?>
