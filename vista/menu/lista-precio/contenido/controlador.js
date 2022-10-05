65cb82dcbc1f587c88664280bc2bbd3bdc0212e0
Kevin Gabriel Rodriguez <unrecovery.dz@gmail.com>
2022-10-05 16:26:43 -0500

    update Lista Precio

--- vista/menu/lista-precio/contenido/controlador.js
+++ vista/menu/lista-precio/contenido/controlador.js
@@ -1,27 +1,36 @@
 obtenerContenidoPrecios("listaprecios.php");
-function obtenerContenidoPrecios(tabla){
+function obtenerContenidoPrecios(tabla) {
   obtenerTitulo("ListaPrecios"); //Aqui mandar el nombre de la area
-  $.post("contenido/"+tabla, function(html){
+  $.post("contenido/" + tabla, function (html) {
     var idrow;
     $("#body-js").html(html);
 
-     var tablaUsuarios = $('#TablaListaPrecios').DataTable({
-       language: {
-         url: "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-       },
-       lengthMenu: [[10, 15, 20, 25, 30, 35, 40, 45, 50, -1], [10, 15, 20, 25, 30, 35, 40, 45, 50, "All"]],
-       columnDefs: [
-         { "width": "4%", "targets": 0 },
-         { "width": "24%", "targets": 1},
-         { "width": "24%", "targets": 2},
-         { "width": "24%", "targets": 3},
-         { "width": "24%", "targets": 4},
-       ],
-     })
+    var tablaPrecio = $("#TablaListaPrecios").DataTable({
+      language: {
+        url: "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
+      },
+      lengthMenu: [
+        [10, 15, 20, 25, 30, 35, 40, 45, 50, -1],
+        [10, 15, 20, 25, 30, 35, 40, 45, 50, "All"],
+      ],
+      columnDefs: [
+        { width: "4%", targets: 0 },
+        { width: "24%", targets: 1 },
+        { width: "24%", targets: 2 },
+        { width: "24%", targets: 3 },
+        { width: "24%", targets: 4 },
+      ],
+    });
 
-    $("#btn-perfil").click(function(){
-      alert()
-    })
+    $("#btn-perfil").click(function () {
+      alert();
+    });
 
+    $("#seleccion-cliente").select2({
+      tags: false,
+      width: "15%",
+      placeholder: "Selecciona un registro",
+    });
+    rellenarSelect("#seleccion-cliente", "clientes_api", 2, 0, 1);
   });
 }
--- vista/menu/lista-precio/contenido/listaprecios.php
+++ vista/menu/lista-precio/contenido/listaprecios.php
@@ -1,43 +1,60 @@
-<div class="row">
-  <div class="card col-12 col-lg-3 pt-3" id="panel-informacion">
+<div class="row ">
+  <div class="card col-12 pt-3" style="margin-bottom:5px;">
+    <div class="text-start" id="text-start" style="margin-top:4px;zoom:95%;margin-bottom:5px;">
+      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
+        <i class="bi bi-list"></i> Rayos X, Imagenologia
+      </button>
+      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
+        <i class="bi bi-list"></i> Laboratorio
+      </button>
+      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
+        <i class="bi bi-list"></i>Otros Servicios
+      </button>
+      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
+        <i class="bi bi-list"></i> Paquetes
+      </button>
 
-  </div>
-  <div class="card col-12 col-lg-9" style="margin-bottom:5px">
-    <div class="text-center" style="margin-top:4px;zoom:95%;margin-bottom:5px;">
-      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
-        <i class="bi bi-pencil-square"></i> Editar estudio
-      </button>
-      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
-        <i class="bi bi-pencil-square"></i> Editar estudio
-      </button>
-      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
-        <i class="bi bi-pencil-square"></i> Editar estudio
-      </button>
+      <label for="inputBuscarTableListaNuevos">Seleccione Cliente:</label>
+      <select name="metodo" id="seleccion-cliente" required>
+      </select>
     </div>
-    <table class="table table-hover display responsive " id="TablaListaPrecios" style="width: 100%">
-      <thead style="width: 100%">
-        <tr>
-          <th scope="col d-flex justify-content-center" class="all">#</th>
-          <th scope="col d-flex justify-content-center" class="all">Nombre</th>
-          <th scope="col d-flex justify-content-center" class="min-tablet">Costo</th>
-          <th scope="col d-flex justify-content-center" class="min-tablet">Margen</th>
-          <th scope="col d-flex justify-content-center" class="min-tablet">Opciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>  <div class="input-group">
-              <span class="input-span">$</span>
-              <input type="text" class="form-control input-form" name="interior" placeholder="" >
-              <span class="input-span">.00</span>
-            </div></td>
-          <td>$400</td>
-          <td>10%</td>
-          <td>Botonesw</td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <table class="table table-hover display responsive " id="TablaListaPrecios" style="width: 100%">
+        <thead style="width: 100%">
+          <tr>
+            <th scope="col d-flex justify-content-center" class="all">#</th>
+            <th scope="col d-flex justify-content-center" class="all">Nombre</th>
+            <th scope="col d-flex justify-content-center" class="min-tablet">Costo</th>
+            <th scope="col d-flex justify-content-center" class="min-tablet">Margen</th>
+            <th scope="col d-flex justify-content-center" class="min-tablet">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>
+              Nombre Estudio
+            </td>
+            <td>
+              <div class="input-group">
+                <span class="input-span">$</span>
+                <input type="number" class="form-control input-form" name="costo" placeholder="">
+                <span class="input-span">.00</span>
+              </div>
+            </td>
+            <td>
+              <div class="input-group">
+                <span class="input-span">%</span>
+                <input type="number" class="form-control input-form" name="margen" placeholder="">
+              </div>
+            </td>
+            <td>
+              Total
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 
 </div>
