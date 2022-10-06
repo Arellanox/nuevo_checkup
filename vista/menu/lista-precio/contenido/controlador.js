1b61cc3d857412dab4eb1d3269e637eb38ecf3af
Kevin Gabriel Rodriguez <unrecovery.dz@gmail.com>
2022-10-06 13:06:02 -0500

    a

--- vista/menu/lista-precio/contenido/controlador.js
+++ vista/menu/lista-precio/contenido/controlador.js
@@ -11,3 +11,20 @@ function obtenerContenidoPrecios(tabla) {
     $.getScript("contenido/js/precio-botones.js");
   });
 }
+
+
+
+
+obtenerContenidoPaquetes("paquetes.php");
+function obtenerContenidoPaquetes(tabla) {
+  obtenerTitulo("Paquetes"); //Aqui mandar el nombre de la area
+  $.post("contenido/" + tabla, function (html) {
+    var idrow;
+    $("#body-js").html(html);
+
+    // Datatable
+    $.getScript("contenido/js/paquete-tabla.js");
+    // Botones
+    $.getScript("contenido/js/paquete-botones.js");
+  });
+}
---
+++ vista/menu/lista-precio/contenido/paquetes.php
@@ -0,0 +1,82 @@
+<div class="row ">
+  <div class="card col-12 pt-3" style="margin-bottom:5px;">
+    <div class="text-start" id="text-start" style="margin-top:4px;zoom:95%;margin-bottom:5px;">
+      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
+        <i class="bi bi-list"></i> Rayos X, Imagenologia
+      </button>
+      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
+        <i class="bi bi-list"></i> Laboratorio
+      </button>
+      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
+        <i class="bi bi-list"></i>Otros Servicios
+      </button>
+      <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-estudio-editar">
+        <i class="bi bi-list"></i> Paquetes
+      </button>
+
+      <label for="inputBuscarTableListaNuevos">Seleccione Cliente:</label>
+      <select name="metodo" id="seleccion-cliente" required>
+      </select>
+    </div>
+    <div>
+      <table class="table table-hover display responsive " id="TablaListaPrecios" style="width: 100%">
+        <thead style="width: 100%">
+          <tr>
+            <th scope="col d-flex justify-content-center" class="all">#</th>
+            <th scope="col d-flex justify-content-center" class="all">Nombre</th>
+            <th scope="col d-flex justify-content-center" class="min-tablet">Costo</th>
+            <th scope="col d-flex justify-content-center" class="min-tablet">Margen</th>
+            <th scope="col d-flex justify-content-center" class="min-tablet">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>
+              Nombre Estudio
+            </td>
+            <td>
+              <div class="input-group">
+                <span class="input-span">$</span>
+                <input type="number" class="form-control input-form costo" name="costo" placeholder="" value="">
+                <span class="input-span">.00</span>
+              </div>
+            </td>
+            <td>
+              <div class="input-group">
+                <span class="input-span">%</span>
+                <input type="number" class="form-control input-form margen" name="margen" placeholder="" value="">
+              </div>
+            </td>
+            <td class="total">
+              $00
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>
+              Nombre Estudio 2
+            </td>
+            <td>
+              <div class="input-group">
+                <span class="input-span">$</span>
+                <input type="number" class="form-control input-form costo" name="costo" placeholder="" value="">
+                <span class="input-span">.00</span>
+              </div>
+            </td>
+            <td>
+              <div class="input-group">
+                <span class="input-span">%</span>
+                <input type="number" class="form-control input-form margen" name="margen" placeholder="" value="">
+              </div>
+            </td>
+            <td>
+              <p class="total">00</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+</div>
