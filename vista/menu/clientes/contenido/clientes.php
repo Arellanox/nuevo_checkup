dbd1a8ac53b32dd827963aca879d298b45870ffb
Kevin Gabriel Rodriguez <unrecovery.dz@gmail.com>
2022-10-11 15:40:25 -0500

    a

--- vista/menu/clientes/contenido/clientes.php
+++ vista/menu/clientes/contenido/clientes.php
@@ -1,11 +1,8 @@
-<div class="col-12 loader" id="loader" style="">
-  <div class="preloader" id="preloader"> </div>
-</div>
 <div class="row">
   <div class="card col-12 col-lg-3 pt-4" id="panel-informacion">
 
   </div>
-  <div class="card col-12 col-lg-9" style="margin-bottom:5px">
+  <div class="card col-7 col-lg-7" style="margin-bottom:5px">
     <div class="text-center" style="margin-top:4px;zoom:95%;margin-bottom:5px;">
       <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-cliente-editar">
         <i class="bi bi-pencil-square"></i> Editar Cliente
@@ -19,6 +16,7 @@
           <th scope="col d-flex justify-content-center" class="min-tablet">
             <Ri:a>Razon Social</Ri:a>
           </th>
+          <th scope="col d-fkex justify-content-center" class="min-tablet">Abreviatura</th>
         </tr>
       </thead>
       <tbody>
@@ -26,9 +24,13 @@
           <td>#1</td>
           <td>Kevin Gabriel Rodriguez</td>
           <td>UNRECOVERY-DZ S. RL de CV</td>
+          <td>URCDZ</td>
         </tr>
       </tbody>
     </table>
   </div>
-
-</div>
+  <div class="row">
+    <div style="margin-top:-125px;" class="card col-10 col-lg-4 pt-4 position-fixed start-100 translate-middle" id=" panel-informacion">
+      <h6>Lista de Contactos</h6>
+    </div>
+  </div>
