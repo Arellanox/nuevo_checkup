4b41899cedf777ee7b5f2e2a66295c4d3c777a61
Josue de la Cruz <arellanox0392@gmail.com>
2022-10-13 15:43:13 -0500

    modificaciones api

--- api/segmentos_api.php
+++ api/segmentos_api.php
@@ -12,17 +12,33 @@ if (! $tokenValido){
     exit;
 }
 
+$id = $_POST['id'];
+$cliente_id = $_POST['cliente_id'];
+$padre = $_POST['padre'];
+$descripcion = $_POST['descripcion'];
+
 // CAMBIAR LOS PARAMETROS DE LAS FUNCIONES PORQUE ESTAN FIJOS Y NO DINAMICOS
 // Creamos un objeto de segmentos para trabajar con él.
 $segmento = new Segmentos();
 
 $api = $_POST['api'];
 
+$params = array(
+    $id,
+    $cliente_id,
+    $padre,
+    $descripcion
+);
+
+$master = new Master();
+
 switch ($api) {
         // Insertar segmentos
     case 1:
-        $form = $segmento->master->mis->getFormValues($_POST);
-        $result = $segmento->insert($form);
+        // $form = $segmento->master->mis->getFormValues($_POST);
+        // $result = $segmento->insert($form);
+        $result = $master->insertByProcedure('sp_segmentos_g',$params);
+
         if (is_numeric($result)) {
             echo json_encode(array("response" => array("code" => 1, "lastId" => $result)));
         } else {
@@ -31,56 +47,57 @@ switch ($api) {
         break;
         // recuperar todos los segmentos activos
     case 2:
-        $segmentos = $segmento->getAll();
+        //$segmentos = $segmento->getAll();
+
+        $segmentos = $master->getByProcedure('sp_segmentos_b',[$cliente_id,$id]);
         if (is_array($segmentos)) {
-            $completeSgm = array();
-            $i = 1;
-            $clientes = new Clientes();
-            $dependencias = new Dependencias();
-            foreach ($segmentos as $sgm) {
-                //$sgmPadre = $segmento->getById($segmento["PADRE"]);
-                //$smgDes = $segmento->getById($segmento["DESCRIPCION"]);
-                $jsondependecias = $dependencias->getById($sgm["ID_SEGMENTO"]);
-                // print_r($jsondependecias);
-                // $jsoncliente = $clientes->getById($jsondependecias[0]["CLIENTE_ID"]);
-                if ($sgm['PADRE'] != null) {
-                    $array = $segmento->getById($sgm['PADRE']);
-                } else {
-                    $array = array(array('DESCRIPCION' => ''));
-                }
-                 count($jsondependecias);
-                if (count($jsondependecias)>0) {
-                    $jsoncliente = $clientes->getById($jsondependecias[0]["CLIENTE_ID"]);
-                } else {
-                    $jsoncliente = array(array('NOMBRE_COMERCIAL' => "<span class='badge bg-danger'><stronge>Sin cliente</stronge></span>"));
-                }
-                $sgm[] = $array;
-                $sgm[] = $jsoncliente;
-                $sgm['count'] = $i;
-                $i++;
-                $completeSgm[] = $sgm;
-            }
-            echo json_encode($completeSgm);
+        //     $completeSgm = array();
+        //     $i = 1;
+        //     $clientes = new Clientes();
+        //     $dependencias = new Dependencias();
+        //     foreach ($segmentos as $sgm) {
+        //         //$sgmPadre = $segmento->getById($segmento["PADRE"]);
+        //         //$smgDes = $segmento->getById($segmento["DESCRIPCION"]);
+        //         $jsondependecias = $dependencias->getById($sgm["ID_SEGMENTO"]);
+        //         // print_r($jsondependecias);
+        //         // $jsoncliente = $clientes->getById($jsondependecias[0]["CLIENTE_ID"]);
+        //         if ($sgm['PADRE'] != null) {
+        //             $array = $segmento->getById($sgm['PADRE']);
+        //         } else {
+        //             $array = array(array('DESCRIPCION' => ''));
+        //         }
+        //          count($jsondependecias);
+        //         if (count($jsondependecias)>0) {
+        //             $jsoncliente = $clientes->getById($jsondependecias[0]["CLIENTE_ID"]);
+        //         } else {
+        //             $jsoncliente = array(array('NOMBRE_COMERCIAL' => "<span class='badge bg-danger'><stronge>Sin cliente</stronge></span>"));
+        //         }
+        //         $sgm[] = $array;
+        //         $sgm[] = $jsoncliente;
+        //         $sgm['count'] = $i;
+        //         $i++;
+        //         $completeSgm[] = $sgm;
+        //     }
+            echo json_encode(array("response" => array("code" => 1, "data" => $segmentos)));
         } else {
             echo json_encode(array("response" => array("code" => 0, "msj" => $segmentos)));
         }
 
         break;
         // Seleccionar solamente un registro de la tabla segmentos
-    case 3:
+   // case 3:
         //$form = $segmento->master->mis->getFormValues($_POST);
-        $seg = $segmento->getById(3);
-        if (is_array($seg)) {
-            echo json_encode(array("response" => array("code" => 1, "segmento" => $seg)));
-        } else {
-            echo json_encode(array("response" => array("code" => 0, "msj" => $seg)));
-        }
-        break;
+        // $seg = $segmento->getById(3);
+        // if (is_array($seg)) {
+        //     echo json_encode(array("response" => array("code" => 1, "segmento" => $seg)));
+        // } else {
+        //     echo json_encode(array("response" => array("code" => 0, "msj" => $seg)));
+        // }
+        // break;
         // Actualizar la información de un segmento
-    case 4:
-        // El id debe ser el último elemento del arreglo POST
-        // $form = $segmento->master->mis->getFormValues($_POST);
-        $seg = $segmento->update($form);
+    case 3:
+       # actualizar un segmento
+        $seg = $master->updateByProcedure('sp_segmentos_g',$params);
 
         if (is_numeric($seg)) {
             echo json_encode(array("response" => array("code" => 1, "affected" => $seg)));
@@ -90,7 +107,9 @@ switch ($api) {
         break;
     case 5:
         //$deletingId = $_POST['id'];
-        $deleted = $segmento->delete(3);
+        // $deleted = $segmento->delete(3);
+
+        $deleted = $master->deleteByProcedure('sp_segmentos_e',array($id));
 
         if (is_numeric($deleted)) {
             echo json_encode(array("response" => array("code" => 1, "affected" => $deleted)));
--- api/servicios_api.php
+++ api/servicios_api.php
@@ -6,8 +6,8 @@ require_once "../clases/token_auth.php";
 $tokenVerification = new TokenVerificacion();
 $tokenValido = $tokenVerification->verificar();
 if (! $tokenValido){
-    // $tokenVerification->logout();
-    // exit;
+    $tokenVerification->logout();
+    exit;
 }
 
 $master = new Master();
