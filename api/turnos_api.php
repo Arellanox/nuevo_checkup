8b4679594003bea838b6ec78f6dd7b6edb481272
IscarRuiz <IscarPelaez@gmail.com>
2022-10-19 12:54:14 -0500

    Update turnos_api.php

--- api/turnos_api.php
+++ api/turnos_api.php
@@ -1,24 +1,29 @@
 <?php
-include "../interfaces/iMetodos.php";
+require_once "../clases/master_class.php";
 require_once "../clases/token_auth.php";
-include "../clases/turnos_class.php";
-include "../clases/pacientes_class.php";
-include_once "../clases/master_class.php";
 
 $tokenVerification = new TokenVerificacion();
 $tokenValido = $tokenVerification->verificar();
-if (! $tokenValido){
+if (!$tokenValido) {
     $tokenVerification->logout();
     exit;
 }
 
-$master = new Master();
+#api
+$api = $_POST['api'];
 
+#buscar
+$id = $_POST['id'];
+$id_paciente = $_POST['id_paciente'];
+$fecha_agenda = $_POST['fecha_busqueda'];
+
+#insertar
 $id_turno = $_POST['id_turno'];
 $paciente_id = $_POST['paciente_id'];
 $paquete_id = $_POST['paquete_id'];
 $prefolio = $_POST['prefolio'];
 $folio = $_POST['folio'];
+$fecha_registro = $_POST['fecha_registro'];
 $fecha_agenda = $_POST['fecha_agenda'];
 $fecha_recepcion = $_POST['fecha_recepcion'];
 $turno = $_POST['turno'];
@@ -28,12 +33,13 @@ $total = $_POST['total'];
 $completado = $_POST['completado'];
 
 
-$params = array(
+$parametros = array(
     $id_turno,
     $paciente_id,
     $paquete_id,
     $prefolio,
     $folio,
+    $fecha_registro,
     $fecha_agenda,
     $fecha_recepcion,
     $turno,
@@ -42,98 +48,38 @@ $params = array(
     $total,
     $completado
 );
+$response="";
 
-$turno = new Turnos();
-$api = 3;
-
+$master = new Master();
 switch ($api) {
     case 1:
-        $response = $master->returnApi($master->insertByProcedure('sp_turnos_g',$params));
-        /* $new = array(1,"asdf124","2022-09-09",null,1,null,"2022-09-09",100.00);
-        try {
-            $response = $turno->insert($new);
-
-        if(is_numeric($response)){
-            echo json_encode(array("response"=>array("code"=>1,"lastId"=>$response)));
-        } else {
-            echo json_encode(array("response"=>array("code"=>2,"msj"=>$response)));
-        }
-        } catch (Exception $th) {
-            throw $th;
-        } */
+        $response = $master->insertByProcedure("sp_turnos_g", $parametros);
         break;
     case 2:
-
-        $response = $master->returnApi($master->getByProcedure('sp_turnos_b',array($id_turno)));
-        /* $response = $turno->getAll();
-
-        if(is_array($response)){
-            $dataset = array();
-
-            foreach($response as $value){
-                $paciente = new Pacientes();
-                $pacienteSet = $paciente->getById($value['PACIENTE_ID']);
-                $value["PACIENTE"] = $pacienteSet;
-                $value[] = $pacienteSet;
-                $dataset[] = $value;
-            }
-            echo json_encode(array("response"=>array("code"=>1,"data"=>$dataset)));
-        } else {
-            echo json_encode(array("response"=>array("code"=>2,"msj"=>$response)));
-        } */
+        # buscar
+        $response = $master->getByProcedure("sp_turnos_b", [$id,]);
         break;
-    // case 3:
-    //     $response = $turno->getById(1);
-    //     if(is_array($response)){
-    //         $dataset = array();
-
-    //         foreach($response as $value){
-    //             $paciente = new Pacientes();
-    //             $pacienteSet = $paciente->getById($value['PACIENTE_ID']);
-    //             $value["PACIENTE"] = $pacienteSet;
-    //             $value[] = $pacienteSet;
-    //             $dataset[] = $value;
-    //         }
-    //         echo json_encode(array("response"=>array("code"=>1,"data"=>$dataset)));
-    //     } else {
-    //         echo json_encode(array("response"=>array("code"=>2,"msj"=>$response)));
-    //     }
-    //     break;
     case 3:
-        #actualizar
-        $response = $master->returnApi($master->updateByProcedure('sp_turnos_g',$params));
-        /* $response = $turno->update($values);
-
-        if(is_numeric($response)){
-            echo json_encode(array("response"=>array("code"=>1,"affected"=>$response)));
-        } else {
-            echo json_encode(array("response"=>array("code"=>2,"affected"=>$response)));
-        } */
+        # actualizar
+        $response = $master->updateByProcedure("sp_turnos_g", $parametros);
         break;
     case 4:
-        #eliminar
-        $response = $master->returnApi($master->deleteByProcedure('sp_turnos_e',array($id_turno)));
-        /* $response = $turno->delete(1);
-
-        if(is_numeric($response)){
-            echo json_encode(array("response"=>array("code"=>1,"affected"=>$response)));
-        } else {
-            echo json_encode(array("response"=>array("code"=>2,"msj"=>$response)));
-        }
-        break; */
+        # desactivar
+        $response = $master->deleteByProcedure("sp_turnos_e", [$id]);
         break;
-    case 5:
+
+   case 5:
         # recuperar la lista de trabajo por area
         $area = $_POST['area_id'];
         $fecha = $_POST['fecha_busqueda'];
-
         $response = $master->returnApi($master->getByProcedure('sp_lista_de_trabajo',array($fecha,$area)));
+    case 6:
+        #historial de servicios
+        $response = $master->getByProcedure("sp_historial_servicios_paciente", [$id,$id_paciente,$fecha_agenda]);
         break;
-    
     default:
-        # code...
+        $response = "api no reconocida";
         break;
 }
 
-echo $response;
-?>
+echo $master->returnApi($response);
