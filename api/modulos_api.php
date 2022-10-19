23b3d228a99246ebe7644c90aa3aa498ed00be30
Josue de la Cruz <arellanox0392@gmail.com>
2022-10-19 15:29:16 -0500

    update modulos api

--- api/modulos_api.php
+++ api/modulos_api.php
@@ -2,10 +2,15 @@
 include_once "../clases/master_class.php";
 
 $api = $_POST['api'];
+$master = new Master();
 
+#para la tabla modulos
 $id_modulo = $_POST['id_modulo'];
 $area_id = $_POST['area_id'];
 $descripcion = $_POST['descripcion'];
+
+# para la tablade usuarios_modulos
+$usuario = $_POST['usuario_id'];
 
 $params = array(
     $id_modulo,
@@ -19,13 +24,25 @@ switch($api){
         $response = $master->insertByProcedure('sp_modulos_g',$params);
         break;
     case 2:
+        #buscar todos o por id
         $response = $master->getByProcedure('sp_modulos_b',[$id_modulo,$area_id]);
         break;
     case 3:
+        # actualizar 
         $response = $master->updateByProcedure("sp_modulos_g",$params);
         break;
     case 4:
+        # eliminar
         $response = $master->deleteByProcedure('sp_modulos_e',[$id_modulo]);
+        break;
+    case 5:
+        #insertar relacion modulos usuarios
+        
+        $response = $master->insertByProcedure('sp_usuarios_modulos_g',[$id_modulo,$usuario]);
+        break;
+    case 6:
+        #eliminar una relacion usuario modulo
+        $response = $master->deleteByProcedure('sp_usuarios_modulos_e',[$id_modulo,$usuario]);
         break;
     default:
         break;
