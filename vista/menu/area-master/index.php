ce16b188ab075168994344afae73dd34ae57ad89
Kevin Gabriel Rodriguez <unrecovery.dz@gmail.com>
2022-10-19 12:44:14 -0500

    ADZ191022

--- vista/menu/area-master/index.php
+++ vista/menu/area-master/index.php
@@ -2,26 +2,32 @@
 //Variables dinamicas;
 session_start();
 include "../../variables.php";
-$menu = "Servicios";
- ?>
+$menu = "Panel General";
+?>
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
- <head>
-   <?php include "../../include/head.php"; ?>
-   <title><?php echo $menu; ?> | Bimo</title>
- </head>
- <body class="" id="body-controlador"> </body>
- <script type="text/javascript">
-    vista('<?php echo $menu; ?>', '<?php echo $https.$url.'/nuevo_checkup/vista/menu/controlador/controlador.php'; ?>')
-     function vista(menu, url){
-       $.post(url, {menu: menu}, function(html){
-          $("#body-controlador").html(html);
-     	 });
-     }
 
-     // Verificar logeo :)
-     $.getScript('<?php echo $https.$url.'/nuevo_checkup/vista/login/contenido/verificar.js';?>' ).done(function(){
-       loggin();
-     });
- </script>
+<head>
+  <?php include "../../include/head.php"; ?>
+  <title><?php echo $menu; ?> | Bimo</title>
+</head>
+
+<body class="" id="body-controlador"> </body>
+<script type="text/javascript">
+  vista('<?php echo $menu; ?>', '<?php echo $https . $url . '/nuevo_checkup/vista/menu/controlador/controlador.php'; ?>')
+
+  function vista(menu, url) {
+    $.post(url, {
+      menu: menu
+    }, function(html) {
+      $("#body-controlador").html(html);
+    });
+  }
+
+  // Verificar logeo :)
+  $.getScript('<?php echo $https . $url . '/nuevo_checkup/vista/login/contenido/verificar.js'; ?>').done(function() {
+    loggin();
+  });
+</script>
+
 </html>
