12acf214026dce4f74df7af4d2966a05dfd68fa7
Kevin Gabriel Rodriguez <unrecovery.dz@gmail.com>
2022-10-21 15:13:11 -0500

    AJAX

--- vista/registro/index.php
+++ vista/registro/index.php
@@ -1,40 +1,83 @@
 <?php
 //Variables dinamicas;
-$codigo = $_POST['code']?$_POST['code']:null;
-$token = $_POST['token']?$_POST['token']:null;
+$codigo = isset($_POST['code']) ? $_POST['code'] : null;
+$token = isset($_POST['token']) ? $_POST['token'] : null;
 include "../variables.php";
 $menu = "Preregistro";
- ?>
+?>
+
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
- <head>
-   <?php include "../include/head.php"; ?>
-   <title><?php echo $menu; ?> | Bimo</title>
- </head>
- <body class="" id="body-controlador"> </body>
- <script type="text/javascript">
-    var logeo = 1;
-    vista('<?php echo $menu; ?>', '<?php echo $https.$url.'/nuevo_checkup/vista/menu/controlador/controlador.php'; ?>')
-     function vista(menu, url){
-       $.post(url, {menu: menu}, function(html){
-          $("#body-controlador").html(html);
 
-     	 });
-     }
+<head>
+  <?php include "../include/head.php"; ?>
+  <title><?php echo $menu; ?> | Bimo</title>
+</head>
+
+<body class="" id="body-controlador"> </body>
+<script type="text/javascript">
+  var logeo = 1;
+  let codigo = '<?php echo $codigo; ?>';
+  let token = '<?php echo $token; ?>';
 
 
+  if (codigo == token) {
+    alert('No tienes acceso')
+  } else if (codigo != null) {
 
 
-     // $.ajax({
-     //   data: {token: '<?php echo $codigo; ?>', api: 6},
-     //   url: "../../api/clientes_api.php",
-     //   type: "POST",
-     //   success: function(data) {
-     //     array_selected = jQuery.parseJSON(data);
-     //     setTimeout(function(){
-     //       $("#procedencia-preregistro").val(array_selected['response']['data'][0]['NOMBRE_SISTEMA']);
-     //     }, 1000)
-     //   },
-     // });
- </script>
+    $.ajax({
+      data: {
+        codigo: '<?php echo $codigo; ?>',
+        api: 6
+      },
+      url: "../../api/clientes_api.php",
+      type: "POST",
+      success: function(data) {
+        data = jQuery.parseJSON(data);
+        if (data.response.data != null) {
+          row = data.response.data[0]
+          $("#procedencia-registro").html(row['NOMBRE_CLIENTE'])
+        } else {
+          alert("No tienes Accesso")
+        }
+      },
+    });
+  } else if (token != null) {
+
+
+
+    $.ajax({
+      data: {
+        token: '<?php echo $token; ?>',
+        api: 1
+      },
+      url: "../../api/token_api.php",
+      type: "POST",
+      success: function(data) {
+        data = jQuery.parseJSON(data);
+        if (data.response.data != null) {
+          $("#procedencia-registro").html('PARTICULAR')
+        } else {
+          alert("No tienes Accesso")
+        }
+      },
+    });
+
+  } else {
+    alert('No tienes acceso')
+  }
+
+  vista('<?php echo $menu; ?>', '<?php echo $https . $url . '/nuevo_checkup/vista/menu/controlador/controlador.php'; ?>')
+
+  function vista(menu, url) {
+    $.post(url, {
+      menu: menu
+    }, function(html) {
+      $("#body-controlador").html(html);
+
+    });
+  }
+</script>
+
 </html>
