e70755a844447c5e13153119fec713072cad2d3d
IscarRuiz <IscarPelaez@gmail.com>
2022-10-28 15:38:01 -0500

    Create toma_de_muestra_api.php

---
+++ api/toma_de_muestra_api.php
@@ -0,0 +1,47 @@
+<?php
+require_once "../clases/master_class.php";
+require_once "../clases/token_auth.php";
+
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+if (!$tokenValido) {
+    $tokenVerification->logout();
+    exit;
+}
+
+#api
+$api = $_POST['api'];
+
+#buscar
+
+$id_turno = $_POST['id_turno'];	
+$id_paciente = $_POST['id_paciente'];	
+$id_area = $_POST['id_area'];	
+$fecha_agenda = $_POST['fecha_agenda'];	
+
+#insertar
+$id_turno = $_POST['id_turno'];	
+$parametros = array(
+    $id_turno
+);
+$response="";
+
+$master = new Master();
+switch ($api) {
+    case 1:
+        $response = $master->insertByProcedure("sp_toma_de_muestra_servicios_g", $parametros);
+        break;
+    case 2:
+        # buscar_servicios de toma de muestra
+        $response = $master->getByProcedure("sp_toma_de_muestra_servicios_b", [$id_paciente,$id_area,$id_turno ]);
+        break;
+    case 3:
+        # actualizar toma de muestra
+        $response = $master->updateByProcedure("sp_toma_de_muestra_servicios_g", [$id_turno]);
+        break;
+    default:
+        $response = "api no reconocida";
+        break;
+}
+
+echo $master->returnApi($response);
