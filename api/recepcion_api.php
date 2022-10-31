a4e2c6f047857d6edd61d26ff24040d7973d8997
IscarRuiz <IscarPelaez@gmail.com>
2022-10-31 15:04:55 -0600

    Update recepcion_api.php
    desactivar los servicios de paciente detalle por turno

--- api/recepcion_api.php
+++ api/recepcion_api.php
@@ -6,7 +6,7 @@ $tokenVerification = new TokenVerificacion();
 $tokenValido = $tokenVerification->verificar();
 if (!$tokenValido) {
     //$tokenVerification->logout();
-   // exit;
+    // exit;
 }
 
 $api = $_POST['api'];
@@ -19,7 +19,7 @@ $comentarioRechazo = $_POST['comentario_rechazo'];
 
 
 #servicio para pacientes particulares o servicios extras para pacientes de empresas
-$servicios =$_POST['servicios']; //array
+$servicios = $_POST['servicios']; //array
 
 switch ($api) {
     case 1:
@@ -27,33 +27,35 @@ switch ($api) {
         # 1 para pacientes aceptados
         # 0 para pacientes rechazados
         # null o no enviar nada, para pacientes en espera
-        $response = $master->getByProcedure('sp_buscar_paciente_por_estado',array($estado_paciente));
+        $response = $master->getByProcedure('sp_buscar_paciente_por_estado', array($estado_paciente));
 
         break;
     case 2:
         # aceptar o rechazar pacientes [tambien regresar a la vida]
         # enviar 1 para aceptarlos, 0 para rechazarlos, null para pacientes en espera
-        $response = $master->updateByProcedure('sp_recepcion_cambiar_estado_paciente',array($idTurno,$estado_paciente,$comentarioRechazo));
+        $response = $master->updateByProcedure('sp_recepcion_cambiar_estado_paciente', array($idTurno, $estado_paciente, $comentarioRechazo));
 
         # Insertar el detalle del paquete al turno en cuestion
-        if($estado_paciente == 1){
+        if ($estado_paciente == 1) {
             # si el paciente es aceptado, cargar los estudios correspondientes
-            $response = $master->insertByProcedure('sp_recepcion_detalle_paquete_g',array($idTurno,$idPaquete,null));
+            $response = $master->insertByProcedure('sp_recepcion_detalle_paciente_g', array($idTurno, $idPaquete, null));
         }
 
         # Insertar servicios extrar para pacientes empresas o servicios para particulares
-        if(count($servicios)>0){
+        if (count($servicios) > 0) {
             # si hay algo en el arreglo lo insertamos
             foreach ($servicios as $key => $value) {
-                $response = $master->insertByProcedure('sp_recepcion_detalle_paquete_g',array($idTurno,null,$value));
+                $response = $master->insertByProcedure('sp_recepcion_detalle_paciente_g', array($idTurno, null, $value));
             }
         }
         break;
-
+    case 3:
+        #desactivar los servicios de un turno
+        $response = $master->updateByProcedure('sp_recepcion_desactivar_servicios', array($idTurno)); 
+        break;
     default:
         # code...
         break;
 }
 
 echo $master->returnApi($response);
-?>
