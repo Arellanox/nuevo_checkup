ebf2d81916f20394e0858a94800a857b23ff3fc3
Josue de la Cruz <arellanox0392@gmail.com>
2022-10-31 14:33:17 -0600

    update paquetes api

--- api/paquetes_api.php
+++ api/paquetes_api.php
@@ -139,9 +139,7 @@ switch ($api) {
     case 8:
         # asignar un paquete a un cliente
         $response = $master->updateByProcedure('sp_clientes_asignar_paquetes',array($cliente_id,$id_paquete));
-        break;
-
-    
+        break;    
     default:
         print_r($_GET);
         $response = "api no reconocida " . $api;
