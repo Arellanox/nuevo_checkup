f1bfb6f90fb2903d0c67ba072a4812c1662e72d6
Henry Garcia <henry_garcia1996@hotmail.com>
2022-11-28 12:45:45 -0600

    comentario de prueba

---
+++ pdf/class/View.php
@@ -0,0 +1,21 @@
+<?php
+
+function render_view( $view_name, &$view_vars=null){
+    if( $view_vars == null ){
+        // include $_SERVER["DOCUMENT_ROOT"]."/views/$view_name";
+        include $_SERVER["DOCUMENT_ROOT"] . "/pdf/views/$view_name";
+        return;
+    }
+
+    $keys = array_keys($view_vars);
+    $key_count = count($keys);
+
+    for($i = 0; $i<$key_count;$i++){
+        ${$keys[$i]} = $view_vars[$keys[$i]];
+    }
+    
+    ob_start();
+    // include $_SERVER["DOCUMENT_ROOT"]."/views/$view_name";
+    include $_SERVER["DOCUMENT_ROOT"] . "/pdf/views/$view_name";
+    return ob_get_clean();
+}
