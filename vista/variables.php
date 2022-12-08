9457caec9e9e1a2b6d1192777614411987caf29f
Cuevas113 <luisgerardocuevas5@gmail.com>
2022-12-08 14:15:46 -0600

    URL Cambio

--- vista/variables.php
+++ vista/variables.php
@@ -1,6 +1,13 @@
 <?php
 
-$url = "localhost";
+// try {
+//     $url = $_SERVER['SERVER_NAME'];
+//     echo $url;
+// } catch (\Throwable $th) {
+//     //throw $th;
+// }
+
+$url = $_SERVER['SERVER_NAME'];
 $https = 'http://';
 
  ?>
