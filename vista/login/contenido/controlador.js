481a7c21ee33a78307233b7c921a1e0eb1cfe709
Cuevas113 <luisgerardocuevas5@gmail.com>
2022-12-08 14:22:35 -0600

    correccion

--- vista/login/contenido/controlador.js
+++ vista/login/contenido/controlador.js
@@ -1,43 +1,44 @@
 // obtenerContenido o cambiar
 obtenerContenido("login.php");
-function obtenerContenido(tabla){
-  $.post("contenido/"+tabla, function(html){
-     $("#body-js").html(html);
+
+function obtenerContenido(tabla) {
+  $.post("contenido/" + tabla, function (html) {
+    $("#body-js").html(html);
 
 
-     $("#formIniciarSesion").submit(function(event){
-        event.preventDefault();
-        $(this).find('button :submit').prop('disabled',true)
-        /*DATOS Y VALIDACION DEL REGISTRO*/
-        var form = document.getElementById("formIniciarSesion");
-        var formData = new FormData(form);
-        formData.set('api', 1);
-        $.ajax({
-          data: formData,
-          url: "../../api/login_api.php",
-          type: "POST",
-          processData: false,
-          contentType: false,
-          beforeSend: function() {
-            alertMensaje('info', 'Espere un momento', 'Validando datos...');
-          },
-          success: function(data) {
-            data = jQuery.parseJSON(data);
-            console.log(data);
-            if (mensajeAjax(data)) {
-              const queryString = window.location.search;
-              const urlParams = new URLSearchParams(queryString);
-              const page = urlParams.get('page')
-              if (page) {
-                $(location).attr('href', page);
-              }else{
-                $(location).attr('href', 'http://localhost/nuevo_checkup/vista/menu/recepcion/');
-              }
-            }else{
-              $(this).find('button :submit').prop('disabled',false)
+    $("#formIniciarSesion").submit(function (event) {
+      event.preventDefault();
+      $(this).find('button :submit').prop('disabled', true)
+      /*DATOS Y VALIDACION DEL REGISTRO*/
+      var form = document.getElementById("formIniciarSesion");
+      var formData = new FormData(form);
+      formData.set('api', 1);
+      $.ajax({
+        data: formData,
+        url: "../../api/login_api.php",
+        type: "POST",
+        processData: false,
+        contentType: false,
+        beforeSend: function () {
+          alertMensaje('info', 'Espere un momento', 'Validando datos...');
+        },
+        success: function (data) {
+          data = jQuery.parseJSON(data);
+          console.log(data);
+          if (mensajeAjax(data)) {
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const page = urlParams.get('page')
+            if (page) {
+              $(location).attr('href', page);
+            } else {
+              $(location).attr('href', http + servidor + -'/nuevo_checkup/vista/menu/recepcion/');
             }
-          },
-        });
-     })
+          } else {
+            $(this).find('button :submit').prop('disabled', false)
+          }
+        },
+      });
+    })
   });
 }
