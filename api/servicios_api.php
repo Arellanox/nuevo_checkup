2d34239ad954b8ffdd47dd339fa4ce9035da3f05
Josue de la Cruz <arellanox0392@gmail.com>
2022-12-26 14:11:42 -0600

    update servicios
    abreviaturas reales

--- api/servicios_api.php
+++ api/servicios_api.php
@@ -353,7 +353,7 @@ switch ($api) {
                 if ($infoEtiqueta[$i]['CONTENEDOR'] == $infoEtiqueta[$e]['CONTENEDOR'] && $infoEtiqueta[$i]['MUESTRA'] == $infoEtiqueta[$e]['MUESTRA']) {
                     #$arregloEtiqueta = array('ABREVIATURA' => $infoEtiqueta[$e]['ABREVIATURA']);
                     #$arregloEtiqueta = array('ABREVIATURA' => $infoEtiqueta[$e]['ABR']);
-                    $arregloEtiqueta = array('ABREVIATURA' => "A;LSDKFJA;LSK A;SLDKFJ A;LKSDFJ ;ALSKDFJ ;ASLDKFJ A;SLKDJF ;ASLDKFJ A;SLDKFJ A;SDLKFJ A;SLDKFJ A;SDLKFJ A;SLDKFJ A;SLDKDFJ ;ASLDKJF A;SLDKFJ ;LKAJSDF ;ALSJDKF A;LSKDFJ ;ALKSDJF");
+                    $arregloEtiqueta = array('ABREVIATURA' => $infoEtiqueta[$e]['ABR']);
                     array_push($arrayEtiquetaEstudios, $arregloEtiqueta);
                 }
             }
