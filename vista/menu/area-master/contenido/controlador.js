aa470b4a4d7faff911233c3d5e9368eae45d3c78
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2022-12-28 10:03:29 -0600

    cambios

--- vista/menu/area-master/contenido/controlador.js
+++ vista/menu/area-master/contenido/controlador.js
@@ -2,7 +2,7 @@ var tablaContenido, areaActiva;
 var dataListaPaciente = {
   api: 7
 };
-var selectPacienteArea, hash;
+var selectPacienteArea, hash, formulario;
 //Variable para guardar los servicios de un paciente seleccionado
 var selectEstudio = new GuardarArreglo();
 var selectrue = 0;
@@ -19,18 +19,23 @@ function hasLocation() {
   if (validarVista(hash) == true) {
     switch (hash) {
       case "IMAGENOLOGIA":
+        formulario = "";
         obtenerContenidoVistaMaster(7, 'Resultados de Imagenología');
         break;
       case "RX":
+        formulario = "";
         obtenerContenidoVistaMaster(8, 'Resultados de Rayos X');
         break;
       case "ESPIROMETRIA":
+        formulario = "";
         obtenerContenidoVistaMaster(5, 'Resultados de Espirometría');
         break;
       case "AUDIOMETRIA":
+        formulario = "";
         obtenerContenidoVistaMaster(4, 'Resultados de Audiometría');
         break;
       case "OFTALMOLOGIA":
+        formulario = "formSubirInterpretacionOftalmo";
         obtenerContenidoVistaMaster(3, 'Resultados de Oftalmología', 'contenido_oftalmologia.php');
         break;
       default:
@@ -41,6 +46,17 @@ function hasLocation() {
 
 }
 
+
+
+//Notas de cosas necesarias:
+/*
+  -Recibir el confirmado de si un resultado o interpretacion ya ha sido subido (si existe resultado)
+  -Poder consultar las capturas y mandarlas ordenadas por pruebas
+  -Guardar la fecha de registro de resultado y quien lo cargó
+  -Generar el reporte de oftalmo y guardarlo junto a los campos
+
+
+*/
 function obtenerContenidoVistaMaster(area, titulo, contenidoHTML = 'contenido.php') {
   areaActiva = area;
   $.post("contenido/" + contenidoHTML, async function (html) {
@@ -52,8 +68,17 @@ function obtenerContenidoVistaMaster(area, titulo, contenidoHTML = 'contenido.ph
       area_id: areaActiva
     }
   }).done(function () {
-    // Datatable
-    $.getScript("contenido/js/vista-tabla.js")
+    switch (area) {
+      case 3:
+        // Datatable
+        $.getScript("contenido/js/oftalmo-tabla.js")
+        break;
+
+      default:
+        // Datatable
+        $.getScript("contenido/js/vista-tabla.js")
+        break;
+    }
     // Botones
     $.getScript("contenido/js/area-botones.js")
 
