28c04c19870db15db76ba7b33e9f9fb8460a186d
Josue de la Cruz <arellanox0392@gmail.com>
2022-12-28 12:36:08 -0600

    update oftalmologia ai
    se agrega la validacion del token

--- api/oftalmologia_api.php
+++ api/oftalmologia_api.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
 include_once "../clases/master_class.php";
+require_once "../clases/token_auth.php";
+include_once "../clases/Pdf.php";
+
+
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+if (!$tokenValido) {
+    $tokenVerification->logout();
+    exit;
+}
+
 
 $master = new Master();
 $api = $_POST['api'];
