c3d365d13a49d8b68afb333fd40f64b8dbc5558e
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2022-12-29 16:31:07 -0600

    Cambio de vista de Areas

--- vista/menu/area-master/modals/controlador.js
+++ vista/menu/area-master/modals/controlador.js
@@ -1,7 +1,5 @@
 $.post("modals/a_modals.php", function (html) {
   $("#modals-js").html(html);
-  // Modal para agregar interpretacion
-  $.getScript("modals/js/ar_subirprueba_area.js");
   // Modal para agregar capturas
   $.getScript("modals/js/ar_subircapturas_area.js");
   // Modal para agregar capturas
