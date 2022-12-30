38a1bb2f41b51388d5a4eabe69b58a59b0a9b959
Josue de la Cruz <arellanox0392@gmail.com>
2022-12-30 15:39:14 -0600

    update rayosx api
    se agrega las api para guardar interpretacion case 1, capturas en el case 2 y para reucperar la informarcion del turno en el case 3 [interpretacion y capturas]

--- api/rayosx_api.php
+++ api/rayosx_api.php
@@ -13,48 +13,66 @@ if (!$tokenValido) {
 
 $master = new Master();
 $id_rayo = $_POST['id_rayo'];
-$api = $_POST['case'];
+$api = $_POST['api'];
 $turno_id = $_POST['turno_id'];
 $usuario = $_SESSION['id'];
 $servicio_id = $_POST['servicio_id'];
-
+$comentario = $_POST['comentario'];
 
 switch ($api) {
     case 1:
         # subir interpretacion de rayos x.
-        
+        # la variable tipo significa que lo que estamos subiendo es una interpretacion.
+        $tipo = 1;
         #creamos el directorio donde se va a guardar la informacion del turno
-        $dir = "../reportes/modulo/rayosx/$turno_id/";
-        $r = $master->createDir($dir);
+        $ruta_saved = "reportes/modulo/rayosx/$turno_id/interpretacion/";
+        $r = $master->createDir("../".$ruta_saved);
+        
 
         if($r!=1){
-            $response = "No se puedo crear el directorio de carpetas";
+            $response = "No se pudo crear el directorio de carpetas. Interpretacion.";
             break;
         }
 
-        $imagenes = $master->guardarFiles($_FILES, "capturas", $dir, "CAPTURA_RX_$turno_id");
-        $interpretacion = $master->guardarFiles($_FILES, "interpretacion", $dir, "INTERPRETACION_RX_$turno_id");
+        #$imagenes = $master->guardarFiles($_FILES, "capturas", $dir, "CAPTURA_RX_$turno_id");
+        $interpretacion = $master->guardarFiles($_FILES, "interpretacion", "../".$ruta_saved, "INTERPRETACION_RX_$turno_id");
 
-        if(!empty($interpretacion)){
-            $tipo = 1;
-        } else {
-            $tipo = 2;
-        }
-
-        $response = $master->insertByProcedure('sp_rayosx_resultados_g',[$id_rayo,$turno_id,$interpretacion[0]['url'],json_encode($imagenes),$usuario,$tipo,$servicio_id]);
+        $response = $master->insertByProcedure('sp_rayosx_resultados_g',[null,$turno_id,$interpretacion[0]['URL'],NULL,$usuario,$tipo,null,null]);
         break;
 
     case 2:
-        # recuperar los resultados de rayos x
-        $res = $master->getByProcedure('sp_rayosx_resultados_b',[$id_rayo,$turno_id]);
-        $response = [];
-        foreach($response as $item){
-            $item['CAPTURAS'] = json_decode($item['CAPTURAS'], true);
-            $response[] = $item;
+        # Subir las capturas de los servicvios en cuestion.
+        $tipo = 2;
+        #creamos el directorio donde se va a guardar la informacion del turno
+        $ruta_saved = "reportes/modulo/rayosx/$turno_id/capturas/";
+        $r = $master->createDir("../".$ruta_saved);
+        
+
+        if($r!=1){
+            $response = "No se pudo crear el directorio de carpetas. Capturas.";
+            break;
         }
+
+        $capturas = $master->guardarFiles($_FILES, "capturas", "../" . $ruta_saved, "CAPTURAS_RX_$turno_id");
+
+        $response = $master->insertByProcedure("sp_rayosx_resultados_g", [null,$turno_id,null,json_encode($capturas),$usuario,$tipo,$servicio_id,$comentario]);
+  
+        break;
+
+    case 3:
+        # recuperar los resultados de rayos x
+        $response = $master->getByNext('sp_rayosx_resultados_b',[$id_rayo,$turno_id]);
+        $capturas = [];
+        foreach($response[1] as $current){
+            $current['CAPTURAS'] = json_decode($current['CAPTURAS'], true);
+            $capturas[] = $current;
+        }
+
+        $reponse[0][0]['CAPTURAS'] = $capturas;
+        
         break;
     default:
-        $response = "Api no definida.";
+        $response = "Api no definida...";
         break;
 }
 
