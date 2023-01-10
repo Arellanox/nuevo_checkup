bdbdac23a42b996179f0506567203f317848ff40
Henry Garcia <henry_garcia1996@hotmail.com>
2023-01-10 15:02:56 -0600

    ultrasonido

--- pdf/views/invoice/rayos.php
+++ pdf/views/invoice/rayos.php
@@ -59,12 +59,12 @@
                 margin-bottom: 2px;
             }
             h2{
-                font-size: 20px;
-                margin-top: 2px;
-                margin-bottom: 2px;
+                font-size: 18px;
+                margin-top: 18px;
+                margin-bottom: 10px;
                 text-align: center;
                 background-color: rgba(215, 222, 228, 0.748);
-                padding-top: 10px;
+                /* padding-top: 10px; */
             }
 
             h3{
@@ -84,7 +84,7 @@
             }
 
             p{
-                font-size: 14px;
+                font-size: 15px;
             }
 
             strong{
@@ -193,7 +193,7 @@
     <?php 
         
         // para el path del logo 
-        $ruta = file_get_contents('../pdf/public/assets/icono_reporte.png');
+        $ruta = file_get_contents('../pdf/public/assets/icono_reporte_checkup.png');
         $encode = base64_encode($ruta);
 
         // Para la firma se requiere mandar la "firma" tambien en base 64 e incrustarlo como en el ejemplo de arriba,
@@ -217,7 +217,7 @@
                             <h4>
                                 DIAGNOSTICO BIOMOLECULAR S.A.de C.V. <br>
                                 Clínica Checkup <br>
-                                Rayos X
+                                Ultrasonido
                             </h4>
                         </td>
                         <td class="col-izq"  style="border-bottom: none; text-align:center;">
@@ -235,20 +235,66 @@
                     <tr>
                         <td style="text-align: center; border-style: solid none solid none; ">
                             <h3>
-                                Ultrasonidos
+                                Ultrasonido
                             </h3>
                         </td>
                     </tr>
                 </tbody>
             </table>
-            <p>Aqui va el encabezado y es el espacio disponible hasta donde llegue el titulo siguiente.</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <td class="col-left"  style="border-bottom: none">
+                            No. Identificación: <strong> <?php echo $encabezado->FOLIO_IMAGEN;?> </strong> 
+                        </td>
+                        <td class="col-center"  style="border-bottom: none">
+                            Edad: <strong style="font-size: 12px;"> <?php echo $encabezado->EDAD;?> años</strong> 
+                        </td>
+                        <td class="col-right"  style="border-bottom: none">
+                            Sexo: <strong style="font-size: 12px;"><?php echo $encabezado->SEXO;?> </strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="col-left"  style="border-bottom: none">
+                            Nombre: <strong style="font-size: 12px;"> <?php echo $encabezado->NOMBRE;?> </strong>  
+                        </td>
+                        <td class="col-center"  style="border-bottom: none">
+                            Fecha de Nacimiento: <strong style="font-size: 12px;"> <?php echo $encabezado->NACIMIENTO;?> </strong>
+                        </td>
+                        <td class="col-right"  style="border-bottom: none">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="col-left"  style="border-bottom: none">
+                            <?php echo (isset($encabezado->PASAPORTE)) ? "Pasaporte: <strong>".$encabezado->PASAPORTE."</strong>" : ""; ?>
+                        </td>
+                        <td class="col-center"  style="border-bottom: none">
+                            Fecha de Resultado:     <strong style="font-size: 12px;"><?php echo $encabezado->FECHA_RESULTADO_IMAGEN;?> </strong>
+                        </td>
+                        <td class="col-right"  style="border-bottom: none">
+                            <!-- Tipo de Muestra: <strong>Sangre</strong> -->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="col-left"  style="border-bottom: none">
+                            Procedencia: <strong style="font-size: 12px;"><?php echo $encabezado->PROCEDENCIA;?> </strong>
+                        </td>
+                        <td class="col-center"  style="border-bottom: none">
+                            <?php echo isset($encabezado->MEDICO_TRATANTE) ? "Médico Tratante: <strong style='font-size: 12px;'>". $encabezado->MEDICO_TRATANTE . "</strong>" : "" ;?> 
+                        </td>
+                        <td class="col-right"  style="border-bottom: none">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- <p>Aqui va el encabezado y es el espacio disponible hasta donde llegue el titulo siguiente.</p> -->
         </div>
         
         <div class="footer">
             <table>
                 <tbody>
                     <tr class="col-foot-one">
-                        <td colspan="12" style="text-align: right; padding-right: 0;"><strong>Atentamente</strong></td>
+                        <td colspan="12" style="text-align: right; padding-right: 0;"><strong style="font-size: 12px;">Atentamente</strong></td>
                     </tr>
                     <tr class="col-foot-two" >
                         <td colspan="10">
@@ -262,56 +308,35 @@
                             <a target="_blank" href="#"> <img src='<?= $qr[1] ?>' alt='QR Code' width='110' height='110'> </a>
                         </td>
                         <td colspan="6" style="text-align: right; width: 50%; padding-top: 30px; margin-bottom: -25px">
-                            <strong >Q.F.B. NERY FABIOLA ORNELAS RESENDIZ    <br>UPCH - Cédula profesional: 09291445</strong>
+                            <strong  style="font-size: 12px;">Q.F.B. NERY FABIOLA ORNELAS RESENDIZ    <br>UPCH - Cédula profesional: 09291445</strong>
                         </td>
                     </tr>
                 </tbody>
             </table>
             <hr style="margin-top: -20px; height: 0.5px; background-color: black ;">
-            <p style="text-align: center;"><small><strong>Avenidad Universidad S/N Colonia Casa Blanca, Villahermosa, Tabasco - Teléfono: 993 131 00 42 Correo electrónico: biologia.molecular@hguadalupe.com</strong></small></p>
+            <p style="text-align: center;"><small><strong style="font-size: 12px;">Avenida José Pagés Llergo No. 150  Interior 1, Colonia Arboledas, Villahermosa Tabasco, C.P. 86079, Teléfono:</strong> <strong style="color:red; font-size: 12px;"> 993 131 00 42 </strong> </br> <strong style="font-size: 12px;">Correo electrónico:</strong> <strong style="color:red; font-size: 12px;">hola@bimo.com.mx</strong></small></p>
         </div> 
-
-        <!--  <div class="footer">
-            <table>
-                <tbody>
-                    <tr class="col-foot-one">
-                        <td colspan="12" style="text-align: center; padding-right: 0;"><strong>Atentamente</strong></td>
-                    </tr>
-                    <tr class="col-foot-two" >
-                        <td colspan="3" style="text-align: center; ">
-                            <a target="_blank" href="<?php echo $qr[0]; ?>"> <img style="margin-bottom: -30px" src='<?= $qr[1] ?>' alt='QR Code' width='110' height='110'> </a>
-                        </td>
-                        <td colspan="3" style="text-align: left;">
-                            <?php echo "<img style='position:absolute;' src='data:image/png;base64, " . $encode_firma . "' height='80px'> " ?>
-                        </td>
-                    </tr>
-                    <tr class="col-foot-three"  style="font-size: 13px;">
-                        <td colspan="12" style="text-align: center; width: 50%; ">
-                            <strong >Q.F.B. NERY FABIOLA ORNELAS RESENDIZ    <br>UPCH - Cédula profesional: 09291445</strong>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <hr style="margin-top: 0px; height: 0.5px; background-color: black ;">
-            <p style="text-align: center; margin-top: 0px"><small><strong>Avenida José Pagés Llergo No. 150  Interior 1, Colonia Arboledas, Villahermosa Tabasco, C.P. 86079, Teléfono: 993 131 00 42 
-            Correo electrónico: hola@bimo.com.mx</strong></small></p>
-        </div>-->
-
-
 
         <!-- body -->
         <div class="invoice-content">
-            <h2>RADIOGRAFÍA DE TORAX</h2>
-            <h4>Hallazgos</h4>
-            <p>
-            Tórax normolíneo. Tejidos blandos perifericos sin datos de lesión. La densidad ósea se encuentra conservada, las estructuras óseas sin alteración. Hemidiafragmas en adecuada posición, de contornos regulares bien definidos, los recesos cardiofrénicos y costodiafragmático se encuentra libres. Campos pulmonares adecuadamente distendidos, no se observaron lesiones nodulares, zonas de consolidación ni infiltrados. La trama broncovascular encuentra conservada. La tráquea central. El corazón de tamaño y morfología normal.
-            </p>
-            <p> <strong>Interpretación: </strong> Estudio dentro de parámetros normales. Realizar correlación clínica.</p>
-
-            <h2>RADIOGRAFIA DE COLUMNA LUMBOSACRA</h2>
-            <h4>Hallazgos</h4>
-            <p>Los cuerpos vertebrales se encuentran desviadosos hacia la izquierda, ángulo de Cobb de 40°, la lordosis se encuentra conservada, ángulo de Ferguson de 340°. Los cuerpos vertebrales son normales en forma, número y tamaño, no hay datos de listesis. Los espacios intersomáticos tienen amplitud conservada. Las cabezas femorales tienen morfología esférica, se proyectan a la misma altura, las articulaciones coxofemorales se encuentran conservadas.</p>
-            <p> <strong>Interpretación: </strong> Columna lumbar de actitud escoliótica, de convexidad izquierda. Realizar correlación clínica.</p>
+            <?php
+                $count = 0;
+                foreach ($resultados->ESTUDIOS as $key => $resultado) {
+                    echo "<h2 style='padding-bottom: 8px; padding-top: 8px;'>" . $resultado->ESTUDIO . "</h2>";
+                    echo "<h4 style='line-height: 1.5: padding-top: 20px;'>Hallazgos</h4>";
+                    echo "<p style='line-height: 1.5'>". $resultado->HALLAZGO."</p>";
+                    echo "<p style='line-height: 1.5'><strong>Interpretación: </strong>". $resultado->INTERPRETACION;
+                    if($resultado->COMENTARIO != "" || $resultado->COMENTARIO != null){
+                        echo "<strong>Comentario: </strong>". $resultado->COMENTARIO ."</p><br>" ;
+                    }
+                    $count ++;
+                    if($count % 2 == 0){
+            ?>
+                        <div class="break"></div>
+            <?php
+                    }
+                }
+            ?>
         </div>
     </body>
 </html>
