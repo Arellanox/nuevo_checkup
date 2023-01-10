e8fdab4ddc118606700116be5dcc29c6596bbc03
Josue de la Cruz <arellanox0392@gmail.com>
2023-01-10 16:27:27 -0600

    updates audiometria

--- api/audiometria_api.php
+++ api/audiometria_api.php
@@ -1,3 +1,19 @@
 <?php
 session_start();
+require_once "../clases/token_auth.php";
+include_once '../clases/master_class.php';
+include_once "../clases/Pdf.php";
+
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+if (!$tokenValido) {
+    $tokenVerification->logout();
+    exit;
+}
+
+$master = new Master();
+$api = $_POST['api'];
+
+$usuario = $_SESSION['id'];
+
 ?>
