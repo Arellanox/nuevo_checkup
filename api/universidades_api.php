97738b456cfa22cacfc1cf484040074755ee14b7
Josue de la Cruz <arellanox0392@gmail.com>
2023-01-19 16:36:32 -0600

    creacion de universidades api

---
+++ api/universidades_api.php
@@ -0,0 +1,37 @@
+<?php
+include "../clases/master_class.php";
+require_once "../clases/token_auth.php";
+
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+if (!$tokenValido) {
+    $tokenVerification->logout();
+    exit;
+}
+
+$master = new Master();
+$api = $_POST['api'];
+
+$id_universidad = $_POST['id_universidad'];
+$descripcion = $_POST['descripcion'];
+
+switch ($api) {
+    case 1:
+        #inserta y actualiza cuando le envias el id de la universidad.
+        $response = $master->insertByProcedure("sp_universidades_g", [$id_universidad, $descripcion]);
+        break;
+    case 2:
+        # buscar
+        $respnse = $master->getByProcedure("sp_universidades_b", [$id_universidad]);
+        break;
+    case 3:
+        #eliminar
+        $response = $master->deleteByProcedure("sp_universidades_e", [$id_universidad]);
+
+    default:
+        $response = "API no definida.";
+        break;
+}
+
+echo $master->returnApi($response);
+?>
