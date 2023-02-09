a8f796567078c290c443cfcc4d5cecf9f99abbc7
Josue de la Cruz <arellanox0392@gmail.com>
2023-02-09 13:22:30 -0600

    update turnos

--- api/turnos_api.php
+++ api/turnos_api.php
@@ -235,7 +235,7 @@ switch ($api) {
     case 14:
         # Devuelve la ruta del reporte del turno para laboratorio clinico.
         $response = $master->getByProcedure("sp_recuperar_reportes_confirmados", [$id_turno, 6, 1]);
-        $response = $master->cleanAttachingFiles($response);
+        //$response = $master->cleanAttachingFiles($response);
         break;
 
     default:
