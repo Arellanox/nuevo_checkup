e86c2943ad720a333b1b504511d126c6ee10808b
Josue de la Cruz <arellanox0392@gmail.com>
2023-02-21 15:40:27 -0600

    librerira pdf

--- dice.php
+++ dice.php
@@ -1,61 +1,116 @@
 <?php
-echo $_SERVER['SERVER_NAME'];
-$arr1 = array("hoola"=>1,2,3);
-$arr2 = array(4,5,6);
+require_once("lib/libmergpdf/vendor/autoload.php");
+use iio\libmergepdf\Merger;
+use iio\libmergepdf\Pages;
+$merger = new Merger;
 
-$arr = array_merge($arr1, $arr2);
+$merger->addFile("electro_img/Brochure Empresarial TGC.pdf");
+$merger->addFile("electro_img/CONSTANCIA BLANCA E. BARRERA FIGUEROA.pdf");
+$merger->addFile("electro_img/nery_20230201104901.pdf");
 
-print_r($arr);
+$createpdf = $merger->merge();
 
-echo date("dmY_His");
-class Dice {
-    public $num_sides;
+// echo $_SERVER['DOCUMENT_ROOT'];
 
-    function Dice(){
-        $this->num_sides = 6;
-    }
 
-    function setNumSides($sides){
-        $this->num_sides = $sides;
-    }
+// $path.= file_get_contents('bimo.pdf');
+// $path.= file_get_contents('nery.pdf');
 
-    function getNumSides(){
-        return $this->num_sides;
-    }
 
-    function roll($sides){
-        $roll = rand(1,$sides);
+
+//==============================================================================
+// $dir ="examples";
+// if(!mkdir($dir)){
+//     echo "no se pudo crear el directorio";
+// }else {
+//     echo "todo bien ";
+// }
+
+// #crear y escribir algo en un archivo.
+// $fh = fopen('test.zip', 'a');
+// // fwrite($fh, '<h1>Hello world!</h1>');
+// fclose($fh);
+
+// # eliminar un archivo.
+// unlink("examples/test.html");
+
+// # copiar un archivo.
+// $file = "checkup.sql";
+// $newfile = "$dir/checkup.sql.bak";
+
+// if(!copy($file,$newfile)){
+//     echo "Error al copiar un archivo.";
+// }
+
+// //zipping archives.
+// $zip = new ZipArchive;
+// if ($zip->open('test.zip') === TRUE) {
+//     $zip->addFile('examples/checkup.sql.bak', 'new.sql');
+//     $zip->close();
+//     echo 'ok';
+// } else {
+//     echo 'failed';
+// }
+
+
+
+// echo $_SERVER['SERVER_NAME'];
+// $arr1 = array("hoola"=>1,2,3);
+// $arr2 = array(4,5,6);
+
+// $arr = array_merge($arr1, $arr2);
+
+// print_r($arr);
+
+// echo date("dmY_His");
+// class Dice {
+//     public $num_sides;
+
+//     function Dice(){
+//         $this->num_sides = 6;
+//     }
+
+//     function setNumSides($sides){
+//         $this->num_sides = $sides;
+//     }
+
+//     function getNumSides(){
+//         return $this->num_sides;
+//     }
+
+//     function roll($sides){
+//         $roll = rand(1,$sides);
         
-        return $roll;
-    }
-}
+//         return $roll;
+//     }
+// }
 
-$dados = array();
+// $dados = array();
 
-for ($i=0; $i < 3; $i++) { 
-    $dados[] = new Dice();
-}
+// for ($i=0; $i < 3; $i++) { 
+//     $dados[] = new Dice();
+// }
 
 
-foreach($dados as $dado){
-    $dado->setNumSides(12);
-}
+// foreach($dados as $dado){
+//     $dado->setNumSides(12);
+// }
 
-$acumulador = 0;
+// $acumulador = 0;
 
-foreach($dados as $dado){
-    $resultado = $dado->roll($dado->getNumSides());
-    echo "Este dado cayó en : ".$resultado;
-    echo "<br>";
-    $acumulador = $acumulador + $resultado;
-}
+// foreach($dados as $dado){
+//     $resultado = $dado->roll($dado->getNumSides());
+//     echo "Este dado cayó en : ".$resultado;
+//     echo "<br>";
+//     $acumulador = $acumulador + $resultado;
+// }
 
-if($acumulador==36){
-    echo "<h1>¡Ganaste!</h1>";
-}else{
-    echo "<h1>Intenta de nuevo</h1>";
-}
+// if($acumulador==36){
+//     echo "<h1>¡Ganaste!</h1>";
+// }else{
+//     echo "<h1>Intenta de nuevo</h1>";
+// }
 
-print_r($dados);
+// print_r($dados);
 
 ?>
