1ccda122f14c0de78d016c0d49a763c891978e8f
Josue de la Cruz <arellanox0392@gmail.com>
2023-03-04 10:49:19 -0600

    updates turnero

--- api/turnero_api.php
+++ api/turnero_api.php
@@ -2,7 +2,7 @@
 require_once "../clases/master_class.php";
 require_once "../clases/token_auth.php";
 include_once "../clases/Pdf.php";
-
+include_once "../clases/turnero_class.php";
 
 $tokenVerification = new TokenVerificacion();
 $tokenValido = $tokenVerification->verificar();
@@ -12,10 +12,12 @@ if (!$tokenValido) {
 }
 
 #api
-$api = isset($_POST['api']) ? $_POST['api'] : '';
+$api = $_POST['api'];
 
-$response = "";
+$area = $_POST['area_id'];
+
 $master = new Master();
+$listaGlobal = null ;
 switch ($api) {
     case 1:
         $master = new Master();
@@ -23,10 +25,70 @@ switch ($api) {
         $infoPaciente = $master->getByProcedure('sp_pantalla_turnero', [null]);
         print_r($infoPaciente);
         break;
-    
+    case 2:
+        # llamar paciente
+        if(empty($listaGlobal)){
+            # si la listaGlobal$listaGlobal esta vacia, la llenamos
+            fillSessionList($master,$area);
+
+            if(empty($listaGlobal)){
+                # si la lista sigue vacia despues de llamar el sp,
+                # significa que no hay pacientes para esa area.
+                $response = "Nada por aquí, nada por allá.";
+            } else {
+                # si la lista no esta vacia, llamamos al primer paciente aceptado.
+                $response = current($listaGlobal->getPacientes());
+            }
+        }
+        break;
+    case 3:
+        # saltar paciente
+        if(empty($listaGlobal)){
+            fillSessionList($master,$area);
+            if(empty($listaGlobal)){
+                $response = "Nada por aquí.";
+            } else {
+                $listaGlobal->setPosition($listaGlobal->getPosition()+1);
+                setcookie("position",$listaGlobal->getPosition()+1);
+                $response = $listaGlobal->getNextPatient();
+            }
+        } else {
+            $listaGlobal->setPosition($listaGlobal->getPosition()+1);
+            setcookie("position",$listaGlobal->getPosition()+1);
+            $response = $listaGlobal->getNextPatient($listaGlobal->getPosition);
+        }
+        break;
+    case 4:
+        # pantalla turnero
+        $response = $master->getByProcedure('sp_turnero_pantalla',[]);
+        break;
     default:
         $response = "api no reconocida";
         break;
 }
 
 echo $master->returnApi($response);
+
+
+function fillSessionList($master, $area){
+    global $listaGlobal;
+    $response = $master->getByProcedure('sp_turnero_lista_pacientes',[$area]);
+    $listaGlobal = fillListPatient($response);
+}
+function fillListPatient($query){
+    
+    $lista = new ListaPacientes();
+    
+    foreach($query as $patient){
+        $paciente = new Paciente();
+        $paciente->setAreaId($patient['AREA_ID']);
+        $paciente->setEtiquetaTurno($patient['ETIQUETA_TURNO']);
+        $paciente->setPaciente($patient['PACIENTE']);
+        $paciente->setTurnoId($patient['TURNO_ID']);
+
+        $lista->pushPaciente($paciente);
+
+    }
+
+    return $lista;
+}
---
+++ clases/turnero_class.php
@@ -0,0 +1,75 @@
+<?php
+class Paciente {
+    public $area_id;
+    public $turno_id;
+    public $etiqueta_turno;
+    public $paciente;
+    function Paciente(){
+    }
+
+    function setAreaId($area){
+        $this->area_id = $area;
+    }
+
+    function getAreaId(){
+        return $this->area_id;
+    }
+
+    function setTurnoId($turno){
+        $this->turno_id = $turno;
+    }
+
+    function getTurnoId(){
+        return $this->turno_id;
+    }
+
+    function setEtiquetaTurno($etiqueta){
+        $this->etiqueta_turno = $etiqueta;
+    }
+
+    function getEtiquetaTurno(){
+        return $this->etiqueta_turno;
+    }
+
+    function setPaciente($pac){
+        $this->paciente = $pac;
+    }
+
+    function getPaciente(){
+        return $this->paciente;
+    }
+}
+
+
+class ListaPacientes {
+    public $pacientes;
+    public $position;
+    
+    function ListaPacientes(){
+        $this->pacientes = array();
+        $this->position = 0;
+    }
+
+    function pushPaciente($paciente){
+        array_push($this->pacientes,$paciente);
+    }
+
+    function getPacientes(){
+        return $this->pacientes;
+    }
+
+    function setPosition($position){
+        $this->position = $position;
+    }
+
+    function getPosition(){
+        return $this->position;
+    }
+
+    function getNextPatient(){
+        $patientList = $this->getPacientes();
+
+        return $patientList[$this->getPosition()];
+    }
+}
+?>
