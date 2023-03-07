60720997e25cddc165598003f58fbc418dcecd9c
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-03-07 16:45:03 -0600

    Navlinks turnos

--- vista/include/header/navbar-menu/navlink-droplist-areas.php
+++ vista/include/header/navbar-menu/navlink-droplist-areas.php
@@ -103,8 +103,6 @@ if ($_SESSION['vista']['CONSULTORIO'] == 1) : ?>
     </ul>
   </div>
 
-
-
   <!-- <a class="dropdown-a align-items-center rounded" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#ELECTROCARDIOGRAMA'; ?>">
     <i class="bi bi-activity"></i> Electrocardiograma
   </a> -->
@@ -123,4 +121,34 @@ if ($_SESSION['vista']['CONSULTORIO'] == 1) : ?>
   <a class="dropdown-a align-items-center rounded" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#OFTALMOLOGIA'; ?>">
     <i class="bi bi-eye"></i> Oftalmología
   </a>
+<?php endif; ?>
+
+
+
+
+<?php if (
+  $_SESSION['vista']['CONTROL_TURNOS_PANTALLA'] == 1 ||
+  $_SESSION['vista']['CONTROL_TURNOS'] == 1
+) : ?>
+  <a class="dropdown-a align-items-center rounded collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#board-turnos" aria-expanded="false">
+    <i class="bi bi-123"></i> Control de Turnos
+  </a>
+  <div class="collapse" id="board-turnos">
+    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+      <?php if ($_SESSION['vista']['CONTROL_TURNOS'] == 1) : ?>
+        <li>
+          <a class="dropdown-a align-items-center rounded" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/control-turnos/'; ?>">
+            <i class="bi bi-dot"></i> Menú
+          </a>
+        </li>
+      <?php endif; ?>
+      <?php if ($_SESSION['vista']['CONTROL_TURNOS_PANTALLA'] == 1) : ?>
+        <li>
+          <a href="<?php echo $https . $url . '/nuevo_checkup/vista/pantalla/control-turnos/'; ?>" class="dropdown-a align-items-center rounded" type="button">
+            <i class="bi bi-dot"></i> Pantalla
+          </a>
+        </li>
+      <?php endif; ?>
+    </ul>
+  </div>
 <?php endif; ?>
