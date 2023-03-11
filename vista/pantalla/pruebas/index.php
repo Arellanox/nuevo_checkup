42b38a4af9f8bece01a399a5d0b9387f164910dc
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-03-11 11:11:43 -0600

    fix 2

--- vista/pantalla/pruebas/index.php
+++ vista/pantalla/pruebas/index.php
@@ -47,7 +47,7 @@
         text.addEventListener('keydown', function({
             code
         }) {
-            if (['shift', 'NumpadEnter'].includes(code)) {
+            if (['Space', 'NumpadEnter'].includes(code)) {
                 const lastWord = this.value.split('\n');
                 renderText.innerHTML += `<div>${this.value}</div>`;
                 this.value = '';
