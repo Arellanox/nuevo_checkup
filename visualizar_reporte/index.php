162113b26b7fa5f98826553456312c6c9504b6b2
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-03-18 08:58:55 -0600

    visualizador

--- visualizar_reporte/index.php
+++ visualizar_reporte/index.php
@@ -30,9 +30,9 @@ $area_id = mb_convert_encoding(base64_decode(urldecode($_GET['area'])), 'UTF-8')
 
 
 // decomentar las siguientes 3 lineas para hacer las pruebas
-$api = 'soma';
-$turno_id = 220;
-$area_id = 2;
+// $api = 'soma';
+// $turno_id = 220;
+// $area_id = 2;
 
 
 switch ($api) {
