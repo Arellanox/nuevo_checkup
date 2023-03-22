67c650f08293c8c5ef81d7a07aba6028a7385853
Josue de la Cruz <arellanox0392@gmail.com>
2023-03-22 13:37:03 -0600

    UPDATES SOMATO

--- pdf/views/invoice/reporte_masometria.php
+++ pdf/views/invoice/reporte_masometria.php
@@ -284,7 +284,7 @@ if (!isset($qr)) {
             <tbody>
                 <tr>
                     <td class="col-left" style="border-bottom: none">
-                        No. Identificación: <strong style="font-size: 12px;"> <?php echo $encabezado->FOLIO_OFTALMO; ?> </strong>
+                        No. Identificación: <strong style="font-size: 12px;"> <?php echo "N/A"; ?> </strong>
                     </td>
                     <td class="col-center" style="border-bottom: none">
                         Edad: <strong style="font-size: 12px;"> <?php echo $encabezado->EDAD; ?> años</strong>
@@ -308,7 +308,7 @@ if (!isset($qr)) {
                         <?php echo (isset($encabezado->PASAPORTE)) ? "Pasaporte: <strong>" . $encabezado->PASAPORTE . "</strong>" : ""; ?>
                     </td>
                     <td class="col-center" style="border-bottom: none">
-                        Fecha de Resultado: <strong style="font-size: 12px;"><?php echo $encabezado->FECHA_RESULTADO_OFTALMO; ?> </strong>
+                        Fecha de Resultado: <strong style="font-size: 12px;"><?php echo $encabezado->FECHA_RESULTADO_MESO; ?> </strong>
                     </td>
                     <td class="col-right" style="border-bottom: none">
                         <!-- Tipo de Muestra: <strong>Sangre</strong> -->
