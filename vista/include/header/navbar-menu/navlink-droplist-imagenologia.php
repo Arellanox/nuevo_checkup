dc671f32e319e2f2f4d6827a977dd808a8ef3164
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-03-22 21:59:12 -0600

    rediseño

--- vista/include/header/navbar-menu/navlink-droplist-imagenologia.php
+++ vista/include/header/navbar-menu/navlink-droplist-imagenologia.php
@@ -1,21 +1,21 @@
 <!-- Imagenologia -->
 <?php if ($_SESSION['vista']['ULTRASONIDO'] == 1) : ?>
-    <a class="dropdown-a align-items-center rounded" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#ULTRASONIDO'; ?>">
+    <a class="dropdown-a align-items-center" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#ULTRASONIDO'; ?>">
         <i class="bi bi-person-video"></i> Ultrasonido
     </a>
 <?php endif; ?>
 <?php if ($_SESSION['vista']['ULTRASONIDOTOMA'] == 1) : ?>
-    <a class="dropdown-a align-items-center rounded" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#ULTRASONIDOTOMA'; ?>">
+    <a class="dropdown-a align-items-center" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#ULTRASONIDOTOMA'; ?>">
         <i class="bi bi-person-bounding-box"></i> Capturas de Ultrasonido
     </a>
 <?php endif; ?>
 <?php if ($_SESSION['vista']['RX'] == 1) : ?>
-    <a class="dropdown-a align-items-center rounded" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#RX'; ?>">
+    <a class="dropdown-a align-items-center" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#RX'; ?>">
         <i class="bi bi-universal-access"></i> Rayos X
     </a>
 <?php endif; ?>
 <?php if ($_SESSION['vista']['RXTOMA'] == 1) : ?>
-    <a class="dropdown-a align-items-center rounded" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#RXTOMA'; ?>">
+    <a class="dropdown-a align-items-center" type="button" href="<?php echo $https . $url . '/nuevo_checkup/vista/menu/area-master/#RXTOMA'; ?>">
         <i class="bi bi-universal-access-circle"></i> Capturas de Rayos X
     </a>
 <?php endif; ?>
