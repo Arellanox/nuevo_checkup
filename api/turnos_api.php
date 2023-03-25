aff7851bcb5a111f92571405953b20d2f7ae1476
Josue de la Cruz <arellanox0392@gmail.com>
2023-03-25 09:29:02 -0600

    update turnos api
    correccion de laboratorio.

--- api/turnos_api.php
+++ api/turnos_api.php
@@ -158,6 +158,11 @@ switch ($api) {
 
             #$a = array($id_turno, $servicio_id, $resultado, $confirmar, $confirmado_por, $valor_absoluto);
             $response = $master->updateByProcedure('sp_subir_resultados', array($id_turno, $servicio_id, $resultado['RESULTADO'], $confirmar, $confirmado_por, $valor_absoluto));
+
+            if(!is_numeric($response)){
+                echo $master->returnApi($response);
+                exit;
+            }
         }
 
         // actualizamos las observaciones por los grupos en casa hijo de la tabla paciente detalle
