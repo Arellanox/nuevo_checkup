dc1e368008461185c378d48ea721721211a6bba3
Josue de la Cruz <arellanox0392@gmail.com>
2023-03-29 09:39:46 -0600

    create laboratorio servicios api

---
+++ api/laboratorio_servicios_api.php
@@ -0,0 +1,21 @@
+<?php
+require_once "../clases/master_class.php";
+require_once "../clases/token_auth.php";
+
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+if (!$tokenValido) {
+    $tokenVerification->logout();
+    exit;
+}
+
+$master = new Master();
+$api = $_POST['api']; 
+
+switch($api){
+    case 1:
+
+        break;
+}
+
+?>
