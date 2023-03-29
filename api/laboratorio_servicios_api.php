a0ab711046b836e58500ef6e53e215d8c5b42a5f
Josue de la Cruz <arellanox0392@gmail.com>
2023-03-29 13:49:39 -0600

    update laboratorio servicios api
    se agrega el case 1 para guardar los servicios completos

--- api/laboratorio_servicios_api.php
+++ api/laboratorio_servicios_api.php
@@ -1,90 +1,89 @@
 <?php
-
-use FontLib\Table\Type\post;
-
 require_once "../clases/master_class.php";
 require_once "../clases/token_auth.php";
 
 $tokenVerification = new TokenVerificacion();
 $tokenValido = $tokenVerification->verificar();
-// if (!$tokenValido) {
-//     $tokenVerification->logout();
-//     exit;
-// }
+if (!$tokenValido) {
+    $tokenVerification->logout();
+    exit;
+}
 
 $master = new Master();
 $api = $_POST['api'];
 
-/*$parametros = Array
-(
-    [descripcion] => servicio_nombre
-    [abreviatura] => SN
-    [clasificacion_id] => 14
-    [medida_id] => 17
-    [dias_entrega] => INMEDIATA
-    [codigo_sat_id] => 14
-    [indicaciones] => Ayuno 12 horas
-    [es_para] => 1
-    [muestra_valores] => 1
-    [local] => 1
-    [maquila_lab_id] => 1
-    [Grupo] => Array
-        (
-            [0] => 15
-            [978719] => 343
-        )
-
-    [Método] => Array
-        (
-            [0] => 18
-            [%0D%0A196326] => 14
-        )
-
-    [contenedores] => Array
-        (
-            [2] => Array
-                (
-                    [contenedor] => 5
-                    [muestra] => 6
-                )
-
-            [1] => Array
-                (
-                    [contenedor] => 5
-                    [muestra] => 6
-                )
-
-        )
-
-    [Equipo] => Array
-        (
-            [0] => 4
-        )
-
-    [valor_minimo] => <1
-    [valor_maximo] => 2>
-    [sexo_enum] => 1
-    [edad_inicial] => 1
-    [edad_final] => 30
-    [grupos] => 0
-    [producto] => 1
-    [area] => 6
-    [seleccionable] => 1
-    [para] => 3
-    [costos] => 1
-    [api] => 0
-        );*/
 $Grupo = $_POST['grupoExamen'];
 $contenedores = $_POST['contenedores'];
 $Equipo = $_POST['Equipo'];
-$Método = $_POST['Método';]
+$Método = $_POST['Método'];
+$id_servicio = $_POST['id_servicio'];
+$descripcion = $_POST['descripcion']; 
+$abreviatura = $_POST['abreviatura'];
+$clasificacion_id = $_POST['clasificacion_id'];
+$medida_id = $_POST['medida_id'];
+$dias_entrega = $_POST['dias_entrega'];
+$codigo_sat_id = $_POST['codigo_sat_id'];
+$indicaciones = $_POST['indicaciones'];
+$es_para = $_POST['es_para'];
+$muestra_valores = $_POST['muestra_valor'];
+$local = $_POST['local'];
+$maquila_lab_id = $_POST['maquila_lab_id'];
+$grupo = $_POST['grupoExamen']; # array
+$metodo = $_POST['Método']; # array
+$contenedores = $_POST['contenedores']; # array
+$equipo = $_POST['Equipo']; # array
+$valor_minimo = $_POST['valor_minimo'];
+$valor_maximo = $_POST['valor_maximo'];
+$sexo = $_POST['sexo_enum'];
+$edad_inicial = $_POST['edad_inicial'];
+$edad_final = $_POST['edad_final'];
+$es_grupo = $_POST['grupos'];
+$es_producto = $_POST['producto'];
+$area_id = $_POST['area'];
+$seleccionable = $_POST['selecionable'];
+$es_para = $_POST['para'];
+$costos = $_POST['costos'];
+
 
 switch ($api) {
     case 1:
-        print_r($master->getFormValues($Equipo));
-        echo json_encode($master->getFormValues($Equipo));
-
-        print_r($master->getFormValues($Método));
-        echo json_encode($master->getFormValues($Método));
+        $response = $master->insertByProcedure("sp_laboratorio_servicios_g",[
+            $id_servicio,
+            $descripcion,
+            $abreviatura,
+            $clasificacion_id,
+            $medida_id,
+            $dias_entrega,
+            $codigo_sat_id,
+            $indicaciones,
+            $muestra_valores,
+            $local,
+            $maquila_lab_id,
+            json_encode($master->getFormValues($grupo)),
+            json_encode($master->getFormValues($metodo)),
+            json_encode($master->getFormValues($contenedores)),
+            json_encode($master->getFormValues($equipo)),
+            $valor_minimo,
+            $valor_maximo,
+            $sexo,
+            $edad_inicial,
+            $edad_final,
+            $es_grupo,
+            $es_producto,
+            $area_id,
+            $seleccionable,
+            $es_para,
+            $costos,
+            $_SESSION['id']
+        ]);
+        break;
+    case 2:
+        # buscar servicio
+        
+    default:
+        $response = "Api no definida.";
         break;
 }
+
+echo $master->returnApi($response);
+?>
