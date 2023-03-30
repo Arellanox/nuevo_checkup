816218614cefa783646fb8dcdee5c9022800ae85
Josue de la Cruz <arellanox0392@gmail.com>
2023-03-30 16:36:03 -0600

    update recepcion

--- api/recepcion_api.php
+++ api/recepcion_api.php
@@ -20,6 +20,25 @@ $comentarioRechazo = $_POST['comentario_rechazo'];
 $identificacion = $_POST['identificacion']; #url
 $area_id = $_POST['area_id'];
 
+# trabajadores de la ujat
+$is_worker = $_POST['nuevo_trabajador']; # bit para saber si es ujat y se debe agregar la info del trabajador
+$e_id_trabajador = $_POST['id_trabajador'];
+$e_nombre = $_POST['nombre'];
+$e_paterno = $_POST['paterno'];
+$e_materno = $_POST['materno'];
+$e_edad = $_POST['edad'];
+$e_fecha_nacimiento = $_POST['fecha_nacimiento'];
+$e_num_trabajador = $_POST['num_trabajador'];
+$e_curp = $_POST['curp'];
+$e_pasaporte = $_POST['pasaporte'];
+$e_extranjero = $_POST['extranjero'];
+$e_genero = $_POST['genero'];
+$e_ures = $_POST['ures'];
+$e_categoria = $_POST['categoria'];
+$e_parentesco = $_POST['parentesco'];
+$e_diagnostico = $_POST['diagnostico'];
+$parametro = $_POST['parametro'];
+
 # reagendar
 $fecha_reagenda = $_POST['fecha_reagenda'];
 
@@ -59,6 +78,32 @@ switch ($api) {
         # aceptar o rechazar pacientes [tambien regresar a la vida]
         # enviar 1 para aceptarlos, 0 para rechazarlos, null para pacientes en espera
         // $response = $master->updateByProcedure('sp_recepcion_cambiar_estado_paciente', array($idTurno, $estado_paciente, $comentarioRechazo));
+
+        #========================================================================================
+        ##############AGREGAR TRABAJAOR DE LA UJAT###############################################
+
+        if(isset($is_worker) && $is_worker==1 && is_numeric($is_worker)){
+            $employee = $master->insertByProcedure("sp_trabajadores_empresas_g",[
+                $e_id_trabajador,
+                $e_nombre,
+                $e_paterno,
+                $e_materno,
+                $e_edad,
+                $e_fecha_nacimiento,
+                $e_num_trabajador,
+                $e_curp,
+                $e_pasaporte,
+                $e_extranjero,
+                $e_genero,
+                $e_ures,
+                $e_categoria,
+                $idTurno,
+                $e_parentesco,
+                $e_diagnostico
+                ]);
+        }
+        #========================================================================================
+
         $response = $master->getByNext('sp_recepcion_cambiar_estado_paciente', array($idTurno, $estado_paciente, $comentarioRechazo));
 
         $etiqueta_turno = $response[1];
@@ -240,10 +285,11 @@ switch ($api) {
         #'ID_PACIENTE', 'CURP.PASAPORTE.NOMBRE_COMPLETO.NACIMIENTO.NUMBER_TRABAJADOR'
         #Del trabajador para enviarte la ID
 
+        # recuperar la lista de lost trabajadores
+        $response = $master->getByProcedure("sp_trabajdores_empresas_b",[$e_id_trabajador,$parametro]);
         break;
-
     default:
-        # code...
+        $response = "Api no definida.";
         break;
 }
 
