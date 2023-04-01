64bebb810f7fd1ea470b06f73daa797735205a3a
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-04-01 13:53:21 -0600

    reporte

--- pdf/views/invoice/resultados.php
+++ pdf/views/invoice/resultados.php
@@ -288,7 +288,7 @@
                                         Fecha de Toma de Muestra: <strong style="font-size: 12px;"> <?php echo $encabezado->FECHA_TOMA; ?> </strong>
                                     </td>
                                     <td class="col-center" style="border-bottom: none">
-                                        Fecha de Resultado: <strong style="font-size: 12px;"><?php echo $encabezado->FECHA_RESULTADO; ?> </strong>
+                                        Fecha de Resultado: <strong style="font-size: 10.3px;"><?php echo $encabezado->FECHA_RESULTADO; ?> </strong>
                                     </td>
                                     <td class="col-right" style="border-bottom: none">
                                         <!-- Tipo de Muestra: <strong>Sangre</strong> -->
