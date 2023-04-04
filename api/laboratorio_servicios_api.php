35e8926fc0bcacfcb99050541adad60cd40458f8
Josue de la Cruz <arellanox0392@gmail.com>
2023-04-04 15:33:54 -0600

    update laboratorio servicios g

--- api/laboratorio_servicios_api.php
+++ api/laboratorio_servicios_api.php
@@ -1,13 +1,13 @@
 <?php
 require_once "../clases/master_class.php";
-// require_once "../clases/token_auth.php";
+require_once "../clases/token_auth.php";
 
-// $tokenVerification = new TokenVerificacion();
-// $tokenValido = $tokenVerification->verificar();
-// if (!$tokenValido) {
-//     $tokenVerification->logout();
-//     exit;
-// }
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+if (!$tokenValido) {
+    $tokenVerification->logout();
+    exit;
+}
 
 $master = new Master();
 $api = $_POST['api'];
@@ -64,7 +64,7 @@ $parametros = array(
     $utilidad,
     $precio_venta
 );
-echo "api ".$api;
+
 switch ($api) {
     case 1:
         $response = $master->insertByProcedure("sp_servicio_laboratorio_g",[
