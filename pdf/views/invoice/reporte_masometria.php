dde353ae7c17f7d1fb7b626909a3e39e2cd2bf2e
osezca <osezca@gmail.com>
2023-04-06 11:09:00 -0600

    Modificaciones de somatometria

--- pdf/views/invoice/reporte_masometria.php
+++ pdf/views/invoice/reporte_masometria.php
@@ -372,19 +372,19 @@ if (!isset($qr)) {
                     <strong style="font-size: 12px;"><?php echo $resultados->PESO; ?> kg</strong>
                 </td>
                 <td colspan="3"></td>
-                <td colspan="3" style="text-align: left;" class="cursive">Huesos</td>
+                <td colspan="2" style="text-align: left;" class="cursive">Huesos</td>
                 <td colspan="2" style="text-align: left;">
                     <strong style="font-size: 12px;"><?php echo $resultados->HUESOS; ?> mm</strong>
                 </td>
             </tr>
             <tr>
                 <td colspan="2" style="text-align: left;" class="cursive">Masa corporal</td>
-                <td colspan="2" style="text-align: left;">
+                <td colspan="3" style="text-align: left;">
                     <strong style="font-size: 12px;"><?php echo $resultados->MASA_CORPORAL; ?> kg/m2</strong>
                 </td>
                 <td colspan="3"></td>
-                <td colspan="3" style="text-align: left;" class="cursive">indice de masa corporal</td>
-                <td colspan="2" style="text-align: left;">
+                <td colspan="2" style="text-align: left;" class="cursive">indice de masa corporal</td>
+                <td colspan="3" style="text-align: left;">
                     <strong style="font-size: 12px;"><?php echo $resultados->INDICE_DE_MASA_CORPORAL; ?> %</strong>
                 </td>
             </tr>
