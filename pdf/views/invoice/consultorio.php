99a341d335deaffc99c63f6a837b6dfd331db45c
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-04-11 11:52:55 -0600

    correccionm

--- pdf/views/invoice/consultorio.php
+++ pdf/views/invoice/consultorio.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado de interpretación de Electrocardiograma</title>
+    <title>Resultado de interpretación de Historia Clinica</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <!-- <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">  -->
@@ -294,7 +294,7 @@ $encode_firma = base64_encode($ruta_firma);
         <?php
                 }
             }
-            echo '<div class="break"> </div>';
+            // echo '<div class="break"> </div>';
         }
         ?>
 
