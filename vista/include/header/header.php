1a9e5430e03475e5c41737d2f334d5ff974926a1
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-04-18 13:59:09 -0600

    Ext de areas (estetico)

--- vista/include/header/header.php
+++ vista/include/header/header.php
@@ -82,7 +82,7 @@ switch ($menu) {
                 <i class="bi bi-telephone-fill"></i> Ext.
               </a>
               <!-- Estos botones se cargan en el servidor desde el archivo del include -->
-              <ul class="dropdown-menu bg-navbar-drop dropdown-menu-end dropdown-menu-lg-start" aria-labelledby="dropExtTelefonos" style="padding: 0px;">
+              <ul class="dropdown-menu bg-navbar-drop dropdown-menu-end dropdown-menu-lg-start" aria-labelledby="dropExtTelefonos" style="padding: 0px; width: 200px">
                 <table class="table radiuszero" style="margin-bottom: 0px;">
                   <thead>
                     <tr>
@@ -100,8 +100,32 @@ switch ($menu) {
                       <td>1001</td>
                     </tr>
                     <tr>
-                      <th scope="row">Central Enfermería </th>
-                      <td>1001</td>
+                      <th scope="row">Central Enfermería</th>
+                      <td>1009</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Sala de Juntas</th>
+                      <td>1009</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Lab. Clínico</th>
+                      <td>1007</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Ultrasonido</th>
+                      <td>1006</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">TI</th>
+                      <td>1008</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Rayos X</th>
+                      <td>1010</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Toma de muestra</th>
+                      <td>1012</td>
                     </tr>
                   </tbody>
                 </table>
