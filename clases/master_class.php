b8881d2958db271f45996b5ba8c3525f8a6d3795
Josue de la Cruz <arellanox0392@gmail.com>
2023-04-21 16:01:33 -0600

    update master class
    Seleccionar la base de datos de acuerdo al nombre del servidor.

--- clases/master_class.php
+++ clases/master_class.php
@@ -36,12 +36,49 @@ class Master extends Miscelaneus
 
     function connectDb()
     {
+        $server = $_SERVER['SERVER_NAME'];
+
+        switch($server){
+            case "localhost":
+            case "helicebiologicos.com":
+                # nube. conexion de pruebas
+                $host = "212.1.208.201";
+                $dbname = "u808450138_checkup_copio";
+                $username = "u808450138_hola";
+                $password = ":N1TFmb0z";
+
+                // $host = "212.1.208.201";
+                // $dbname = "u808450138_capacitaciones";
+                // $username = "u808450138_capacitador";
+                // $password = '7$V1HPRSVa*2';
+                break;
+            case "drjb.com.mx":
+                $host = "212.1.208.201";
+                $dbname = "u808450138_capacitaciones";
+                $username = "u808450138_capacitador";
+                $password = '7$V1HPRSVa*2';
+                break;
+            case "bimo-lab.com":
+                # local
+                # $host = "212.1.208.201";
+                $host = 'localhost'; //Servidor
+                $dbname = "u808450138_checkup";
+                $username = "u808450138_bimo";
+                $password = "I?6m7]Pe0";
+                break;
+            default:
+                $host = 'localhost'; //Servidor
+                $dbname = "NONE";
+                $username = "NONE";
+                $password = "NONE";
+                break;
+        }
         // require_once 'pdoconfig.php';
-       // $host = "212.1.208.201";
-        $host = 'localhost'; //Servidor
-        $dbname = "u808450138_checkup";
-        $username = "u808450138_bimo";
-        $password = "I?6m7]Pe0";
+        // $host = "212.1.208.201";
+        // $host = 'localhost'; //Servidor
+        // $dbname = "u808450138_checkup";
+        // $username = "u808450138_bimo";
+        // $password = "I?6m7]Pe0";
 
         #local
         // $host = "localhost";
