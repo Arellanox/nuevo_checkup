ca2cd058db1d75ff977089ddb35d70bd3e52ec84
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-06-01 14:24:05 -0600

    correcccion de personal

--- personal/index.php
+++ personal/index.php
@@ -4,38 +4,43 @@ $id = $_GET['q'];
 $rutaArchivo = 'personal.json';
 $json = file_get_contents($rutaArchivo);
 $data = json_decode($json, true);
-// echo $id;
-// echo $data[$id];
-if (!empty($data[$id]))
+
+if (!empty($data[$id])) {
     echo "
-<!DOCTYPE html>
-<html>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Identificacion | bimo</title>
+            <link rel='icon' type='image/png' href='https://www.bimo-lab.com/archivos/sistema/LogoConFondoAppAndroid.png'>
+            <style>
+                body {
+                    margin: 0;
+                    padding: 0;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    background-image: url('$data[$id]');
+                    background-size: cover;
+                    background-position: center;
+                }
 
-<head>
-    <title>Identificacion | bimo</title>
-    <link rel='icon' type='image/png' href='https://www.bimo-lab.com/archivos/sistema/LogoConFondoAppAndroid.png'>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-image: url('$data[$id]');
-            background-size: cover;
-            background-position: center;
-        }
-        @media screen and (orientation: landscape) {
-            body {
-                background-size: contain;
-            }
-        }
-    </style>
-</head>
+                @media screen and (orientation: landscape) {
+                    body {
+                        background-size: contain;
+                    }
+                }
 
-<body>
-</body>
-
-</html>
-";
+                @media screen and (max-width: 767px) {
+                    body {
+                        display: block;
+                        text-align: center;
+                    }
+                }
+            </style>
+        </head>
+        <body>
+        </body>
+        </html>
+    ";
+}
