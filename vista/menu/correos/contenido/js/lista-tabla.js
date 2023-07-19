5d7ec2169c0f54698a41f0b51f2fc0969c54ae52
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-07-19 15:38:52 -0600

    Update lista-tabla.js

--- vista/menu/correos/contenido/js/lista-tabla.js
+++ vista/menu/correos/contenido/js/lista-tabla.js
@@ -63,9 +63,14 @@ function getNewView(url, filename) {
     // Crear una instancia inicial de AdobeDC.View
     let adobeDCView = new AdobeDC.View({ clientId: "cd0a5ec82af74d85b589bbb7f1175ce3", divId: "adobe-dc-view" });
 
+    var nuevaURL = "<URL del nuevo PDF>";
+
+    // Agregar un parámetro único a la URL para evitar la caché del navegador
+    nuevaURL += "?timestamp=" + Date.now();
+
     // Cargar y mostrar el nuevo PDF en el visor
     adobeDCView.previewFile({
-        content: { location: { url: url } },
+        content: { location: { url: nuevaURL } },
         metaData: { fileName: filename }
     });
 }
