d015d8df2aee2386f6c87d0a0e5c15157bb39c76
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-07-24 20:45:40 -0600

    Update header.php

--- vista/include/header/header.php
+++ vista/include/header/header.php
@@ -70,7 +70,7 @@ switch ($menu) {
 
       // echo $fecha_sumada; // imprime la nueva fecha en formato 'año-mes-día'
 
-      if (false && $menu != 'procedencia') :
+      if (true && $menu != 'procedencia') :
       ?>
 
         <img src="https://bimo-lab.com/nuevo_checkup/1724986_dbc8d.gif" style="width: 90px; z-index: 99; position: absolute; left: 40px; top: 12px; transform: rotate(0.04turn);" />
@@ -89,12 +89,13 @@ switch ($menu) {
               </div> -->
                 <div class="modal-body">
                   <div id="tsparticles"></div>
-                  <div style="position: relative; width: 100%; height: 0; padding-top: 154.5455%;
+                  <div style="position: relative; width: 100%; height: 0; padding-top: 140.9524%;
  padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
  border-radius: 8px; will-change: transform;">
-                    <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;" src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFnk5mlwcU&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+                    <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;" src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFpHsrQedc&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
                     </iframe>
                   </div>
+
 
                   <!-- <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFhaaU2rcM&#x2F;watch?utm_content=DAFhaaU2rcM&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Tarjeta vertical felicitación cumpleaños empleado empresa elegante dorado</a> de Bimo Talento Humano -->
                 </div>
