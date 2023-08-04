e1ea45368ae6881065336258e97ce1c1e53c231b
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-08-04 15:53:35 -0600

    Update FTDMultiplex.php

--- pdf/views/invoice/includes/FTDMultiplex.php
+++ pdf/views/invoice/includes/FTDMultiplex.php
@@ -48,7 +48,7 @@
 <br>
 <br>
 <p style="text-align:justify;"><strong>Comentarios: </strong>Esta prueba identifica en tiempo real la presencia de 7 tipos de Patógenos (<?php foreach ($body as $key => $value) {
-                                                                                                                                                echo $value->resultado;
+                                                                                                                                                echo $value->nombre;
                                                                                                                                                 if (count($body) - 1 > $key)
                                                                                                                                                     echo ',';
                                                                                                                                             } ?>) causantes de enfermedades tropicales; el médico tratante es quien realiza la interpretación de este resultado considerando los datos clínicos del paciente.
