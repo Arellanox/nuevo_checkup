274e704ed7f473ac77a67c155dfca011e3fdea2c
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-08-08 13:07:38 -0600

    Lista dinamica

--- vista/pacientes/servicios-disponibles/index.php
+++ vista/pacientes/servicios-disponibles/index.php
@@ -3,6 +3,8 @@
 
 session_start();
 include "../../variables.php";
+
+// Configuración de areas
 $menu = "lista_servicios";
 ?>
 <!DOCTYPE html>
@@ -15,6 +17,57 @@ $menu = "lista_servicios";
 
 <body class="" id="body-controlador"> </body>
 <script type="text/javascript">
+  // Definir el objeto que mapea los números de áreas a los nombres de áreas
+  const areasMap = {
+    1: "CONSULTORIO",
+    2: "SOMATOMETRÍA",
+    3: "OFTALMOLOGÍA",
+    4: "AUDIOMETRÍA",
+    5: "ESPIROMETRÍA",
+    6: "LABORATORIO CLÍNICO",
+    7: "IMAGENOLOGÍA",
+    8: "RAYOS X",
+    9: "PRUEBA DE ESFUERZO",
+    10: "ELECTROCARDIOGRAMA",
+    11: "ULTRASONIDO",
+    12: "LABORATORIO BIOMOLECULAR",
+    13: "CITOLOGÍA",
+    14: "NUTRICIÓN",
+    15: "COTIZACIONES",
+    16: "TICKET",
+    17: "CORTE",
+    18: "CARDIOLOGÍA",
+    19: "CHECKUPS"
+  };
+
+  // Obtener la cadena de consulta de la URL
+  const url = window.location.search;
+  const urlParams = new URLSearchParams(url);
+
+  // Obtener el valor de la variable desde los parámetros
+  const filter = urlParams.get("filter");
+
+  let strings = [];
+
+  // Verificar si la filter no es null ni está vacía
+  if (filter) {
+    // Convierte la cadena en un arreglo utilizando JSON.parse()
+    strings = JSON.parse(filter);
+  }
+
+  // Mapear los números de áreas a nombres de áreas en un nuevo arreglo
+  const areasString = strings.map(areaNumber => areasMap[areaNumber]);
+
+  // Si no hay áreas seleccionadas, llenar el arreglo con todos los nombres de áreas
+  if (areasString.length === 0) {
+    areasString.push(...Object.values(areasMap));
+  }
+
+  // Imprimir el arreglo de nombres de áreas
+  console.log(areasString);
+
+
+
   vista('<?php echo $menu; ?>', '<?php echo $https . $url . '/' . $appname . '/vista/menu/controlador/controlador.php'; ?>')
 
   function vista(menu, url) {
