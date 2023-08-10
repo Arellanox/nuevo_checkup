6d2a34cae825331634c7397afafd9e494edcebb3
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-08-10 09:51:27 -0600

    pruebas de laboratorio, grupos

--- vista/menu/laboratorio-estudios/contenido/js/grupos-tabla.js
+++ vista/menu/laboratorio-estudios/contenido/js/grupos-tabla.js
@@ -17,14 +17,53 @@ var tablaGrupos = $('#TablaGruposServicios').DataTable({
   columns: [
     { data: 'COUNT' },
     { data: 'DESCRIPCION' },
+    { data: 'ABREVIATURA' },
     { data: 'CLASIFICACION_EXAMEN' },
+    { data: 'ES_PARA' },
     { data: 'DESCRIPCION_AREA' },
-    { data: 'ACTIVO' },
+    { data: 'INDICACIONES' },
     // {defaultContent: 'En progreso...'}
   ],
   columnDefs: [
-    { "width": "3px", "targets": [0, 4] },
+    // { "width": "3px", "targets": [0, 4] },
+    { target: 0, title: '#', className: 'all' },
+    { target: 1, title: 'Descripción', className: 'all' },
+    { target: 2, title: 'Abreviatura', className: 'all' },
+    { target: 3, title: 'Clasificación', className: 'min-tablet' },
+    { target: 4, title: 'Dirigido', className: 'min-tablet' },
+    { target: 5, title: 'Area', className: 'desktop' },
+    { target: 6, title: 'Indicaciones', className: 'none' }
+  ],
+
+  dom: 'Blfrtip',
+  buttons: [
+    {
+      text: '<i class="bi bi-pencil-square"></i> Editar',
+      className: 'btn btn-pantone-7408',
+      action: function () {
+        if (array_selected != null) {
+          getDataFirst(1, array_selected['ID_SERVICIO'])
+        } else {
+          alertSelectTable();
+        }
+      }
+    },
+    {
+      text: '<i class="bi bi-box-seam"></i> Rellenar Grupo',
+      className: 'btn btn-pantone-7408',
+      action: function () {
+        if (array_selected != null) {
+          firstDataModal();
+          alertToast('Espere un momento..', 'info', 2500)
+        } else {
+          alertSelectTable();
+        }
+      }
+    },
   ],
 
 })
+
+inputBusquedaTable('TablaGruposServicios', tablaGrupos, [], [], '_', '_')
+
 selectDatatable("TablaGruposServicios", tablaGrupos, 1, 'servicios_api', 'estudio')
