d124f45e1970dafa0f5cd8660e9e7e772930e045
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-08-17 13:12:27 -0600

    Update lab-buttons.php

--- vista/include/header/navbar-menu/buttons/lab-buttons.php
+++ vista/include/header/navbar-menu/buttons/lab-buttons.php
@@ -1,18 +1,18 @@
-<?php if ($_SESSION['vista']['LABORATORIO_MUESTRA_1'] == 1) : ?>
+<?php if ($_SESSION['vista']['LABORATORIO_MUESTRA_1'] == 1 && $menu != 'muestras') : ?>
     <li class="nav-item">
         <a class="dropdown-a align-items-center" target="_blank" type="button" href="<?php echo $https . $url . '/' . $appname . '/vista/menu/muestras/#LABORATORIO_MUESTRA_1'; ?>">
             <i class="bi bi-droplet-half"></i> Toma de muestras
         </a>
     </li>
 <?php endif; ?>
-<?php if ($_SESSION['vista']['LABORATORIO'] == 1) : ?>
+<?php if ($_SESSION['vista']['LABORATORIO'] == 1 && $menu != 'Laboratorio') : ?>
     <li class="nav-item">
         <a class="dropdown-a align-items-center" target="_blank" type="button" href="<?php echo $https . $url . '/' . $appname . '/vista/menu/laboratorio/#LABORATORIO'; ?>">
             <i class="bi bi-heart-pulse"></i> Laboratorio Clínico
         </a>
     </li>
 <?php endif; ?>
-<?php if ($_SESSION['vista']['ESTUDIOS_LABORATORIO'] == 1) : ?>
+<?php if ($_SESSION['vista']['ESTUDIOS_LABORATORIO'] == 1 && $menu != 'ServiciosLab') : ?>
     <li class="nav-item">
         <a class="dropdown-a align-items-center" target="_blank" type="button" href="<?php echo $https . $url . '/' . $appname . '/vista/menu/laboratorio-estudios/#EstudiosLab'; ?>">
             <i class="bi bi-box2-heart"></i> Estudios
