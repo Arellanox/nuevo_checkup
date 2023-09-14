a71456555144689b4f46151263785abb8d298bc9
Josue de la Cruz <arellanox0392@gmail.com>
2023-09-14 13:22:06 -0600

    Update rayosx_api.php

--- api/rayosx_api.php
+++ api/rayosx_api.php
@@ -33,7 +33,8 @@ $id_imagen = $_POST['id_imagen'];
 $formulario = $_POST['servicios'];
 $hallazgo = $_POST['hallazgo'];
 $inter_texto = $_POST['inter_texto'];
-$host = /*isset($_SERVER['SERVER_NAME']) ? "http://localhost/nuevo_checkup/" : */ "https://bimo-lab.com/nuevo_checkup/";
+$host = $_SERVER['SERVER_NAME'] == "localhost" ? "http://localhost/practicantes/" : "https://bimo-lab.com/nuevo_checkup/";
+$host = $master->selectHost("localhost");
 $date = date("dmY_His");
 $confirmado = $_POST['confirmado'];
 
