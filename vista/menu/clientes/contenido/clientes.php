ffa3a45a0b790f42fc6864a8fb17ecd405e82866
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2023-09-14 12:43:34 -0600

    clientes

--- vista/menu/clientes/contenido/clientes.php
+++ vista/menu/clientes/contenido/clientes.php
@@ -1,57 +1,57 @@
 <div class="col-12 loader" id="loader" style="">
-  <div class="preloader" id="preloader"> </div>
+    <div class="preloader" id="preloader"> </div>
 </div>
 <div class="row d-flex justify-content-center">
-  <div class="col-6 col-lg-6" style="margin-right: -5px !important;">
-    <div class="card mt-3 p-3">
-      <div class="text-center" style="margin-top:4px;zoom:95%;margin-bottom:5px;">
-        <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-cliente-editar">
-          <i class="bi bi-pencil-square"></i> Editar Cliente
-        </button>
-        <!-- <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="agregar-direccion" data-bs-toggle="modal" data-bs-target="#modalRegistrarNuevaDirecciónñ">
+    <div class="col-6 col-lg-6" style="margin-right: -5px !important;">
+        <div class="mt-3 rounded p-3 shadow my-2">
+            <div class="text-center" style="margin-top:4px;zoom:95%;margin-bottom:5px;">
+                <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="btn-cliente-editar">
+                    <i class="bi bi-pencil-square"></i> Editar Cliente
+                </button>
+                <!-- <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="agregar-direccion" data-bs-toggle="modal" data-bs-target="#modalRegistrarNuevaDirecciónñ">
           <i class="bi bi-pencil-square"></i> Agregar Nueva Direccion
         </button> -->
-        <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="generar-codigoqr">
-          <i class="bi bi-qr-code"></i> Código QR
-        </button>
-      </div>
-      <table class="table table-hover display responsive tableContenido" id="TablaClientes" style="width: 100%">
-        <thead class="" style="width: 100%">
-          <tr>
-            <th scope="col d-flex justify-content-center" class="all">#</th>
-            <th scope="col d-flex justify-content-center" class="all">Nombre</th>
-            <th scope="col d-flex justify-content-center" class="min-tablet">
-              <Ri:a>Razon Social</Ri:a>
-            </th>
-            <th scope="col d-fkex justify-content-center" class="min-tablet">Abreviatura</th>
-          </tr>
-        </thead>
-        <tbody>
-        </tbody>
-      </table>
+                <button type="button" class="btn btn-hover me-2" style="margin-bottom:4px" id="generar-codigoqr">
+                    <i class="bi bi-qr-code"></i> Código QR
+                </button>
+            </div>
+            <table class="table table-hover display responsive tableContenido" id="TablaClientes" style="width: 100%">
+                <thead class="" style="width: 100%">
+                    <tr>
+                        <th scope="col d-flex justify-content-center" class="all">#</th>
+                        <th scope="col d-flex justify-content-center" class="all">Nombre</th>
+                        <th scope="col d-flex justify-content-center" class="min-tablet">
+                            <Ri:a>Razon Social</Ri:a>
+                        </th>
+                        <th scope="col d-fkex justify-content-center" class="min-tablet">Abreviatura</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
     </div>
-  </div>
 
-  <div class="col-3 col-lg-3" style="margin-right: -5px !important;">
-    <div class="card mt-3 p-3" id="informacion-segmento">
+    <div class="col-3 col-lg-3" style="margin-right: -5px !important;">
+        <div class="mt-3 rounded p-3 shadow my-2" id="informacion-segmento">
 
+        </div>
     </div>
-  </div>
 
-  <div class="col-lg-3" style="margin-right: -5px !important;">
-    <div class="card mt-3 p-3" id="informacion-cliente">
+    <div class="col-lg-3" style="margin-right: -5px !important;">
+        <div class="mt-3 rounded p-3 shadow my-2" id="informacion-cliente">
 
+        </div>
     </div>
-  </div>
 </div>
 
 
 <style media="screen">
-  #TablaContacto_filter {
+#TablaContacto_filter {
     display: none;
-  }
+}
 
-  #TablaSegmentosAdmin_filter {
+#TablaSegmentosAdmin_filter {
     display: none;
-  }
+}
 </style>
