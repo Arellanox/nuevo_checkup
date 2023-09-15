9a72c6598eb9c68d898dbd28792a0ba1968282ef
garicano25 <112988491+garicano25@users.noreply.github.com>
2023-09-15 16:05:45 -0600

    chido

--- vista/menu/corte_caja/modals/js/admin_cajas.js
+++ vista/menu/corte_caja/modals/js/admin_cajas.js
@@ -1,6 +1,5 @@
-var responsablesCajaEnviar = new Array();
-
-
+let html = ''
+var TablaUsuariosResponsables = ''
 $("#formCrearCaja").on("submit", function (e) {
     e.preventDefault();
 
@@ -46,19 +45,27 @@ var TablaTotaldeCajas = $('#TablaTotaldeCajas').DataTable({
         dataSrc: 'response.data'
     },
     columns: [
-        {data: "COUNT"},
+        { data: "COUNT" },
         { data: "DESCRIPCION" },
         {
             data: "MONTO", render: function (data) {
                 const monte_actual = parseFloat(ifnull(data, '0')).toFixed(2)
                 return `$${monte_actual}`;
             }
+        },
+        {
+            data: null, render: function (data) {
+                let html = `
+                    <i class="bi bi-trash borarCaja" style = "cursor:pointer"></i>`
+                return data['MONTO'] == '0' ? html : '';
+            }
         }
     ],
     columnDefs: [
         { target: 0, title: '#', className: 'all' },
         { target: 1, title: 'Nombre', className: 'all' },
-        { target: 2, title: 'Monto Actual', className: 'min-tablet' }
+        { target: 2, title: 'Monto Actual', className: 'min-tablet' },
+        { target: 3, title: 'Acción', className: 'all', width: '10px' }
     ],
 
 })
@@ -74,18 +81,50 @@ inputBusquedaTable("TablaTotaldeCajas", TablaTotaldeCajas, [{
 
 let id_caja = ''
 selectTable('#TablaTotaldeCajas', TablaTotaldeCajas, {
-    unSelect: true,
-}, (data) => {
+    unSelect: true, ClickClass: [
+        {
+           
+            class: 'borarCaja',
+            callback: async function (data) {
+                console.log(data)
+                data = {
+                    api: 4,
+                    id_caja: data['ID_CAJAS']
+                }
 
-    $("#nombreCaja").text(array_selected["DESCRIPCION"])
-    $("#btnAgregarResponsableCaja").prop('disabled', false)
-    $("#select-user").prop('disabled', false)
+                alertMensajeConfirm({
+                    title: "¿Esta seguro de eliminar esta caja?",
+                    text: "Es necesario confirmar para eliminar esta nueva caja",
+                    icon: "question"
+                }, function () {
+
+                    ajaxAwait(data, 'corte_caja_api', { callbackAfter: true }, false,
+                        function (data) {
+                            alertToast('La caja fue eliminada conexito', 'info', 4000)
+                            TablaTotaldeCajas.ajax.reload();
+                        })
+                }, 1)
+
+            }
+}
+    ]
+
+}, (select, data) => {
+    if (select) {
+        $("#nombreCaja").text(data["DESCRIPCION"])
+        $("#btnAgregarResponsableCaja").prop('disabled', false)
+        $("#select-user").prop('disabled', false)
+
+        dataUsuariosResponsables['id_caja'] = data["ID_CAJAS"]
+        console.log(data)
+        TablaUsuariosResponsables.ajax.reload()
+    } 
+    
+   
 
 })
 
 
-
-//----------------------------------------------------------------
 
 const adminCajasModal = document.getElementById('ModalAdministrarCajas')
 adminCajasModal.addEventListener('show.bs.modal', event => {
@@ -100,72 +139,94 @@ adminCajasModal.addEventListener('show.bs.modal', event => {
 
 })
 
-//AGREGAR RESPONSABLES A LAS CAJAS
 
+
+
+let dataUsuariosResponsables = { api: 6, id_caja: 0 }
+TablaUsuariosResponsables = $('#TablaUsuariosResponsables').DataTable({
+    language: {
+        url: "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
+    },
+    lengthChange: false,
+    info: false,
+    paging: false,
+    sorting: false,
+    scrollY: '75vh',
+    scrollCollapse: true,
+    ajax: {
+        dataType: 'json',
+        data: function (d) {
+            return $.extend(d, dataUsuariosResponsables);
+        },
+        method: 'POST',
+        url: '../../../api/corte_caja_api.php',
+        beforeSend: function () { loader("In") },
+        complete: function () { loader("Out") },
+        dataSrc: 'response.data'
+    },
+    columns: [
+        { data: "USUARIO_ID" },
+        { data: "px" },
+        {
+            data: null, render: function (data) {
+                let html = `
+                        <i class="bi bi-trash borarUsuarioResponsable" style = "cursor:pointer"></i>`
+                return html;
+            }
+        },
+
+    ],
+    columnDefs: [
+        { target: 0, title: '#', className: 'all' },
+        { target: 1, title: 'Nombre', className: 'all' },
+        { target: 2, title: 'Acción', className: 'all', width: '10px' }
+    ],
+
+})
+
+inputBusquedaTable("TablaUsuariosResponsables", TablaUsuariosResponsables, [{
+    msj: 'Tabla de usuarios responsables',
+    place: 'top'
+}], {
+    msj: "Filtre los resultados por nombre",
+    place: 'top'
+}, "col-12")
+
+
+//----------------------------------------------------------------AGREGAR USUARIOS
 select2("#select-user", "ModalAdministrarCajas", 'Seleccione un responsable para esta caja');
-
-rellenarSelect("#select-user", "corte_caja_api", 6, 'ID_USUARIO', 'nombrecompleto', {
+rellenarSelect("#select-user", "corte_caja_api", 8, 'ID_USUARIO', 'nombrecompleto', {
 
 }, function (data) {
     estudiosLab = data;
 });
 
 
-$('#btnAgregarResponsableCaja').on('click', function () {
-    let text = $("#select-user option:selected").text();
-    let id = $("#select-user").val();
+$("#btnAgregarResponsableCaja").on('click', function () {
 
-    agregarFilaDivUsuarios('#listResponsablesCajas', text, id)
-})
+    dataJson_agreagarUsuarios = {
+        api: 5,
+        id_caja: array_selected["ID_CAJAS"],
+        usuario_encargado: $("#select-user").val()
+    }
 
+        alertMensajeConfirm({
+            title: "¿Esta seguro de agregar este usuario?",
+            text: "Es necesario confirmar para realizar esta acción",
+            icon: "question"
+        }, function () {
 
-function agregarFilaDivUsuarios(appendDiv, text, id) {
-    responsablesCajaEnviar.push(id)
-    let html = '<li class="list-group-item">' +
-        '<div class="row">' +
-        '<div class="col-10 d-flex  align-items-center">' +
-        text +
-        '</div>' +
-        '<div class="col-2">' +
-        '<button type="button" class="btn btn-hover me-2 eliminarfilaEncargado" data-bs-id="' + id + '"> <i class="bi bi-trash"></i> </button>' +
-        '</div>' +
-        '</div>' +
-        '</li>';
-    $(appendDiv).append(html);
-    // console.log(estudiosEnviar);
-}
+            ajaxAwait(dataJson_agreagarUsuarios, 'corte_caja_api', { callbackAfter: true }, false, function (data) {
+                alertToast('Usuario agregado :)', 'success', 4000)
+                TablaUsuariosResponsables.ajax.reload()
 
-function eliminarElementoArray(id) {
-    responsablesCajaEnviar = jQuery.grep(responsablesCajaEnviar, function (value) {
-        return value != id;
-    });
-}
+            })
+        }, 1)
 
 
-$(document).on('click', '.eliminarfilaEncargado', function () {
-    let id = $(this).attr('data-bs-id');
-    eliminarElementoArray(id);
-    var parent_element = $(this).closest("li[class='list-group-item']");
-    $(parent_element).remove()
-
-});
-
-
-//----------------------------------------------------------------AGREGAR USUARIOS
-$("#btnAgregarResponsableCaja").on('click', function () { 
-
-        dataJson_agreagarUsuarios = {
-            api: 5,
-            id_caja: array_selected["ID_CAJAS"],
-            usuario_encargado: $("#select-user").val()
-        }
-
-        ajaxAwait(dataJson_agreagarUsuarios, 'corte_caja_api', { callbackAfter: true }, false, function (data) {
-            
-            alertToast('Usuario agregado :)', 'success', 4000)
-
-        })
+ 
 
 
 
 })
+
