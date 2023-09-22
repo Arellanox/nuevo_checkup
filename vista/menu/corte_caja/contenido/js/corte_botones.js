b12df5e4468b338ca5dd2b8040f5d32601770fc9
JuanDaniel300 <118577337+JuanDaniel300@users.noreply.github.com>
2023-09-22 15:18:23 -0600

    Revert "cambios corte de caja"
    This reverts commit 89c78cf373d09a72687f15b9dcfb8190e46cda8d.

--- vista/menu/corte_caja/contenido/js/corte_botones.js
+++ vista/menu/corte_caja/contenido/js/corte_botones.js
@@ -68,8 +68,8 @@ $(document).on('click', '#btnCerrarCaja', function (e) {
 })
 
 // Escuchar el evento click del boton para visualziar el reporte *PDF
-$(document).on('click', '#btnVisualizarReporte', function () {
-
+$(document).on('click', '#btnVisualizarReporte', function (e) {
+    e.preventDefault();
 
     api = encodeURIComponent(window.btoa('corte'));
     area = encodeURIComponent(window.btoa(-4));
