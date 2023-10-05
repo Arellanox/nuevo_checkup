f45ad283b6f8f156546dd8ad42f364d97992e3a8
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2023-10-05 10:34:20 -0600

    Update medicos_tratantes_api.php

--- api/medicos_tratantes_api.php
+++ api/medicos_tratantes_api.php
@@ -28,49 +28,15 @@ $usuario_id = isset($_SESSION['id']) ? $_SESSION['id'] : $_POST['usuario_id'];
 
 switch ($api) {
     case 1:
-        #insertar un nuevo medico.
-        if ($ignorarALevenshtein == 1) {
-
-            # si deciden ignorar a levenshtein lo insertamos
-            $response = $master->insertByProcedure("sp_medicos_tratantes_g", [$id_medico, $nombre_medico, $email, $usuario_id]);
-        } else {
-
-            # si no ignoran a levenshtein obtenemos la lista de pacientes y devolvemos las coincidencias aproximadas
-            $medicos = $master->getByProcedure("sp_medicos_tratantes_b", [null, null, null]);
-
-            $x = [];
-
-            # si recibimos la id del usuarios
-            if (isset($usuario_id)) {
-                $usuario = ($master->getByProcedure("sp_usuarios_b", [$usuario_id, null]))[0];
-                $nombre_medico = $usuario['NOMBRE'] . ' ' . $usuario['PATERNO'] . ' ' . $usuario['MATERNO'];
-            }
-
-            foreach ($medicos as $medico) {
-                $medico['distancia'] = $master->getLevenshteinDistance($nombre_medico, $medico['NOMBRE_MEDICO']);
-                $x[] = $medico;
-            }
-
-
-            $coincidencias = array_filter($x, function ($obj) {
-                return $obj['distancia'] <= 8;
-            });
-
-            if (count($coincidencias) == 0) {
-                # si levenshtein dice que no hay coincidencias, insertamos al medico.
-                $response = $master->insertByProcedure("sp_medicos_tratantes_g", [$id_medico, $nombre_medico, $email, $usuario_id]);
-            } else {
-                # si existen coincidencias, las devolvemos enel response
-                // echo json_encode(array('code' => 2, "msj" => $master->getFormValues($coincidencias)));
-                $response = $coincidencias;
-                // exit;
-            }
-        }
+        #Actualiza un nuevo medico
+        $response = $master->insertByProcedure("sp_medicos_tratantes_g", [$id_medico, $nombre_medico, $email, $usuario_id]);
         break;
+
     case 2:
         # buscar los medicos tratantes
         $response = $master->getByProcedure("sp_medicos_tratantes_b", [$id_medico, $nombre_medico, $email]);
         break;
+
     case 3:
         # eliminar medico tratante
         $response = $master->deleteByProcedure("sp_medicos_tratantes_e", [$id_medico]);
@@ -81,6 +47,42 @@ switch ($api) {
         #lista de pacientes que estan siendo atendidos por el usuario - medico tratante
         $response = $master->getByProcedure("sp_medicos_tratantes_vista", [$usuario_id, $fecha_inicio, $fecha_fin]);
         break;
+
+    case 5:
+        #Busca si tiene alguna coincidencia
+
+        # si no ignoran a levenshtein obtenemos la lista de pacientes y devolvemos las coincidencias aproximadas
+        $medicos = $master->getByProcedure("sp_medicos_tratantes_b", [null, null, null]);
+
+        $x = [];
+
+        # si recibimos la id del usuarios
+        if (isset($usuario_id)) {
+            $usuario = ($master->getByProcedure("sp_usuarios_b", [$usuario_id, null]))[0];
+            $nombre_medico = $usuario['NOMBRE'] . ' ' . $usuario['PATERNO'] . ' ' . $usuario['MATERNO'];
+        }
+
+        foreach ($medicos as $medico) {
+            $medico['distancia'] = $master->getLevenshteinDistance($nombre_medico, $medico['NOMBRE_MEDICO']);
+            $x[] = $medico;
+        }
+
+
+        $coincidencias = array_filter($x, function ($obj) {
+            return $obj['distancia'] <= 8;
+        });
+
+        if (count($coincidencias) == 0) {
+            # si levenshtein dice que no hay coincidencias, insertamos al medico.
+            $response = $master->insertByProcedure("sp_medicos_tratantes_g", [$id_medico, $nombre_medico, $email, $usuario_id]);
+        } else {
+            # si existen coincidencias, las devolvemos enel response
+            // echo json_encode(array('code' => 2, "msj" => $master->getFormValues($coincidencias)));
+            $response = $coincidencias;
+            // exit;
+        }
+        break;
+
     default:
         $response = "API no definida.";
 }
