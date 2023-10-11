57a4c9df65c5af56d506ae7e2833d5d2b4befc71
Josue de la Cruz <arellanox0392@gmail.com>
2023-10-11 11:11:39 -0600

    Update master_class.php
    se agrega una linea en la clase maestra para poder reproducir el error en la base de datos, cuando un sp mande error.

--- clases/master_class.php
+++ clases/master_class.php
@@ -140,6 +140,8 @@ class Master extends Miscelaneus
         } else {
             $error_msj = "Ha ocurrido un error(" . $sentencia->errorCode() . "). " . implode(" ", $sentencia->errorInfo());
             $this->mis->setLog($error_msj, $nombreProcedimiento);
+            $this->mis->setLog("call ".$nombreProcedimiento."(".implode(",",$parametros).")","[Reproduce el error en la base]");
+
             # return "ERROR. No se pudieron recuperar los datos.";
             $retorno = "Alerta: la consulta al servidor no se realizó correctamente";
         }
