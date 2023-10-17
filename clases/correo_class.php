758df976cc56e3fb17321f7b4ffba34572d47998
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2023-10-17 16:04:28 -0600

    clase de correos

--- clases/correo_class.php
+++ clases/correo_class.php
@@ -96,8 +96,28 @@ class Correo
         }
     }
 
+    public function getCorreoSeleccionado()
+    {
+        return $this->correo_seleccionado;
+    }
 
-    function sendEmail($bodySelected, $subject, $emails = array(), $token = null, $reportes = array(), $resultados = 0, $paciente = null)
+    private function setCorreoSeleccionado($correo)
+    {
+        $this->correo_seleccionado = $correo;
+    }
+    function sendEmail(
+        $bodySelected,
+        $subject,
+        $emails = array(),
+        $token = null,
+        $reportes = array(),
+        $resultados = 0,
+        $paciente = null,
+        # estas variables son para insertar en la tabla de correos.
+        $id_turno = null,
+        $area_id = null,
+        $master = null
+    )
     # $bodyselected indica el cuerpo que se envia en el correo.
     # $emails, direcciones de correo electronico de destino.
     # $token, se usa para enviar el link de prerregistro.
@@ -113,6 +133,12 @@ class Correo
             $username = 'hola@bimo-lab.com';
             $password = 'X@96ck6B1V4&tm!4QZp3F';
             $fromName = 'bimo';
+        } else if ($resultados == 1) {
+            $username = 'soporte@bimo-lab.com';
+            $this->setCorreoSeleccionado($username);
+            // $password = 'Bimo2023!';
+            $password = $this->emailCred->soporte;
+            $fromName = 'Resultados [bimo]';
         } else {
             $username = 'resultados@bimo-lab.com';
             $password = 'Bimo2023!';
@@ -182,15 +208,36 @@ class Correo
 
             # send email
             $mail->send();
+            $response = $master->insertByProcedure("sp_correos_g", [
+                $id_turno,
+                $area_id,
+                $this->getCorreoSeleccionado(),
+                json_encode($emails),
+                null,
+                "CORRECTO",
+                1
+            ]);
 
             return true;
         } catch (Exception $e) {
+
+            $response = $master->insertByProcedure("sp_correos_g", [
+                $id_turno,
+                $area_id,
+                $this->getCorreoSeleccionado(),
+                json_encode($emails),
+                null,
+                "ERROR",
+                0
+            ]);
+
             $mis->setLog($e, "Clase correo [sendMail]");
             return false;
         }
     }
 
-    private function cuerpoFormularioContacto($data){
+    private function cuerpoFormularioContacto($data)
+    {
         $html = "<!DOCTYPE html>
         <html>
         
@@ -243,15 +290,15 @@ class Correo
             <div class=\"container\">
                 <h1>Datos de Contacto</h1>
                 <div class=\"info\">
-                    <p><strong>Nombre:</strong> ".$data['nombre']."</p>
-                    <p><strong>Email:</strong> ".$data['email']."</p>
-                    <p><strong>Número de Teléfono:</strong> ".$data['telefono']. "</p>
-                    <p><strong>Asunto:</strong>". $data['asunto']."</p>
-                    <p><strong>Política de privacidad:</strong> ". $data['politica']."</p>
+                    <p><strong>Nombre:</strong> " . $data['nombre'] . "</p>
+                    <p><strong>Email:</strong> " . $data['email'] . "</p>
+                    <p><strong>Número de Teléfono:</strong> " . $data['telefono'] . "</p>
+                    <p><strong>Asunto:</strong>" . $data['asunto'] . "</p>
+                    <p><strong>Política de privacidad:</strong> " . $data['politica'] . "</p>
                 </div>
                 <div class=\"mensaje\">
                     <p><strong>Mensaje:</strong></p>
-                    <p>".$data['comentario_ayuda']." ?></p>
+                    <p>" . $data['comentario_ayuda'] . " ?></p>
                 </div>
             </div>
         </body>
