fcc1f9e8e1b5011e204b1e30a3cf5cbf16ca80a2
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2023-10-23 15:29:37 -0600

    correciones de nombre de medico

--- vista/menu/consultorio/contenido/js/consultorio-paciente.js
+++ vista/menu/consultorio/contenido/js/consultorio-paciente.js
@@ -236,7 +236,7 @@ function obtenerHistorialConsultaMedica(turno) {
           for (var i = 0; i < row.length; i++) {
             if (row[i]['CONSULTA_TERMINADA'] == 1) {
               let fecha = formatoFecha2(row[i]['FECHA_CONSULTA'], [0, 1, 2, 2, 0, 0, 0]);
-              let nombre_medico = row[i]['CREADO_POR'];
+              let nombre_medico = row[i]['NOMBRE_COMPLETO'];
               let motivo = row[i]['MOTIVO_CONSULTA'];
 
 
