5a8b3bfd13c043979c9e526af87970d1f85558f1
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-10-27 10:57:23 -0600

    Nueva columna, vendedor

--- vista/documentacion/reporte-paciente/contenido/js/tabla-copia.js
+++ vista/documentacion/reporte-paciente/contenido/js/tabla-copia.js
@@ -59,6 +59,7 @@ tablaPrincipal = $('#tablaPrincipal').DataTable({
             }
         },
         { data: 'PROCEDENCIA' },
+        { data: 'VENDEDOR' },
         { data: 'TRABAJADOR' },
         { data: 'VERIFICACION' },
         { data: 'CATEGORIA' },
@@ -82,11 +83,12 @@ tablaPrincipal = $('#tablaPrincipal').DataTable({
         { target: 13, className: 'all', title: 'Total', width: '7%' },
         { target: 14, className: 'all', title: 'Fecha Recepción', width: '12%' },
         { target: 15, className: 'all', title: 'Procedencia' },
-        { target: 16, className: 'none beneficiario', title: 'Trabajador', visible: false },
-        { target: 17, className: 'none beneficiario', title: 'Verificacion (url)', visible: false },
-        { target: 18, className: 'none beneficiario', title: 'Categoria', visible: false },
-        { target: 19, className: 'none beneficiario', title: 'Ures', visible: false },
-        { target: 20, className: 'all', title: 'Diagnostico' },
+        { target: 16, className: 'none', title: 'Vendedor' },
+        { target: 17, className: 'none beneficiario', title: 'Trabajador', visible: false },
+        { target: 18, className: 'none beneficiario', title: 'Verificacion (url)', visible: false },
+        { target: 19, className: 'none beneficiario', title: 'Categoria', visible: false },
+        { target: 20, className: 'none beneficiario', title: 'Ures', visible: false },
+        { target: 21, className: 'all', title: 'Diagnostico' },
     ],
 
 
