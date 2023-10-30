a78805e2faab8ade6337f974b2a7325d0f514b97
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-10-30 13:24:59 -0600

    Correcciones pequeñas, progreso en curso

--- api/medicos_tratantes_api.php
+++ api/medicos_tratantes_api.php
@@ -17,57 +17,33 @@ $ignorarALevenshtein = $_POST['ignorarALevenshtein']; # enviar 1 para ignorar, 0
 $id_medico = $_POST['id_medico'];
 $nombre_medico = $_POST['nombre_medico'];
 $email = $_POST['email'];
-$usuario_id = $master->setToNull([$_POST['usuario_id']])[0];
+$medico_usuario_id = $_POST['usuario_id'];
+// $usuario_id = $master->setToNull([$_POST['usuario_id']])[0];
 
 # variables para la vista del medico
-$fecha_inicio = $_POST['fecha_iniciio'];
+$fecha_inicio = $_POST['fecha_inicio'];
 $fecha_fin = $_POST['fecha_fin'];
+
+$nuevo_medico = $_POST['nuevo_medico'];
+$usuario_id = $_SESSION['id'];
+$filtrar_todos = $_POST['filtrar_todos'];
+
+# Turno ID
+$turno_id = $_POST['turno_id'];
+$telefono = $_POST['telefono'];
+$especialidad = $_POST['especialidad'];
 
 switch ($api) {
     case 1:
-        #insertar un nuevo medico.
-        if ($ignorarALevenshtein == 1) {
-
-            # si deciden ignorar a levenshtein lo insertamos
-            $response = $master->insertByProcedure("sp_medicos_tratantes_g", [$id_medico, $nombre_medico, $email, $usuario_id]);
-        } else {
-
-            # si no ignoran a levenshtein obtenemos la lista de pacientes y devolvemos las coincidencias aproximadas
-            $medicos = $master->getByProcedure("sp_medicos_tratantes_b", [null, null, null]);
-
-            $x = [];
-
-            # si recibimos la id del usuarios
-            if (isset($usuario_id)) {
-                $usuario = ($master->getByProcedure("sp_usuarios_b", [$usuario_id, null]))[0];
-                $nombre_medico = $usuario['NOMBRE'] . ' ' . $usuario['PATERNO'] . ' ' . $usuario['MATERNO'];
-            }
-
-            foreach ($medicos as $medico) {
-                $medico['distancia'] = $master->getLevenshteinDistance($nombre_medico, $medico['NOMBRE_MEDICO']);
-                $x[] = $medico;
-            }
-
-
-            $coincidencias = array_filter($x, function ($obj) {
-                return $obj['distancia'] <= 8;
-            });
-
-            if (count($coincidencias) == 0) {
-                # si levenshtein dice que no hay coincidencias, insertamos al medico.
-                $response = $master->insertByProcedure("sp_medicos_tratantes_g", [$id_medico, $nombre_medico, $email, $usuario_id]);
-            } else {
-                # si existen coincidencias, las devolvemos enel response
-                // echo json_encode(array('code' => 2, "msj" => $master->getFormValues($coincidencias)));
-                $response = $coincidencias;
-                // exit;
-            }
-        }
+        #Actualiza un nuevo medico
+        $response = $master->insertByProcedure("sp_medicos_tratantes_g", [$id_medico, $nombre_medico, $email, $medico_usuario_id, $telefono, $especialidad]);
         break;
+
     case 2:
         # buscar los medicos tratantes
         $response = $master->getByProcedure("sp_medicos_tratantes_b", [$id_medico, $nombre_medico, $email]);
         break;
+
     case 3:
         # eliminar medico tratante
         $response = $master->deleteByProcedure("sp_medicos_tratantes_e", [$id_medico]);
@@ -76,8 +52,49 @@ switch ($api) {
         # vista de los usuarios tipo medico tratante.
 
         #lista de pacientes que estan siendo atendidos por el usuario - medico tratante
-        $response = $master->getByProcedure("sp_medicos_tratantes_vista", [$_SESSION['id'], $fecha_inicio, $fecha_fin]);
+        $response = $master->getByProcedure("sp_medicos_tratantes_vista", [$usuario_id, $fecha_inicio, $fecha_fin, $filtrar_todos]);
         break;
+
+    case 5:
+        # Verificacion de las coincidencias encontradas
+        if (strlen($nombre_medico) <= 3) {
+            $response = [];
+        } else {
+            $medicos = $master->getByProcedure("sp_medicos_tratantes_b", [null, null, null]);
+            $coincidencias = [];
+            $nombre_medico = strtolower($nombre_medico);
+            foreach ($medicos as $medico) {
+                $base = strtolower($medico['NOMBRE_MEDICO']);
+                $baseTokens = explode(' ', $base);
+                $userTokens = explode(' ', $nombre_medico);
+
+                $matches = 0;
+                foreach ($userTokens as $userToken) {
+                    foreach ($baseTokens as $baseToken) {
+                        if (levenshtein($baseToken, $userToken) <= 2) { // Umbral de distancia
+                            $matches++;
+                            break;
+                        }
+                    }
+                }
+
+                $score = $matches / count($userTokens); // Cambio clave aquí: ahora consideramos la proporción basada en el userInput
+
+                if ($score > 0.7) { // Puedes ajustar este umbral según lo necesites
+                    $coincidencias[] = $medico['NOMBRE_MEDICO'];
+                }
+            }
+
+            $response = $coincidencias;
+        }
+
+        break;
+
+    case 6:
+        # recuperar reportes de paciente por el turno
+        $response = $master->getByProcedure("sp_recuperar_reportes_confirmados", [$turno_id, null, null, null, null]);
+        break;
+
     default:
         $response = "API no definida.";
 }
