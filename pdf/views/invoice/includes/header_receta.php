7a2c302f6a52bd4c457fabc9827e77a0bf415976
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2023-11-04 13:46:43 -0600

    CAMBIOS EN LOS REPORTES DEL ENCABEZADO->EDAD

--- pdf/views/invoice/includes/header_receta.php
+++ pdf/views/invoice/includes/header_receta.php
@@ -42,7 +42,7 @@
                 No. Folio: <strong style="font-size: 12px;"> <?php echo $pie['folio']; ?> </strong>
             </td>
             <td class="col-center" style="border-bottom: none">
-                Edad: <strong style="font-size: 12px;"> <?php echo $encabezado->EDAD < 1 ? ($encabezado->EDAD * 10) . " meses" : $encabezado->EDAD . " años"; ?></strong>
+                Edad: <strong style="font-size: 12px;"> <?php echo $encabezado->EDAD; ?></strong>
             </td>
             <td class="col-right" style="border-bottom: none">
                 Sexo: <strong style="font-size: 12px;"><?php echo $encabezado->SEXO; ?> </strong>
