5998d474d2fe6e46aaaf9472ebcf0be7cb4bccfb
Josue de la Cruz <arellanox0392@gmail.com>
2023-11-15 13:13:16 -0600

    Create certificacos_api.php

---
+++ api/certificacos_api.php
@@ -0,0 +1,33 @@
+<?php
+require_once "../clases/master_class.php";
+require_once "../clases/token_auth.php";
+include "../clases/correo_class.php";
+
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+if (!$tokenValido) {
+    // $tokenVerification->logout();
+    // exit;
+}
+
+$master = new Master();
+$api = $_POST['api'];
+
+# variables para lista de trabajo.
+$fecha = $_POST['fecha_busqueda'];
+
+
+switch($api){
+    case 1:
+        # lista de trabajo para certificados.
+        # pacientes que tengan cargado entre sus estudios una historia clinica.
+        $response = $master->getByProcedure("sp_lista_de_trabajo_certificados", [$fecha, 1, null, null, null]);
+        break;
+    default:
+        $response = "API no definida.";
+    break;
+
+}
+
+echo $master->returnApi($response);
+?>
