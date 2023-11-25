6cc3dc25e5b334414bb4146b0be6e4597c9b6042
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-11-25 14:41:28 -0600

    Ocultar columnas, cotizaciones

--- vista/menu/lista-precio/contenido/cotizaciones.php
+++ vista/menu/lista-precio/contenido/cotizaciones.php
@@ -110,8 +110,8 @@
                             <th class="all">Descripción</th>
                             <th class="min-tablet">CVE</th>
                             <th class="min-tablet">Cantidad</th>
-                            <th class="min-tablet">Costo</th>
-                            <th class="min-tablet">Costo Total</th>
+                            <!-- <th class="min-tablet">Costo</th>
+                            <th class="min-tablet">Costo Total</th> -->
                             <th class="min-tablet">Precio Venta</th>
                             <th class="min-tablet">Descuento</th>
                             <th class="min-tablet">Subtotal</th>
