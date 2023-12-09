ee9e142ae8ab7a27186ae6602c5dc066a53dd0bb
garicano25 <112988491+garicano25@users.noreply.github.com>
2023-12-09 11:37:41 -0600

    Create prequirurgico_api.php

---
+++ api/prequirurgico_api.php
@@ -0,0 +1,35 @@
+<?php
+include "../clases/master_class.php";
+require_once "../clases/token_auth.php";
+
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+if (!$tokenValido) {
+    // $tokenVerification->logout();
+    // exit;
+}
+
+$master = new Master();
+
+$api = $_POST['api'];
+
+$cliente_id = $_POST['cliente_id'];
+
+
+switch ($api){
+
+    case 1:
+        $area = $_POST['area_id'];
+        $fecha = $_POST['fecha_busqueda'];
+        $response = $master->getByProcedure("sp_prequirurgico_b", [$fecha, $area, null, $_SESSION['id'], $cliente_id]);
+            
+        break;
+
+    default:
+        $response = "Api no definida";
+
+
+};
+
+
+echo $master->returnApi($response);
