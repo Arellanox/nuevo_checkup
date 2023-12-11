417733ad684fe70d7f4c76f3f3a0a98b4be6728e
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-12-11 15:02:00 -0600

    Update m_formPrequirurjico.js

--- vista/menu/valoracion_prequirurgica/modals/js/m_formPrequirurjico.js
+++ vista/menu/valoracion_prequirurgica/modals/js/m_formPrequirurjico.js
@@ -1,9 +1,26 @@
+// Abrir el model de formulario
 $('#btn-interpretacionPrequi').on('click', function () {
     $('#MostrarCapturaPrequirurjico').modal('show');
 })
 
+// Obtener el reporte previo
+$('#btn-ver-reporte').click(function () {
+    area_nombre = ''
+    api = encodeURIComponent(window.btoa(area_nombre));
+    turno = encodeURIComponent(window.btoa(dataSelect.array['turno']));
+    area = encodeURIComponent(window.btoa(areaActiva));
 
 
+    window.open(`${http}${servidor}/${appname}/visualizar_reporte/?api=${api}&turno=${turno}&area=${area}`, "_blank");
+})
+
+
+
+
+
+
+
+// Enviar interpretacion a back
 $(`#formInterpretacion`).submit(function (e) {
     e.preventDefault();
 
