2bab71c40973046da7c082d18da70fd5a2272f57
Josue de la Cruz <arellanox0392@gmail.com>
2023-12-12 11:59:15 -0600

    Update prequirurgico_api.php

--- api/prequirurgico_api.php
+++ api/prequirurgico_api.php
@@ -11,19 +11,31 @@ if (!$tokenValido) {
 
 $master = new Master();
 
-$api = $_POST['api'];
+$datos = $_POST['datos'];
+$api = $datos['api'];
+$antecedentes = $datos['antecedentes'];
+$exploracion = $datos['exploracion_fisica'];
+$electro = $datos['']
 
 $cliente_id = $_POST['cliente_id'];
+
+
 
 
 switch ($api) {
 
     case 1:
+        # para recuperar la lista de trabajo.
+        # paciente que tengan por procedencia CLINICA DEL DR CASTILLO
+        # o que tengan cargados los paquetes de prequirurgico.
         $area = $_POST['area_id'];
         $fecha = isset($_POST['fecha_busqueda']) ?  $_POST['fecha_busqueda'] : NULL;
         $response = $master->getByProcedure("sp_prequirurgico_b", [$fecha, $area, null, $_SESSION['id'], $cliente_id]);
 
         break;
+    case 2:
+        # guardar los datos.
+
 
     default:
         $response = "Api no definida";
