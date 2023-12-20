33f06c938666c409f3cd728faf56d05c9774723d
JuanDaniel300 <118577337+JuanDaniel300@users.noreply.github.com>
2023-12-20 12:44:04 -0600

    Update vinco_gcs.php

--- pdf/views/invoice/includes/certificados/vinco_gcs.php
+++ pdf/views/invoice/includes/certificados/vinco_gcs.php
@@ -2,5 +2,337 @@
     @page {
         margin: 80px 10px 94px 10px;
     }
+
+    .body-certificado {
+        padding: 10px 30px 10px 30px;
+    }
+
+    .body-certificado p {
+        font-size: 13px;
+    }
+
+    .body-certificado h1 {
+        padding: none !important;
+        margin: none !important;
+    }
+
+    .body-certificado .none-p {
+        padding: none !important;
+        margin: none !important;
+    }
+
+    .body-certificado .center {
+        text-align: center !important;
+    }
+
+    .body-certificado .justify {
+        text-align: justify !important;
+    }
+
+    .body-certificado table {
+        width: 100%;
+        max-width: 100%;
+
+        caption-side: bottom;
+        border-collapse: collapse;
+    }
+
+    .body-certificado th,
+    .body-certificado td {
+        border: 1px solid black;
+        width: 100%;
+        max-width: 100%;
+        word-break: break-all;
+    }
+
+    .body-certificado .border {
+        border: 1px solid black;
+    }
+
+    .body-certificado td {
+        padding: 2px;
+        font-size: 13px;
+    }
+
+    .body-certificado .res {
+        font-size: 13px !important;
+    }
+
+    .body-certificado .left {
+        padding-left: 30px !important;
+    }
+
+    .body-certificado .bg {
+        background-color: #e7e6e6 !important;
+    }
+
+    .body-certificado .bold {
+        font-weight: bold !important;
+    }
+
+    .body-certificado .italic {
+        font-style: italic !important;
+    }
+
+    .body-certificado .pb {
+        padding-bottom: 20px !important;
+    }
+
+    .body-certificado .p {
+        padding: 5px !important;
+    }
+
+    .body-certificado .tabla2 {
+        margin-left: auto !important;
+    }
+
+    .body-certificado .bg-black {
+        background-color: black !important;
+    }
+
+    .body-certificado .bg-gray {
+        background-color: #757070 !important;
+    }
 </style>
+
 <!-- Body -->
+<div class="body-certificado">
+    <!-- Tabla 1 -->
+    <table>
+        <!-- Datos Generales -->
+        <tr>
+            <td colspan="10" class="bg center bold italic">DATOS GENERALES</td>
+        </tr>
+        <tr>
+            <td colspan="5">
+                LUGAR:
+                <strong>Villahermosa, Tabasco.</strong>
+            </td>
+            <td colspan="5">
+                FECHA:
+                <strong>07 octubre 2023</strong>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                NOMBRE:
+                <strong>Gabriel Chavez Sanchez</strong>
+            </td>
+            <td colspan="2">
+                EDAD:
+                <strong>49 años</strong>
+            </td>
+            <td colspan="4">
+                NACIONALIDAD:
+                <strong>Mexicana</strong>
+            </td>
+        </tr>
+        <!-- Examen periodico -->
+        <tr>
+            <td colspan="10" class="bg center bold italic">TIPO DE EXAMEN MEDICO</td>
+        </tr>
+        <tr>
+            <td>INGRESO</td>
+            <td class="bold center"></td>
+            <td>PERIODICO</td>
+            <td class="bold center">X</td>
+            <td>EGRESO</td>
+            <td class="bold center"></td>
+            <td>ESPECIAL</td>
+            <td class="bold center"></td>
+            <td>OTRO:</td>
+            <td class="bold center"></td>
+        </tr>
+        <tr>
+            <td colspan="10">
+                QUIEN ES/ SERA CONSIDERADO PERSONAL EN ACTIVO EN LA EMPRESA:
+                <strong>
+                    VINCO
+                </strong>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="10">
+                EN LA POSICIÓN DE:
+                <strong>
+                    Ingeniero de campo.
+                </strong>
+            </td>
+        </tr>
+        <!-- Diagnostico -->
+        <tr>
+            <td colspan="10" class="bg center bold italic">DIAGNÓSTICO</td>
+        </tr>
+        <tr>
+            <td colspan="10" class="center bold italic pb">
+                PITIRIASIS VERSICOLOR EN PIERNA DERECHA
+            </td>
+        </tr>
+        <tr>
+            <td colspan="10" class="bg center bold italic">CLASIFICACIÓN EN GRADO DE SALUD </td>
+        </tr>
+        <tr>
+            <td colspan="10" class="center bold italic pb">
+                1
+            </td>
+        </tr>
+        <!-- APTITUDES PARA EL TRABAJO -->
+        <tr>
+            <td colspan="10" class="bg center bold italic">APTITUD DE TRABAJO</td>
+        </tr>
+        <tr>
+            <td colspan="1"></td>
+            <td colspan="1" class="bg-black"></td>
+            <td colspan="8">
+                APTO PARA TRABAJAR
+            </td>
+        </tr>
+        <tr>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="8">
+                APTO PARA TRABAJAR
+            </td>
+        </tr>
+        <tr>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="8">
+                NO APTO PARA TRABAJAR
+            </td>
+        </tr>
+    </table>
+    <!-- Extension de la tabla 1 -->
+    <table style="width: 60%;" class="tabla2">
+        <tr>
+            <td style="border-top: none;" class="p  bg center bold italic"> VIGENCIA</td>
+            <td style="border-top: none;" class="p">1 año</td>
+            <td style="border-top: none;" class="p  bg center bold italic"> FECHA DE
+                VENCIMIENTO </td>
+            <td style="border-top: none;" class="p">06/10/2024</td>
+        </tr>
+    </table>
+    <!-- Tabla 2 -->
+    <table style="margin-top: 30px;">
+        <tr>
+            <td colspan="4" class="bg-gray center bold italic p" style="color: white;">
+                ESTUDIOS Y ANÁLISIS
+                COMPLEMENTARIOS
+            </td>
+        </tr>
+        <tr>
+            <td class="bg p center bold italic">
+                ESTUDIO
+            </td>
+            <td class="bg p center bold italic">
+                RESULTADO
+            </td>
+            <td class="bg p center bold italic">
+                ESTUDIO
+            </td>
+            <td class="bg p center bold italic">
+                RESULTADO
+            </td>
+        </tr>
+        <tr>
+            <td class="bold center">
+                Audiometría tonal
+            </td>
+            <td class="center">
+                Normal
+            </td>
+            <td class="bold center">
+                Signos vitales
+            </td>
+            <td class="center">
+                Normal
+            </td>
+        </tr>
+        <tr>
+            <td class="bold center">
+                Valoración visual
+            </td>
+            <td class="center">
+                N/A
+            </td>
+            <td class="bold center">
+                Biometría hemática
+                completa
+            </td>
+            <td class="center">
+                Normal
+            </td>
+        </tr>
+        <tr>
+            <td class="bold center">
+                Tele de tórax
+            </td>
+            <td class="center">
+                De aspecto normal
+            </td>
+            <td class="bold center">
+                Química sanguínea 6
+                elementos
+            </td>
+            <td class="center">
+                Normal
+            </td>
+        </tr>
+        <tr>
+            <td class="bold center">
+                Rx Ap y latera de columna
+            </td>
+            <td class="center">
+                De aspecto normal
+            </td>
+            <td class="bold center">
+                Perfil de drogas 5
+                elementos
+            </td>
+            <td class="center">
+                Negativo
+            </td>
+        </tr>
+        <tr>
+            <td class="bold center">
+                Electrocardiograma
+            </td>
+            <td class="center">
+                Sin alteraciones
+            </td>
+            <td class="bold center">
+                Etanol en sangre
+            </td>
+            <td class="center">
+                Negativo
+            </td>
+        </tr>
+        <tr>
+            <td class="bold center">
+                InBody
+            </td>
+            <td class="center">
+                Sobrepeso
+            </td>
+            <td class="bold center">
+                Examen general de orina
+            </td>
+            <td class="center">
+                Normal
+            </td>
+        </tr>
+    </table>
+    <div class="conclusion">
+        <div class="medico" style="margin-top: 30px; ">
+            <p class="bold none-p center">
+                Dra. Beatriz Alejandra Ramos Gonzáles
+            </p>
+            <p class="bold none-p center">
+                Cédula profesional: 77965955
+            </p>
+            <p class="bold none-p center">
+                Certificación NIOSH SP-000515-23
+            </p>
+        </div>
+    </div>
+</div>
