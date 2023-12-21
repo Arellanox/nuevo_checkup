d92ecda21d56d6e30e73b065b2df0631cc65258b
Josue de la Cruz <arellanox0392@gmail.com>
2023-12-21 10:12:59 -0600

    Update checadorBimo_api.php

--- api/checadorBimo_api.php
+++ api/checadorBimo_api.php
@@ -4,6 +4,11 @@ $master = new Master();
 
 $asistencia = json_decode(file_get_contents('php://input'), true);
 
+$fecha_inicio = $_POST['fecha_inicial'];
+$fecha_final = $_POST['fecha_final'];
+$bimer_id = $_POST['bimer_id'];
+
+$asistencia['api'] = isset($asistencia['api'])? $asistencia['api'] : $_POST['api'];
 
 
 if (isset($asistencia['api']) && isset($asistencia['nombre'])) {
@@ -33,6 +38,13 @@ if (isset($asistencia['api']) && isset($asistencia['nombre'])) {
             $response = $master->getByProcedure('sp_codificar_rostro_g', [null, null]);
 
             break;
+        case 4:
+            # recuperar los registros de entradas/salidas
+            $data = $master->getByProcedure("sp_checador_data", [$fecha_inicio, $fecha_final, $bimer_id]);
+            echo "hola3";
+            print_r($data);
+            exit;
+            break;
         default:
 
             $response = "Api no definida";
@@ -47,3 +59,26 @@ if (isset($asistencia['api']) && isset($asistencia['nombre'])) {
 
     echo $respuesta;
 }
+
+
+# recuperamos la data
+$data = $master->getByNext("sp_checador_data", [$fecha_inicio, $fecha_final, $bimer_id]);
+
+# bimers que aparecen en los registros (solo una vez).
+$records = $data[1];
+
+# dataset de los registros de entradas y salidas.
+$data = $data[0];
+
+$bimerRecords = array();
+foreach($records as $record){
+    $fecha = $record['FECHA'];
+    $bimers = array_filter($data,function($registro) use($fecha){
+        return $registro['FECHA'] == $fecha;
+    });
+    
+
+    $bimerRecords[$fecha] = $bimers;
+}
+
+print_r($bimerRecords);
