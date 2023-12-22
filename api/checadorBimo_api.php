d44597f2dc4ea0b6af68093508c044115c4a048b
garicano25 <112988491+garicano25@users.noreply.github.com>
2023-12-22 10:03:01 -0600

    s

--- api/checadorBimo_api.php
+++ api/checadorBimo_api.php
@@ -1,78 +1,86 @@
 <?php
-require_once "../clases/master_class.php";
+
+
+include_once "../clases/master_class.php";
 $master = new Master();
 
-$asistencia = json_decode(file_get_contents('php://input'), true);
-
-$fecha_inicio = $asistencia['fecha_inicial'];
-$fecha_final = $asistencia['fecha_final'];
+$fecha_inicio = $_POST['fecha_inicial'];
+$fecha_final = $_POST['fecha_final'];
 $bimer_id = $asistencia['bimer_id'];
 
-$asistencia['api'] = isset($asistencia['api'])? $asistencia['api'] : $_POST['api'];
+// $asistencia = json_decode(file_get_contents('php://input'), true);
 
 
-if (isset($asistencia['api']) && isset($asistencia['nombre'])) {
-
-    switch ($asistencia['api']) {
-
-        case 1:
-            #GUARDAMOS LA ASISTENCIA EN LA BASE DE DATOS
-            $nombre =  str_replace(array('"', "'"), '', json_encode($asistencia['nombre']));
-
-            $response = $master->getByProcedure('sp_checardor_bimo_g', [$nombre]);
-
-            break;
-
-        case 2:
-            #GUARDAMOS LAS NUEVAS CODIFICACIONES DE ROSTROS
-            $nombre = json_encode($asistencia['nombre']);
-            $codificacion = json_encode($asistencia['codificacion']);
-
-            $response = $master->insertByProcedure('sp_codificar_rostro_g', [$nombre, $codificacion]);
+// $asistencia['api'] = isset($asistencia['api']) ? $asistencia['api'] : $_POST['api'];
+$api = isset($asistencia['api']) ? $asistencia['api'] : $_POST['api'];
 
 
-            break;
 
-        case 3:
-            #OBTENEMOS TODAS LAS CODIFICACIONES DE ROSTROS QUE TENEMOS ALMACENADA EN LA BASE DE DATOS
-            $response = $master->getByProcedure('sp_codificar_rostro_g', [null, null]);
+// if (isset($asistencia['api']) && isset($asistencia['nombre'])) {
 
-            break;
-        case 4:
-            # recuperar los registros de entradas/salidas
-            # recuperamos la data
-            $data = $master->getByNext("sp_checador_data", [$fecha_inicio, $fecha_final, $bimer_id]);
+switch ($api) {
 
-            $bimers = $data[1];
-            $records = $data[0];
-            $dates = $data[2];
+    case 1:
+        #GUARDAMOS LA ASISTENCIA EN LA BASE DE DATOS
+        $nombre =  str_replace(array('"', "'"), '', json_encode($asistencia['nombre']));
 
-            $filteredRecords = array();
-            foreach($dates as $date){
-                $fecha = $date['FECHA'];
+        $response = $master->getByProcedure('sp_checardor_bimo_g', [$nombre]);
 
-                $filtered = array_filter($records, function($item) use ($fecha){
-                    return $item["FECHA"] ==$fecha;
-                });
+        break;
 
-                $filteredRecords[$fecha] = $filtered;
-            }
+    case 2:
+        #GUARDAMOS LAS NUEVAS CODIFICACIONES DE ROSTROS
+        $nombre = json_encode($asistencia['nombre']);
+        $codificacion = json_encode($asistencia['codificacion']);
 
-            $response = $filteredRecords;
+        $response = $master->insertByProcedure('sp_codificar_rostro_g', [$nombre, $codificacion]);
 
-            break;
-        default:
 
-            $response = "Api no definida";
+        break;
+
+    case 3:
+        #OBTENEMOS TODAS LAS CODIFICACIONES DE ROSTROS QUE TENEMOS ALMACENADA EN LA BASE DE DATOS
+        $response = $master->getByProcedure('sp_codificar_rostro_g', [null, null]);
+
+        break;
+    case 4:
+
+        
+        # recuperar los registros de entradas/salidas
+        # recuperamos la data
+        $data = $master->getByNext("sp_checador_data", [$fecha_inicio, $fecha_final, $bimer_id]);
+
+        $bimers = $data[1];
+        $records = $data[0];
+        $dates = $data[2];
+
+        $filteredRecords = array();
+        foreach ($dates as $date) {
+            $fecha = $date['FECHA'];
+
+            $filtered = array_filter($records, function ($item) use ($fecha) {
+                return $item["FECHA"] == $fecha;
+            });
+
+            $filteredRecords[$fecha] = $filtered;
+        }
+
+        $response = $filteredRecords;
+
+
+     
+
+        break;
+    default:
+
+        $response = "Api no definida";
+        
     }
 
+        $respuesta = $master->returnApi($response);
 
-    $respuesta = $master->returnApi($response);
+        echo $respuesta;
 
-    // $fh = fopen("asistencia.txt", 'a');
-    // fwrite($fh, $respuesta);
-    // fclose($fh);
 
-    echo $respuesta;
-}
+// }
 
