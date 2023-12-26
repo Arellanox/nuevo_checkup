87dccfe1d117a6f95060694ab1784868e1a4e723
JuanDaniel300 <118577337+JuanDaniel300@users.noreply.github.com>
2023-12-26 09:56:21 -0600

    Update asistencia-tabla.js

--- vista/menu/asistencia/contenido/js/asistencia-tabla.js
+++ vista/menu/asistencia/contenido/js/asistencia-tabla.js
@@ -13,68 +13,62 @@ TablaAsistencia = $('#TablaAsistencia').DataTable({
     sorting: false,
     scrollY: '68vh',
     scrollCollapse: true,
-    ajax: {
-        dataType: 'json',
-        data: function (d) {
-            return $.extend(d, dataAsistencia);
-        },
-        // data: { api: 2, equipo: id_equipos },
-        method: 'POST',
-        url: '../../../api/checadorBimo_api.php',
-        beforeSend: function () {
-        },
-        complete: function () {
-            //Para ocultar segunda columna
-            // loader("Out", 'bottom')
+    // ajax: {
+    //     dataType: 'json',
+    //     data: function (d) {
+    //         return $.extend(d, dataAsistencia);
+    //     },
+    //     // data: { api: 2, equipo: id_equipos },
+    //     method: 'POST',
+    //     url: '../../../api/checadorBimo_api.php',
+    //     beforeSend: function () {
+    //     },
+    //     complete: function () {
+    //         //Para ocultar segunda columna
+    //         // loader("Out", 'bottom')
 
-            $.fn.dataTable
-                .tables({
-                    visible: true,
-                    api: true
-                })
-                .columns.adjust();
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            alertErrorAJAX(jqXHR, textStatus, errorThrown);
-        },
-        dataSrc: 'response.data'
-    },
-    createdRow: function (row, data, dataIndex) {
-        if (data.FINALIZADO == 0) {
-            $(row).addClass('bg-warning text-black');
-        } else if (data.FINALIZADO == 1) {
-        }
-    },
-    columns: [
-        { data: 'FOLIO' },
-        {
-            data: 'FECHA_INICIO', render: function (data) {
-                return formatoFecha2(data, [0, 1, 3, 1]);
-            }
-        },
-        {
-            data: 'FECHA_FINAL', render: function (data) {
-                return ifnull(formatoFecha2(data, [0, 1, 3, 1]), "N/A");
-            }
-        },
-        {
-            data: 'px'
-        },
-        {
-            data: 'FINALIZADO', render: function (data) {
-                let html;
+    //         $.fn.dataTable
+    //             .tables({
+    //                 visible: true,
+    //                 api: true
+    //             })
+    //             .columns.adjust();
+    //     },
+    //     error: function (jqXHR, textStatus, errorThrown) {
+    //         alertErrorAJAX(jqXHR, textStatus, errorThrown);
+    //     },
+    //     dataSrc: 'response.data'
+    // },
+    // columns: [
+    //     { data: 'FOLIO' },
+    //     {
+    //         data: 'FECHA_INICIO', render: function (data) {
+    //             return formatoFecha2(data, [0, 1, 3, 1]);
+    //         }
+    //     },
+    //     {
+    //         data: 'FECHA_FINAL', render: function (data) {
+    //             return ifnull(formatoFecha2(data, [0, 1, 3, 1]), "N/A");
+    //         }
+    //     },
+    //     {
+    //         data: 'px'
+    //     },
+    //     {
+    //         data: 'FINALIZADO', render: function (data) {
+    //             let html;
 
 
-                if (data === "0") {
-                    html = `<i class="bi bi-x-circle"></i>`
-                } else if (data === "1") {
-                    html = `<i class="bi bi-check-circle"></i>`
-                }
+    //             if (data === "0") {
+    //                 html = `<i class="bi bi-x-circle"></i>`
+    //             } else if (data === "1") {
+    //                 html = `<i class="bi bi-check-circle"></i>`
+    //             }
 
-                return html
-            }
-        }
-    ],
+    //             return html
+    //         }
+    //     }
+    // ],
     columnDefs: [
         { target: 0, title: 'FOLIO', className: 'all' },
         { target: 1, title: 'FECHA', className: 'all' },
@@ -87,10 +81,10 @@ TablaAsistencia = $('#TablaAsistencia').DataTable({
 })
 
 inputBusquedaTable("TablaAsistencia", TablaAsistencia, [{
-    msj: 'Tabla de historial de cortes de caja',
+    msj: 'Lista de asistencia ',
     place: 'top'
 }], {
-    msj: "Filtre los resultados",
+    msj: "Filtre los resultados de la lista de asistencia",
     place: 'top'
 }, "col-12")
 
