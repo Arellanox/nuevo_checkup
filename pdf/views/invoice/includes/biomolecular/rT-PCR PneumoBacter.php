2f90bb50b44dac33836d2d1284c716c04bf423c9
Josue de la Cruz <arellanox0392@gmail.com>
2024-01-03 18:36:48 -0600

    Update rT-PCR PneumoBacter.php

--- pdf/views/invoice/includes/biomolecular/rT-PCR PneumoBacter.php
+++ pdf/views/invoice/includes/biomolecular/rT-PCR PneumoBacter.php
@@ -37,7 +37,7 @@
         if ($value->resultado != 'LABEL_BIOMOLECULAR') {
     ?>
             <tr>
-                <td style="text-align: left;" class=""><?php echo $value->nombre ?></td>
+                <td style="text-align: left;" class="cursive"><?php echo $value->nombre ?></td>
                 <td class="<?php echo $value->resultado == 'POSITIVO' ? "bold rojo" : "bold";  ?>" style="text-align:center;"><?php if ($value->resultado != 'N/A') echo $value->resultado ?></td>
                 <td style="text-align:center;"><?php if ($value->resultado != 'N/A') echo "NO DETECTADO"; ?></td>
             </tr>
