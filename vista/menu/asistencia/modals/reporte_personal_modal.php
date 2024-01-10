6c972c177856529a6a2a4ef06926d8fb9ec634ae
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2024-01-10 14:49:04 -0600

    Update reporte_personal_modal.php

--- vista/menu/asistencia/modals/reporte_personal_modal.php
+++ vista/menu/asistencia/modals/reporte_personal_modal.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="modalReportePersonal" tabindex="-1" data-bs-backdrop="static" data-bs-focus="false" data-bs-keyboard="true" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable" role="document">
+    <div class="modal-dialog modal-fullscreen modal-dialog-centered modal-dialog-scrollable" role="document">
 
         <div class="modal-content">
             <div class="modal-header">
@@ -7,58 +7,80 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-6 col-xl-6">
-                        <div class="mb-3">
-                            <label for="FechaInicioPdf">Fecha de Inicio:</label>
-                            <input type="date" name="FechaInicioPdf" value="" class="form-control input-form" required id="FechaInicioPdf">
+                <div class="row" style="max-height: 100%;height: 100%;">
+                    <!-- Formulario -->
+                    <section class="col-12 col-lg overflow-auto" style="height: 100%;">
+                        <div class="rounded p-3 shadow my-2">
+                            <div class="row">
+                                <div class="col-12 col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="FechaInicioPdf">Fecha de Inicio:</label>
+                                        <input type="date" name="FechaInicioPdf" value="" class="form-control input-form" required id="FechaInicioPdf">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="FechaFinalPdf">Fecha Final:</label>
+                                        <input type="date" name="FechaFinalPdf" value="" class="form-control input-form" required id="FechaFinalPdf">
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-12 col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="vacaciones">Vacaciones:</label>
+                                        <input type="text" name="vacaciones" value="" class="form-control input-form" required id="vacaciones">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="permisosCGS">Permisos CGS:</label>
+                                        <input type="text" name="permisosCGS" value="" class="form-control input-form" required id="permisosCGS">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="incapacidad">Incapacidad:</label>
+                                        <input type="text" name="incapacidad" value="" class="form-control input-form" required id="incapacidad">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="faltaInjustificada">Faltas injustificada:</label>
+                                        <input type="text" name="faltaInjustificada" value="" class="form-control input-form" required id="faltaInjustificada">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="hrsExtras">Hrs. Extras:</label>
+                                        <input type="text" name="hrsExtras" value="" class="form-control input-form" required id="hrsExtras">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="mb-3">
+                                        <label for="permisoSGS">Permiso SGS:</label>
+                                        <input type="text" name="permisoSGS" value="" class="form-control input-form" required id="permisoSGS">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-6 col-xl-6">
-                        <div class="mb-3">
-                            <label for="FechaFinalPdf">Fecha Final:</label>
-                            <input type="date" name="FechaFinalPdf" value="" class="form-control input-form" required id="FechaFinalPdf">
+                    </section>
+
+                    <!-- Vista de formulario (Escritorio o mas) -->
+                    <section class="col-12 col-lg-3 overflow-auto" style="height: 100%;">
+                        <div class="rounded p-3 shadow my-2">
+                            <table class="table table-hover display responsive" id=""></table>
                         </div>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="col-6">
-                        <div class="mb-3">
-                            <label for="vacaciones">Vacaciones:</label>
-                            <input type="text" name="vacaciones" value="" class="form-control input-form" required id="vacaciones">
+                    </section>
+                    <!-- Vista de formulario (Escritorio o mas) -->
+                    <section class="col-12 col-lg-4 overflow-auto" style="height: 100%;">
+                        <div id="pdfviewer" style="height: 100%;">
+                            <div id="adobe-dc-view"></div>
+
+
                         </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="mb-3">
-                            <label for="permisosCGS">Permisos CGS:</label>
-                            <input type="text" name="permisosCGS" value="" class="form-control input-form" required id="permisosCGS">
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="mb-3">
-                            <label for="incapacidad">Incapacidad:</label>
-                            <input type="text" name="incapacidad" value="" class="form-control input-form" required id="incapacidad">
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="mb-3">
-                            <label for="faltaInjustificada">Faltas injustificada:</label>
-                            <input type="text" name="faltaInjustificada" value="" class="form-control input-form" required id="faltaInjustificada">
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="mb-3">
-                            <label for="hrsExtras">Hrs. Extras:</label>
-                            <input type="text" name="hrsExtras" value="" class="form-control input-form" required id="hrsExtras">
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="mb-3">
-                            <label for="permisoSGS">Permiso SGS:</label>
-                            <input type="text" name="permisoSGS" value="" class="form-control input-form" required id="permisoSGS">
-                        </div>
-                    </div>
+                    </section>
                 </div>
             </div>
             <div class="modal-footer">
