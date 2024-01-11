9d3b29d2faff8396b0b7237ffc1e9ff3210695db
garicano25 <112988491+garicano25@users.noreply.github.com>
2024-01-11 13:20:28 -0600

    modal reporte

--- vista/menu/asistencia/modals/reporte_modal.php
+++ vista/menu/asistencia/modals/reporte_modal.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="modalReporteExcel" tabindex="-1" data-bs-backdrop="static" data-bs-focus="false" data-bs-keyboard="true" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+    <div class="modal-dialog modal-fullscreen modal-dialog-centered modal-dialog-scrollable" role="document">
 
         <div class="modal-content">
             <div class="modal-header">
@@ -8,18 +8,31 @@
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-12 col-xl-12">
-                        <div class="mb-3">
+                    <div class="col-12 col-xl-5">
+                        <div class="mb-2">
                             <label for="FechaInicio">Fecha de Inicio:</label>
                             <input type="date" name="FechaInicio" value="" class="form-control input-form" required id="FechaInicio">
                         </div>
                     </div>
-                    <div class="col-12 col-xl-12">
-                        <div class="mb-3">
+                    <div class="col-12 col-xl-5">
+                        <div class="mb-2">
                             <label for="FechaFinal">Fecha Final:</label>
                             <input type="date" name="FechaFinal" value="" class="form-control input-form" required id="FechaFinal">
                         </div>
                     </div>
+                    <div class="col-12 col-xl-2">
+                        <div class="mb-2 mt-4">
+                            <button type="button" class="btn btn-secondary buttons-excel buttons-html5 btn-success" id="generTablaReporte">
+                                <i class="bi bi-search"></i> Consultar Información
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-2 rounded p-3 shadow my-2">
+
+                    <table id="tablaReporteAsistencias" class="table table-hover display responsive"></table>
+
+
                 </div>
             </div>
             <div class="modal-footer">
