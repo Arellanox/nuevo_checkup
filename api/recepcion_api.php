7381718915179ef7f3cbf4de67701ef259cc9308
Josue de la Cruz <arellanox0392@gmail.com>
2024-01-12 15:17:45 -0600

    Update recepcion_api.php

--- api/recepcion_api.php
+++ api/recepcion_api.php
@@ -110,7 +110,7 @@ switch ($api) {
         if ($estado_paciente == 1) {
             # si el paciente es aceptado, cargar los estudios correspondientes
             rename($identificacion, "../../archivos/identificaciones/" . $idTurno . ".png");
-            $response = $master->insertByProcedure('sp_recepcion_detalle_paciente_g', array($idTurno, $idPaquete, null));
+            $response = $master->insertByProcedure('sp_recepcion_detalle_paciente_g', array($idTurno, $idPaquete, null, 1));
             #aqui subir las ordenes medicas si las hay
             #crear la carpeta de tunos dentro de 
 
@@ -150,7 +150,7 @@ switch ($api) {
                 # si hay algo en el arreglo lo insertamos
                 foreach ($servicios as $key => $value) {
                     // print_r($servicios);
-                    $response2 = $master->insertByProcedure('sp_recepcion_detalle_paciente_g', array($idTurno, null, $value));
+                    $response2 = $master->insertByProcedure('sp_recepcion_detalle_paciente_g', array($idTurno, null, $value, 1));
                 }
             }
         }
