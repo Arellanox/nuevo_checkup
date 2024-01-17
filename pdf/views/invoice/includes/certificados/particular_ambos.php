f7e7c83bf9e27efa90cc1676204b0feb3969a74d
Josue de la Cruz <arellanox0392@gmail.com>
2024-01-17 10:21:01 -0600

    Update particular_ambos.php
    se puso el servicio de TELE DE TÓRAX POSTERO ANTERIOR (PA) por que a juan se le olvido

--- pdf/views/invoice/includes/certificados/particular_ambos.php
+++ pdf/views/invoice/includes/certificados/particular_ambos.php
@@ -94,7 +94,7 @@ $genero = strtolower($resultados[0]->GENERO);
 
 
 // echo "<pre>";
-// var_dump($genero);
+// var_dump($resultado);
 // echo "</pre>";
 
 
@@ -121,7 +121,7 @@ $particular = array(
         "audiometria" => $cuerpo['audiometria'],
         "espirometria" => $cuerpo['espirometria'],
         "prueba_esfuerzo" => $cuerpo['prueba_esfuerzo'],
-        "rx_tele_torax" => "Estudio de torax de aspecto normal",
+        "rx_tele_torax" => $servicios['TELE DE TÓRAX POSTERO ANTERIOR (PA)'],
         "rx_lumbar_anteroposterior" =>  $servicios['COLUMNA LUMBAR ANTEROPOSTERIOR']->INTERPRETACION,
         "rx_lumbar_lateral" => $servicios['COLUMNA LUMBAR LATERAL']->INTERPRETACION,
         "ultrasonido_abdominal" => $servicios['ULTRASONIDO DE ABDOMEN COMPLETO']->INTERPRETACION,
