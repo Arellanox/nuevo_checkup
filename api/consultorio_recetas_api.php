a3902e90ee1182ebf938d0b28f2e5f6d661fe2fa
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2024-01-26 16:11:30 -0600

    modificacion recetas medicas

--- api/consultorio_recetas_api.php
+++ api/consultorio_recetas_api.php
@@ -1,5 +1,13 @@
 <?php
 include_once "../clases/master_class.php";
+require_once "../clases/token_auth.php";
+
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+if (!$tokenValido) {
+    // $tokenVerification->logout();
+    // exit;
+}
 
 $master = new Master();
 $api = $_POST['api'];
@@ -16,6 +24,7 @@ $via_de_administracion = $_POST['via_de_administracion'];
 $duracion_de_tratamiento = $_POST['duracion_de_tratamiento'];
 $indicaciones_de_uso = $_POST['indicaciones_de_uso'];
 $activo = isset($_POST['ACTIVO']) ? null : 1;
+$medico = $_SESSION['id'];
 
 $parametros = $master->setToNull(array(
     $id_receta,
@@ -28,31 +37,37 @@ $parametros = $master->setToNull(array(
     $frecuencia,
     $via_de_administracion,
     $duracion_de_tratamiento,
-    $indicaciones_de_uso
+    $indicaciones_de_uso,
+    $medico
 ));
 
-switch($api){
-    //Insertar datos en la tabla consultorio_recetas
+switch ($api) {
+        //Insertar datos en la tabla consultorio_recetas
     case 1:
-        $response = $master->insertByProcedure("sp_consultorio_recetas_g", $parametros);
+        if (empty($medico)) {
+            $response = 'Su sesión ha caducado, inicie nuevamente';
+        } else {
+
+            $response = $master->insertByProcedure("sp_consultorio_recetas_g", $parametros);
+        }
         break;
 
-    //buscar datos en la tabla consultorio recetas
+        //buscar datos en la tabla consultorio recetas
     case 2:
         $response = $master->getByProcedure("sp_consultorio_recetas_b", [$turno_id]);
         break;
 
     case 3:
-        break;    
-        
-    //Desactivar registros de recetas
+        break;
+
+        //Desactivar registros de recetas
     case 4:
         $response = $master->deleteByProcedure("sp_consultorio_recetas_e", [$id_receta]);
-        break;    
+        break;
 
     default:
-    $response = "API no definida";
-        break;    
+        $response = "API no definida";
+        break;
 }
 
 echo $master->returnApi($response);
