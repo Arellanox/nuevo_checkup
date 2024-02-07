43e01fe770106deed74e3348e4787bd9e861b261
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2024-02-07 13:41:13 -0600

    Nuevo boton para el estado de lostes

--- vista/procedencia/estudios-laboratorio/contenido/js/franq-botones.js
+++ vista/procedencia/estudios-laboratorio/contenido/js/franq-botones.js
@@ -12,3 +12,16 @@ $(document).on('click', '.btn-agregar_paciente', async function (event) {
 })
 
 
+
+// Abre el estado de los lotes
+$(document).on('click', '-#muestras_enviadas', async function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Configura y obten los datos antes de la tabla de lotes
+
+
+    // Una vez obtenido, abre el modal (esto desde el complete del ajax de la tabla)
+})
+
+
