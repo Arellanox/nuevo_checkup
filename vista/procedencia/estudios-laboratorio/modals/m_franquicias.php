454cfc2dbbedc3f74a9aa03a7bf3ed777a07c3a2
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2024-02-09 11:45:39 -0600

    alerts registro muestra y criterios rechazo

--- vista/procedencia/estudios-laboratorio/modals/m_franquicias.php
+++ vista/procedencia/estudios-laboratorio/modals/m_franquicias.php
@@ -4,3 +4,5 @@ include 'franq_agregar_paciente.html';
 include 'franq_enviar_lotes.html';
 include 'franq_lotes_enviados.html';
 include 'franq_tomarMuestra.html';
+include 'alerts_criterios_rechazo.html';
+include 'alerts_registro_muestras.html';
