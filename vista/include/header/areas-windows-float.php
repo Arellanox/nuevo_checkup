779d0e8f7893685ad801b45be8fca6a96dac5474
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2024-02-12 14:30:59 -0600

    Update areas-windows-float.php

--- vista/include/header/areas-windows-float.php
+++ vista/include/header/areas-windows-float.php
@@ -205,19 +205,19 @@ if (
         <div class="dropdown ">
 
             <a class="dropdown-toggle align-items-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#board-menu-empresas" aria-expanded="false">
-                <i class="bi bi-hospital"></i> Empresas
+                <i class="bi bi-hospital"></i> Tomas Externas
             </a>
 
             <div class="collapse" id="board-menu-empresas">
                 <ul style="padding-left: 15px;" class="btn-toggle-nav text-black list-unstyled fw-normal pb-1 small shadow">
 
-                    <?php //if ($_SESSION['vista']['ESTUDIOS_CALIDAD_EXCEL'] == 1) : 
+                    <?php if ($_SESSION['vista']['FRANQUICIAS'] == 1) :
                     ?>
-                    <a class="dropdown-a text-white align-items-center" type="button" href="<?php echo $https . $url . '/' . $appname . '/vista/procedencia/estudios-laboratorio/'; ?>">
-                        <i class="bi bi-people"></i> Pacientes
-                    </a>
+                        <a class="dropdown-a text-white align-items-center" type="button" href="<?php echo $https . $url . '/' . $appname . '/vista/procedencia/estudios-laboratorio/'; ?>">
+                            <i class="bi bi-people"></i> Solicitud de Laboratorio
+                        </a>
 
-                    <?php //endif; 
+                    <?php endif;
                     ?>
                 </ul>
             </div>
@@ -225,6 +225,7 @@ if (
     </li>
 <?php endif;
 ?>
+
 
 <?php if ($_SESSION['perfil'] == 1) : ?>
     <li class="nav-item Recepción">
