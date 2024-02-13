613e356286d4bd786aee287f3e041b847a2d71e7
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2024-02-13 11:49:27 -0600

    tablas de muestras de tablas

--- vista/menu/recepcion-muestras/contenido/js/muestras-tabla.js
+++ vista/menu/recepcion-muestras/contenido/js/muestras-tabla.js
@@ -2,120 +2,120 @@
 let dataListaLotes = { api: 5, id_cliente: session.id_cliente }; //data de lista de lotes
 
 
-tablaMuestras = $('#TablaMuestras').DataTable({
-  language: {
-    url: "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-  },
-  lengthChange: false,
-  info: true,
-  paging: false,
-  scrollY: autoHeightDiv(0, 384),
-  scrollCollapse: true,
-  ajax: {
-    dataType: 'json',
-    data: function (d) {
-      return $.extend(d, dataListaPaciente);
-    },
-    method: 'POST',
-    url: '../../../api/toma_de_muestra_api.php',
-    beforeSend: function () { loader("In") },
-    complete: function () {
-      loader("Out", 'bottom')
+// tablaMuestras = $('#TablaMuestras').DataTable({
+//   language: {
+//     url: "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+//   },
+//   lengthChange: false,
+//   info: true,
+//   paging: false,
+//   scrollY: autoHeightDiv(0, 384),
+//   scrollCollapse: true,
+//   ajax: {
+//     dataType: 'json',
+//     data: function (d) {
+//       return $.extend(d, dataListaPaciente);
+//     },
+//     method: 'POST',
+//     url: '../../../api/toma_de_muestra_api.php',
+//     beforeSend: function () { loader("In") },
+//     complete: function () {
+//       loader("Out", 'bottom')
 
-      //Para ocultar segunda columna
-      reloadSelectTable()
-    },
-    dataSrc: 'response.data'
-  },
-  createdRow: function (row, data, dataIndex) {
-    if (data.MUESTRA_TOMADA == 1) {
-      $(row).addClass('bg-success text-white');
-    }
-  },
-  columns: [
-    {
-      data: 'ID_PACIENTE', render: function (data) {
-        return '';
-      }
-    },
-    { data: 'NOMBRE_COMPLETO' },
-    { data: 'PREFOLIO' },
-    { data: 'EDAD' },
-    { data: 'EDAD' },
-    // {defaultContent: 'En progreso...'}
-  ],
-  columnDefs: [
-    { "width": "10px", "targets": 0 },
-  ],
-
-})
-
-loaderDiv("Out", null, "#loader-muestras", '#loaderDivmuestras');
-// selectDatatable('TablaMuestras', tablaMuestras, 0, 0, 0, 0, function (selectTR = null, array = null) {
+//       //Para ocultar segunda columna
+//       reloadSelectTable()
+//     },
+//     dataSrc: 'response.data'
+//   },
+//   createdRow: function (row, data, dataIndex) {
+//     if (data.MUESTRA_TOMADA == 1) {
+//       $(row).addClass('bg-success text-white');
+//     }
+//   },
+//   columns: [
+//     {
+//       data: 'ID_PACIENTE', render: function (data) {
+//         return '';
+//       }
+//     },
+//     { data: 'NOMBRE_COMPLETO' },
+//     { data: 'PREFOLIO' },
+//     { data: 'EDAD' },
+//     { data: 'EDAD' },
+//     // {defaultContent: 'En progreso...'}
+//   ],
+//   columnDefs: [
+//     { "width": "10px", "targets": 0 },
+//   ],
 
 // })
 
+// loaderDiv("Out", null, "#loader-muestras", '#loaderDivmuestras');
+// // selectDatatable('TablaMuestras', tablaMuestras, 0, 0, 0, 0, function (selectTR = null, array = null) {
 
-//new selectDatatable:
-selectTable('#TablaMuestras', tablaMuestras, { unSelect: true, movil: true, reload: ['col-xl-9'] }, async function (select, data, callback) {
-  selectListaMuestras = data;
-
-  if (select == 1) {
-
-    //Activa o desactiva el boton
-    if (selectListaMuestras.MUESTRA_TOMADA == 1) {
-      $('#muestra-tomado').prop('disabled', true)
-    } else {
-      $('#muestra-tomado').prop('disabled', false)
-    }
-
-    //Procesos
-    await obtenerPanelInformacion(selectListaMuestras['ID_TURNO'], 'pacientes_api', 'paciente', '#panel-informacion', '_lab')
-    await obtenerListaEstudiosContenedores(selectListaMuestras['ID_TURNO'])
-
-    //Muestra las columnas
-    callback('In')
-  } else {
-
-    callback('Out')
-    selectListaMuestras = null;
-  }
-})
+// // })
 
 
+// //new selectDatatable:
+// selectTable('#TablaMuestras', tablaMuestras, { unSelect: true, movil: true, reload: ['col-xl-9'] }, async function (select, data, callback) {
+//   selectListaMuestras = data;
 
-inputBusquedaTable('TablaMuestras', tablaMuestras, [{
-  msj: 'Los pacientes con muestras tomadas se visualizarán confirmados de color verde',
-  place: 'top'
-}], [], 'col-12')
+//   if (select == 1) {
 
+//     //Activa o desactiva el boton
+//     if (selectListaMuestras.MUESTRA_TOMADA == 1) {
+//       $('#muestra-tomado').prop('disabled', true)
+//     } else {
+//       $('#muestra-tomado').prop('disabled', false)
+//     }
 
-function obtenerListaEstudiosContenedores(idturno = null) {
-  return new Promise(resolve => {
+//     //Procesos
+//     await obtenerPanelInformacion(selectListaMuestras['ID_TURNO'], 'pacientes_api', 'paciente', '#panel-informacion', '_lab')
+//     await obtenerListaEstudiosContenedores(selectListaMuestras['ID_TURNO'])
 
-    ajaxAwait({ api: 2, id_turno: idturno }, 'toma_de_muestra_api', { callbackAfter: true, WithoutResponseData: true }, false, (row) => {
-      let html = '';
-      for (var i = 0; i < row.length; i++) {
-        // console.log(row[i]);
-        html += '<li class="list-group-item">';
-        html += row[i]['GRUPO'];
-        html += '<i class="bi bi-arrow-right-short"></i><strong>' + row[i]['MUESTRA'] + '</strong> - <strong>' + row[i]['CONTENEDOR'] + '</strong></li>';
+//     //Muestra las columnas
+//     callback('In')
+//   } else {
 
-      }
-      $('#lista-estudios-paciente').html(html);
-
-      //Complete
-      loaderDiv("Out", null, "#loader-muestras", '#loaderDivmuestras');
-      resolve(1);
-    });
-
-
-  });
-}
+//     callback('Out')
+//     selectListaMuestras = null;
+//   }
+// })
 
 
-//Panel turnos, mandar id fisica al  principio
-obtenerPanelInformacion(7, null, "turnos_panel", '#turnos_panel')
+
+// inputBusquedaTable('TablaMuestras', tablaMuestras, [{
+//   msj: 'Los pacientes con muestras tomadas se visualizarán confirmados de color verde',
+//   place: 'top'
+// }], [], 'col-12')
+
+
+// function obtenerListaEstudiosContenedores(idturno = null) {
+//   return new Promise(resolve => {
+
+//     ajaxAwait({ api: 2, id_turno: idturno }, 'toma_de_muestra_api', { callbackAfter: true, WithoutResponseData: true }, false, (row) => {
+//       let html = '';
+//       for (var i = 0; i < row.length; i++) {
+//         // console.log(row[i]);
+//         html += '<li class="list-group-item">';
+//         html += row[i]['GRUPO'];
+//         html += '<i class="bi bi-arrow-right-short"></i><strong>' + row[i]['MUESTRA'] + '</strong> - <strong>' + row[i]['CONTENEDOR'] + '</strong></li>';
+
+//       }
+//       $('#lista-estudios-paciente').html(html);
+
+//       //Complete
+//       loaderDiv("Out", null, "#loader-muestras", '#loaderDivmuestras');
+//       resolve(1);
+//     });
+
+
+//   });
+// }
+
+
+// //Panel turnos, mandar id fisica al  principio
+// obtenerPanelInformacion(7, null, "turnos_panel", '#turnos_panel')
 
 
 
@@ -124,72 +124,72 @@ obtenerPanelInformacion(7, null, "turnos_panel", '#turnos_panel')
 
 
 // |-------------------------------- Tabla de lista de lotes -----------------------------------------|
-TablaListaLotes = $('#TablaListaLotes').DataTable({
-  language: {
-      url: "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-  },
-  lengthChange: false,
-  info: true,
-  paging: false,
-  scrollY: '47vh',
-  scrollCollapse: true,
-  ajax: {
-      dataType: 'json',
-      data: function (d) {
-          return $.extend(d, dataListaLotes);
-      },
-      method: 'POST',
-      url: `${http}${servidor}/${appname}/api/maquilas_api.php`,
-      complete: function () {
-          // if (TablaListaLotes_inicio)
-          //     $('#EnvioLotesPacientes').modal('show');
-      },
-      dataSrc: 'response.data'
-  },
-  columns: [
-      { data: 'COUNT' },
-      { data: 'FOLIO' },
-      {
-          data: 'ESTATUS', render: function (data) {
-              return ifnull(data, 'N/A', true)
-          }
-      },
-      {
-          data: 'RUTA_REPORTE', render: (data) => {
-              // Inicializar un arreglo vacío para contener nuestros botones
-              var buttons = [];
+// TablaListaLotes = $('#TablaListaLotes').DataTable({
+//   language: {
+//       url: "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+//   },
+//   lengthChange: false,
+//   info: true,
+//   paging: false,
+//   scrollY: '47vh',
+//   scrollCollapse: true,
+//   ajax: {
+//       dataType: 'json',
+//       // data: function (d) {
+//       //     return $.extend(d, dataListaLotes);
+//       // },
+//       method: 'POST',
+//       url: `${http}${servidor}/${appname}/api/maquilas_api.php`,
+//       complete: function () {
+//           // if (TablaListaLotes_inicio)
+//           //     $('#EnvioLotesPacientes').modal('show');
+//       },
+//       dataSrc: 'response.data'
+//   },
+//   columns: [
+//       { data: 'COUNT' },
+//       { data: 'FOLIO' },
+//       {
+//           data: 'ESTATUS', render: function (data) {
+//               return ifnull(data, 'N/A', true)
+//           }
+//       },
+//       {
+//           data: 'RUTA_REPORTE', render: (data) => {
+//               // Inicializar un arreglo vacío para contener nuestros botones
+//               var buttons = [];
 
-              buttons.push(
-                  '<a href="' + data + '" target="_blank" class="btn btn-borrar me-2">' +
-                  '<i class="bi bi-file-earmark-pdf-fill"></i>' +
-                  '</a>'
-              );
+//               buttons.push(
+//                   '<a href="' + data + '" target="_blank" class="btn btn-borrar me-2">' +
+//                   '<i class="bi bi-file-earmark-pdf-fill"></i>' +
+//                   '</a>'
+//               );
 
-              // Unir todos los botones con un espacio y devolver la cadena HTML
-              return '<div class="d-flex justify-content-start align-items-center">' + buttons.join(' ') + '</div>';
-          }
-      },
-      {
-          data: 'REGISTRADO', render: function (data) {
+//               // Unir todos los botones con un espacio y devolver la cadena HTML
+//               return '<div class="d-flex justify-content-start align-items-center">' + buttons.join(' ') + '</div>';
+//           }
+//       },
+//       {
+//           data: 'REGISTRADO', render: function (data) {
 
-              const formattedDate = formatoFecha2(data, [0, 1, 5, 2, 2, 2, 0], null); {
+//               const formattedDate = formatoFecha2(data, [0, 1, 5, 2, 2, 2, 0], null); {
 
-                  // Separar la fecha y la hora basado en la coma
-                  const parts = formattedDate.split(', ');
-                  const datePart = parts[0];
-                  const timePart = parts[1];
+//                   // Separar la fecha y la hora basado en la coma
+//                   const parts = formattedDate.split(', ');
+//                   const datePart = parts[0];
+//                   const timePart = parts[1];
 
-                  // Retornar la fecha y la hora envueltas en spans con las clases correspondientes
-                  return `
-                          <span class="d-block">${datePart}</span>
-                          <span class="d-block">${timePart}</span>`;
-              }
-          }
-      },
-      { data: 'USUARIO' },
-  ],
-  columnDefs: [
-      { "width": "10px", "targets": [0, 3] },
-  ],
+//                   // Retornar la fecha y la hora envueltas en spans con las clases correspondientes
+//                   return `
+//                           <span class="d-block">${datePart}</span>
+//                           <span class="d-block">${timePart}</span>`;
+//               }
+//           }
+//       },
+//       { data: 'USUARIO' },
+//   ],
+//   columnDefs: [
+//       { "width": "10px", "targets": [0, 3] },
+//   ],
 
-})
+// })
--- vista/menu/recepcion-muestras/contenido/muestras.php
+++ vista/menu/recepcion-muestras/contenido/muestras.php
@@ -1,6 +1,6 @@
-<div class="col-12 loader" id="loader">
+<!-- <div class="col-12 loader" id="loader">
   <div class="preloader" id="preloader"> </div>
-</div>
+</div> -->
 
 <!-- tabs para movil -->
 <div id="tab-button"></div>
@@ -10,34 +10,34 @@
   <!-- Lotes -->
   <!-- Los lotes con estatus finalizados, llevan 3 colores, verde (aceptados), amarillo(Aceptados con recervas), rojo(rechazados) -->
   <!-- El numero de conteo, la procedencia, folio del lote, fecha del envio del lote, estatus (visibles) -->
-        <!-- registrado por, formato(pdf) (invisible) -->
+  <!-- registrado por, formato(pdf) (invisible) -->
   <div class="col-12 col-xl-4 tab-first" id="tab-paciente" style="margin-right: -5px !important;">
     <div class="rounded p-3 shadow my-2" id="lista-pacientes">
-      <h5>Lista de pacientes</h5>
+      <h5>Lista de lotes</h5>
 
       <!-- Control de turnos -->
-      <div id="turnos_panel"></div>
+      <!-- <div id="turnos_panel"></div> -->
 
-      <table class="table display responsive" id="TablaMuestras" style="width: 100%">
+      <table class="table responsive" id="TablaListaLotes" style="width: 100%">
         <thead class="">
           <tr>
-            <th class="all">#</th>
-            <th class="all">Nombre</th>
-            <th class="min-tablet">Folio</th>
-            <th class="tablet">Compañia</th>
-            <th class="tablet">Edad</th>
-          </tr>
+            <th scope="col d-flex justify-content-center" class="all">#</th>
+            <th scope="col d-flex justify-content-center" class="all">Procedencia</th>
+            <th scope="col d-flex justify-content-center" class="all">Folio</th>
+            <th scope="col d-flex justify-content-center" class="all">Fecha envio</th>
+            <th scope="col d-flex justify-content-center" class="all">Estatus</th>
+            <th scope="col d-flex justify-content-center" class="none">Registrado por: </th>
+            <th scope="col d-flex justify-content-center" class="none">Formato</th>
+
         </thead>
-        <tbody>
-        </tbody>
       </table>
     </div>
   </div>
 
   <!-- <div class="col-12 col-xl-3 tab-second" id="tab-informacion" style="margin-right: -5px !important;  display:none !important">
     <div class="rounded p-3 shadow my-2" id="panel-informacion"> </div>
-
   </div> -->
+
   <!-- PAcientes del lote -->
   <!-- Informacion del lote, como encabezado -->
 
@@ -45,18 +45,29 @@
 
   <!-- Resultado, fecha de resultado, fecha de registro, registrado por, edad, sexo (invisibles) -->
   <div class="col-12 col-xl-8 tab-second" id="tab-reporte" style="margin-right: -5px !important;">
-    <table class="table responsive" id="TablaListaLotes" style="width: 100%">
-      <thead class="">
-        <tr>
-          <th scope="col d-flex justify-content-center" class="all 5-%">#</th>
-          <th scope="col d-flex justify-content-center" class="all">Folio</th>
-          <th scope="col d-flex justify-content-center" class="all">Estatus</th>
-          <th scope="col d-flex justify-content-center" class="all 5-%">Formato</th>
-          <th scope="col d-flex justify-content-center" class="none">Fecha registro:</th>
-          <th scope="col d-flex justify-content-center" class="none">Registrado por: </th>
 
-      </thead>
-    </table>
+    <div class="d-flex justify-content-end">
+      <div class="rounded p-3 shadow my-2 w-50">
+        <p>Informacion del lote</p>
+      </div>
+    </div>
+
+    <div class="rounded p-3 shadow my-2">
+      <table class="table responsive" id="TablaPacientesLotes" style="width: 100%">
+        <thead class="">
+          <tr>
+            <th scope="col d-flex justify-content-center" class="all 5-%">#</th>
+            <th scope="col d-flex justify-content-center" class="all">Nombre</th>
+            <th scope="col d-flex justify-content-center" class="all">Estatus</th>
+            <th scope="col d-flex justify-content-center" class="none">Resultado</th>
+            <th scope="col d-flex justify-content-center" class="none">Fecha resultado:</th>
+            <th scope="col d-flex justify-content-center" class="none">Fecha registro:</th>
+            <th scope="col d-flex justify-content-center" class="none">Registrado por: </th>
+            <th scope="col d-flex justify-content-center" class="none">Edad:</th>
+            <th scope="col d-flex justify-content-center" class="none">Sexo:</th>
+        </thead>
+      </table>
+    </div>
   </div>
 
 
