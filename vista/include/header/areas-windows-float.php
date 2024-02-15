756e24261a9fe499897172343a9e5b0cd1e8be32
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2024-02-15 15:22:28 -0600

    Update areas-windows-float.php

--- vista/include/header/areas-windows-float.php
+++ vista/include/header/areas-windows-float.php
@@ -208,19 +208,19 @@ if (
                 <i class="bi bi-hospital"></i> Tomas Externas
             </a>
 
-            <div class="collapse" id="board-menu-empresas">
+            <!-- <div class="collapse" id="board-menu-empresas">
                 <ul style="padding-left: 15px;" class="btn-toggle-nav text-black list-unstyled fw-normal pb-1 small shadow">
 
-                    <!-- <?php if ($_SESSION['vista']['FRANQUICIAS'] == 1) :
+                    <?php if ($_SESSION['vista']['FRANQUICIAS'] == 1) :
                     ?>
                         <a class="dropdown-a text-white align-items-center" type="button" href="<?php echo $https . $url . '/' . $appname . '/vista/procedencia/estudios-laboratorio/'; ?>">
                             <i class="bi bi-people"></i> Solicitud de Laboratorio
                         </a>
 
                     <?php endif;
-                    ?> -->
+                    ?>
                 </ul>
-            </div>
+            </div> -->
         </div>
     </li>
 <?php endif;
