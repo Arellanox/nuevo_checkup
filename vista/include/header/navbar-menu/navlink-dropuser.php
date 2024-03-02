94c3006a3d104d9fbb50cbdcdb57ab3460e23cbb
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2024-03-02 10:11:55 -0600

    background

--- vista/include/header/navbar-menu/navlink-dropuser.php
+++ vista/include/header/navbar-menu/navlink-dropuser.php
@@ -1,4 +1,4 @@
-<a href="#LogOut" class="dropdown-a"><i class="bi bi-box-arrow-up"></i> Cerrar Sesión</a>
-<a href="" class="btn-promociones dropdown-a promociones-block  d-lg-none" data-bs-toggle="modal" data-bs-target="#modalPromociones" style="display: none;">
+<a href="" class="dropdown-a promociones-block d-lg-none" data-bs-toggle="modal" data-bs-target="#modalPromociones">
     <i class="bi bi-tag-fill"></i> Promociones
 </a>
+<a href="#LogOut" class="dropdown-a"><i class="bi bi-box-arrow-up"></i> Cerrar Sesión</a>
