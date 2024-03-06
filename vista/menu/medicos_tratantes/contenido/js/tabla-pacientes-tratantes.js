02fa1d4a53b31788caac99b90bf323ea1c43ffc3
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2024-03-06 10:58:29 -0600

    Update tabla-pacientes-tratantes.js

--- vista/menu/medicos_tratantes/contenido/js/tabla-pacientes-tratantes.js
+++ vista/menu/medicos_tratantes/contenido/js/tabla-pacientes-tratantes.js
@@ -26,7 +26,7 @@ var dataJsonTablaEstudiosPaciente;
 // ###################### Tablas ################################################
 
 // ==============================================================================
-
+dataPacientesTratantes = { api: 4 };
 //Tbla donde se vizualiza los Médicos tratantes ya registrados en la base de datos
 tablaPacientesTratantes = $("#tablaPacientesTratantes").DataTable({
     language: { url: "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json", },
