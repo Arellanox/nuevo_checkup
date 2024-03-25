7bef03fedd3bbf3d1518d661d9839a068e0e2457
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2024-03-25 12:18:53 -0600

    pacientes subrogados (subida de resultados)

--- vista/menu/pacientes-subrogados/contenido/js/v-pacientes.js
+++ vista/menu/pacientes-subrogados/contenido/js/v-pacientes.js
@@ -2,7 +2,7 @@
 var id_area, id_turno
 
 
-// Tabla de pacientes
+// Tabla de pacientes subrogados
 dataPacuentesSubrogados = { api: 15 }
 TablaPacientesSubrogados = $('#TablaPacientesSubrogados').DataTable({
     language: {
@@ -84,31 +84,40 @@ selectTable('#TablaPacientesSubrogados', TablaPacientesSubrogados, {
 
     if (select == 1) {
 
-        $('#panel-areas').html('')
-        await recuperarAreas(pacienteSub)
+        $('#panel-areas').html('') // Limpia el panel de areas
+        inputReset.resetInputDrag() // Limpia el dragAndDrop cada vez que se selecciona un nuevo paciente
+        await recuperarAreas(pacienteSub) // Recupera las areas de ese paciente
 
         //Muestra las columnas
         callback('In')
     } else {
-
+        // Oculta las columnas
         callback('Out')
     }
 })
 
-
+// Funcion que recupera las areas del paciente
 function recuperarAreas(data) {
     return new Promise(function (resolve, reject) {
         ajaxAwait(
             { api: 21, turno_id: data['ID_TURNO'] }, 'proveedores_api', { callbackAfter: true }, false, function (data) {
                 let dataAreas = data.response.data
 
+                // Cilco para imprimir las areas
                 dataAreas.forEach(areas => {
+
+                    //Cambia de color si tiene reporte
+                    var estadoDiv = areas.RESULTADO !== null ? 'style="background-color: #198754;"' : '';
+                    var estadoP = areas.RESULTADO !== null ? 'style="color: white"' : '';
+
+                    // Crea el html que se mostrara las areas
                     var cardAreas = `
-                <div class="rounded card p-3 my-3 card-areas " data-id-area="${areas.ID_AREA}" data-id-turno="${areas.ID_TURNO}">
-                    <p>${areas.AREA}</p>
+                <div class="rounded card p-3 my-3 card-areas" ${estadoDiv} data-id-area="${areas.ID_AREA}" data-id-turno="${areas.ID_TURNO}" data-id-reporte="${areas.RESULTADO}">
+                    <p ${estadoP}>${areas.AREA}</p>
                 </div>
                 `;
 
+                    // Se imprimen las areas
                     $('#panel-areas').append(cardAreas)
                 })
 
@@ -118,6 +127,7 @@ function recuperarAreas(data) {
 
 }
 
+// Damos click alguna area
 $('#panel-areas').on('click', '.card-areas', function (e) {
     e.preventDefault();
     e.stopPropagation();
@@ -129,6 +139,8 @@ $('#panel-areas').on('click', '.card-areas', function (e) {
     let $div = $(this)
     id_area = $div.data('id-area');
     id_turno = $div.attr('data-id-turno');
+    resultado = $div.attr('data-id-reporte');
+
 
     $('.card-areas').removeClass('selected');
     $div.addClass('selected');
@@ -140,16 +152,43 @@ $('#panel-areas').on('click', '.card-areas', function (e) {
         $('#tab-btn-Reporte').click();
     }, 200);
 
+    // Evaluamos si el area de ese paciente tiene reporte
+    if (resultado != 'null') {
+
+        // En dado caso que tenga mostramos el boton
+        $('#btn-ReporteArea').fadeIn(0)
+        $('#btn-ver-resultados-areas').html(`<a type="button" target="_blank" href="${resultado}" style="margin-bottom:4px"><i class="bi bi-file-earmark-pdf"></i> Reporte</a>`);
+    } else {
+
+        // Si no, se oculta
+        $('#btn-ReporteArea').fadeOut()
+    }
+
+
 });
 
-
-InputDragDrop('#dropDocumentoPaciente', (inputArea, salidaInput) => {
+    //Funcion para guarda los reportes que el usuario va a subir
+let inputReset = InputDragDrop('#dropDocumentoPaciente', (inputArea, salidaInput) => {
 
     ajaxAwaitFormData({
         api: 20, turno_id: id_turno, area_id: id_area
-    }, 'proveedores_api', 'subirDocumentoPaciente', { callbackAfter: true }, false, function () {
+    }, 'proveedores_api', 'subirDocumentoPaciente', { callbackAfter: true }, false, function (data) {
+
+        // Una vez enviado los datos, se regresa la url para mostrar el boton con el reporte que se cargo
+        let resultadoEnviado = data.response.data.url[0]['_ruta_reporte']
 
         alertToast('Documento guardado', 'success');
+
+        // Mostramos el boton 
+        $('#btn-ReporteArea').fadeIn(0)
+        $('#btn-ver-resultados-areas').html(`<a type="button" target="_blank" href="${resultadoEnviado}" style="margin-bottom:4px"><i class="bi bi-file-earmark-pdf"></i> Reporte</a>`);
+
+        // Borramos el contenido que tenga en el div de areas
+        $('#panel-areas').html('')
+
+        // Y recargamos la vista
+        recuperarAreas(pacienteSub)
+
 
         // Siempre se ejecuta al final del proceso
         salidaInput({
