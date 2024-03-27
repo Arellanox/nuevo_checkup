49db4091e39657d9657eb117c2110dbefbf52ef5
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2024-03-27 17:48:15 -0600

    Correcciones

--- pdf/views/invoice/includes/biomolecular/PCR_deteccion_mycobacterium.php
+++ pdf/views/invoice/includes/biomolecular/PCR_deteccion_mycobacterium.php
@@ -1,16 +1,38 @@
+<?php
+
+
+function text_bold($string)
+{
+    $nombre_bold = ['Mycobacterium'];
+
+    $new_string = $string;
+
+    // return $new_string;
+
+    foreach ($nombre_bold as $string_search) {
+        $valor_ct = strpos($string, $string_search) !== false;
+        if ($valor_ct)
+            return $new_string = str_replace($string_search, "<span style=' font-style: italic'>$string_search</span>", $string);
+    }
+
+    return $new_string;
+}
+?>
+
+
 <br>
 <br>
 <br>
 <br>
-<p style="position:absolute;top:2px;left:548px;white-space:nowrap" class="ft00">Muestra: <strong style="font-size: 11px"><?php echo $body[9]->resultado ?></strong> </p>
+<!-- <p style="position:absolute;top:2px;left:548px;white-space:nowrap" class="ft00">Muestra: <strong style="font-size: 11px"><?php echo $body[9]->resultado ?></strong> </p> -->
 <table style="width: 100%;font-size: 13.4px">
     <tr style="background-color: darkgrey;">
-        <td style="font-size: 13.4px"><strong>Prueba</strong></td>
-        <td style="font-size: 13.4px"><strong>Resultado</strong></td>
-        <td style="font-size: 13.4px"><strong>Valor de Normalidad</strong></td>
+        <td style="font-size: 14px; padding: 3px;"><strong>Prueba</strong></td>
+        <td style="font-size: 14px; padding: 3px;"><strong>Resultado</strong></td>
+        <td style="font-size: 14px; padding: 3px;"><strong>Valor de Normalidad</strong></td>
     </tr>
     <tr>
-        <td style="font-size: 16.4px"><strong>PCR Detección Mycobacterium tuberculosis</strong></td>
+        <td style="font-size: 16.4px"><strong> </strong></td>
         <td></td>
         <td></td>
     </tr>
@@ -22,23 +44,23 @@
     // $kit = $body[7]->resultado;
     // $NoLote = $body[8]->resultado;
 
-    $kit = 'Lote de Kit';
-    $NoLote = 'Numero de lote';
+    $muestra = $body[2]->resultado;
+    // $NoLote = 'Numero de lote';
 
-    $body = array_slice($body, 0, count($body));
+    $body = array_slice($body, 0, count($body) - 1);
     foreach ($body as $key => $value) {
         if (isset($value->resultado)) {
     ?>
             <tr>
-                <td class="italic" style="font-size: 13.4px; font-style: italic"><?php echo $value->nombre ?></td>
-                <td class="italic" style="font-size: 13.4px; font-style: italic"><?php
-                                                                                    if ($value->resultado == 'POSITIVO') {
-                                                                                        echo "<span style='font-weight:bold'>$value->resultado</span>";
-                                                                                    } else {
-                                                                                        echo $value->resultado;
-                                                                                    }
-                                                                                    ?></td>
-                <td class="italic" style="font-size: 13.4px; font-style: italic">NEGATIVO</td>
+                <td class="italic" style="font-size: 14px; "><?php echo text_bold($value->nombre) ?></td>
+                <td class="italic" style="font-size: 14px; text-align:center;"><?php
+                                                                                if ($value->resultado == 'POSITIVO') {
+                                                                                    echo "<span style='font-weight:bold'>$value->resultado</span>";
+                                                                                } else {
+                                                                                    echo $value->resultado;
+                                                                                }
+                                                                                ?></td>
+                <td class="italic" style="font-size: 14px; text-align:center;">NEGATIVO</td>
             </tr>
     <?php
         }
@@ -52,8 +74,7 @@
 <br>
 <!-- <p style="text-align:justify;"><strong>Comentarios: </strong>Esta prueba identifica en tiempo real la presencia de 9 tipos de Patógenos causantes de ETS (Enfermedades de Transmisión Sexual); el médico tratante es quien realiza la interpretación de este resultado considerando los datos clínicos del paciente.</p> -->
 <br>
-<p style="text-align:justify;"><strong>Método: </strong> PCR Tiempo Real<br>
-    <strong>Tipo de muestra: </strong>Expectoración, Lavado bronquial, Orina <br>
-    Líquidos orgánicos sin hemolisis, Sangre/EDTA, Biopsia, Bloques de parafina <br>
+<p style="text-align:justify;font-size: 14px"><strong style="font-size: 14px">Método: </strong> PCR Tiempo Real<br>
+    <strong style="font-size: 14px">Tipo de muestra: </strong><?php echo $muestra; ?>
 </p>
 <br>
