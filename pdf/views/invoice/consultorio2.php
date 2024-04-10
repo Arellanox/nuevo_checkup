df97419fe0c2af6f9ba49b330c44039767cde65d
Josue de la Cruz <arellanox0392@gmail.com>
2024-04-10 13:10:48 -0600

    Update consultorio2.php

--- pdf/views/invoice/consultorio2.php
+++ pdf/views/invoice/consultorio2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado de interpretación de Historia Clinica</title>
+    <title>Resultado de Consulta Médica</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <!-- <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">  -->
