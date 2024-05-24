37b30da8c0ad1f16f0dda2094120f722be147363
Josue de la Cruz <arellanox0392@gmail.com>
2024-05-24 12:26:40 -0600

    Update interface_api.php

--- api/interface_api.php
+++ api/interface_api.php
@@ -11,7 +11,7 @@ $master->setLog(file_get_contents('php://input'), "asafasd");
 
 // }
 
-echo "OK!";
+echo $_SERVER['SERVER_NAME'] . ": OK!";
 
 $response = $master->insertByProcedure('sp_pseudo_interface', [
     json_encode($datos['datos'])
