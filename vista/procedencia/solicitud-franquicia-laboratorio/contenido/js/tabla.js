8e858c9533e7cd97ccd64609ccaa07a078fe63e9
Ángel Saúl P. C. <pascualcamacho2606@gmail.com>
2025-04-10 10:15:38 -0600

    Eliminación de archivos de ordenes medicas

--- vista/procedencia/solicitud-franquicia-laboratorio/contenido/js/tabla.js
+++ vista/procedencia/solicitud-franquicia-laboratorio/contenido/js/tabla.js
@@ -75,66 +75,6 @@
                 }
             }, // Fecha de muestra (formato)
             { data: 'TOMADOR_MUESTRA' }, // texto
-            {
-                dat: "ID_TURNO", render: function (data) {
-                    api = encodeURIComponent(window.btoa('etiquetas'));
-                    data = encodeURIComponent(window.btoa(data));
-
-                    let url = `${http}${servidor}/${appname}/visualizar_reporte/?api=${api}&turno=${data}`
-                    let buttons = [];
-
-                    buttons.push(
-                        '<a href="' + url + '" target="_blank" class="btn btn-borrar me-2">' +
-                        '<i class="bi bi-file-earmark-pdf-fill"></i> Etiquetas de laboratorio' +
-                        '</a>'
-                    );
-
-                    return '<div class="d-flex justify-content-start align-items-center">' + buttons.join(' ') + '</div>';
-                }
-            },
-            {
-                data: 'REPORTES',
-                render: function (data) {
-                    let buttons = [];
-
-                    if (data) {
-                        // Recorrer cada reporte en los datos
-                        for (const key in data) {
-                            if (Object.hasOwnProperty.call(data, key)) {
-                                const element = data[key];
-                                buttons.push(
-                                    '<a href="' + element + '" target="_blank" class="btn btn-borrar me-2">' +
-                                    '<i class="bi bi-file-earmark-pdf-fill"></i>' +
-                                    '</a>'
-                                );
-                            }
-                        }
-                    } else {
-                        buttons.push(`<div class="badge text-bg-warning px-3 rounded-pill font-weight-normal 
-                            text-dark">Pendiente</div>`)
-                        // Manejar el caso en el que 'data' no es un array (por ejemplo, mostrar un mensaje de error o un valor por defecto)
-                        // console.error('Data is not an array:', data);
-                    }
-
-                    // Unir todos los botones con un espacio y devolver la cadena HTML
-                    return '<div class="d-flex justify-content-start align-items-center">' + buttons.join(' ') + '</div>';
-                }
-            }, // botones
-            {
-                data: 'ORDEN_MEDICA', render: function (data) {
-                    // Inicializar un arreglo vacío para contener nuestros botones
-                    let buttons = [];
-
-                    buttons.push(
-                        '<a href="' + data + '" target="_blank" class="btn btn-borrar me-2">' +
-                        '<i class="bi bi-file-earmark-pdf-fill"></i> Orden Médica Cargada' +
-                        '</a>'
-                    );
-
-                    // Unir todos los botones con un espacio y devolver la cadena HTML
-                    return '<div class="d-flex justify-content-start align-items-center">' + buttons.join(' ') + '</div>';
-                }
-            }, // boton
             { data: 'COMENTARIO_ORDEN_MEDICA' }, // texto
 
         ],
@@ -150,10 +90,7 @@
             { "targets": 8, "className": "none", "title": "Quién registro" }, // USUARIO
             { "targets": 9, "className": "all", "title": "Muestra" }, // Muestra (Color rojo sin tomar muestra - colocar fecha o Capturar muestra (modal))
             { "targets": 10, "className": "none", "title": "Muestra Tomado por" }, // ¿quien tomo la muestra?
-            { "targets": 11, "className": "none", "title": "Etiquetas" },
-            { "targets": 12, "className": "all", "title": "Resultado" }, // Resultados (Clinico y Biomolecular [PDF])
-            { "targets": 13, "className": "none", "title": "Orden médica" }, // Orden médica (boton)
-            { "targets": 14, "className": "none", "title": "Comentarios" } // Comentarios de la orden médica
+            { "targets": 11, "className": "none", "title": "Comentarios" } // Comentarios de la orden médica
         ]
     })
 
