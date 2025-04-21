7051f8535a4a085972a20f851227c5b6314aa90d
Josue de la Cruz <arellanox0392@gmail.com>
2025-04-21 12:58:01 -0600

    Update recepcion-aceptados-tabla.js

--- vista/menu/recepcion/contenido/js/recepcion-aceptados-tabla.js
+++ vista/menu/recepcion/contenido/js/recepcion-aceptados-tabla.js
@@ -399,7 +399,7 @@ tablaRecepcionPacientesIngrersados = $('#TablaRecepcionPacientes-Ingresados').Da
         if(array_selected){
           alertMensajeConfirm(
             {
-              title: '¿No ha pagado este Wey?',
+              title: '¿No ha pagado?',
               text: 'Podrás cerrar el corte al marcarlo como pendiente.',
               icon: 'info'
             },
