7d87ac2892747da3b09d4ed7b318e6b3a19eb3e0
JavierMNew <javiernew638@gmail.com>
2025-06-04 12:16:12 -0600

    nuevo permiso agregado

--- vista/menu/inventarios/index.php
+++ vista/menu/inventarios/index.php
@@ -19,12 +19,13 @@ $menu = "Inventarios";
     var supr = <?php echo empty($_SESSION['permisos']['invEliArt']) ? 0 : $_SESSION['permisos']['invEliArt']; ?>;
     //preguntar si sirve
     var editEntradas = <?php echo empty($_SESSION['permisos']['invRegEnt']) ? 0 : $_SESSION['permisos']['invRegEnt']; ?>;
+    var invVerTrans = <?php echo (isset($_SESSION['permisos']['invVerTrans']) && $_SESSION['permisos']['invVerTrans'] == 1) ? 1 : 0; ?>;
 
     var userPermissions = {
-        canEdit: edit == '1' ? true : false,
-        canDelete: supr == '1' ? true : false,
-        //preguntar si sirve
-        canEditEntradas: editEntradas == '1' ? true : false
+        canEdit: edit === 1,
+        canDelete: supr === 1,
+        canEditEntradas: editEntradas === 1,
+        canViewTransactions: invVerTrans === 1
     }
 
     vista('<?php echo $menu; ?>', '<?php echo $https . $url . '/' . $appname . '/vista/menu/controlador/controlador.php'; ?>')
