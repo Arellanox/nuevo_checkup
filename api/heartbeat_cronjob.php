5e877ef5079342a2ca97e1f28b5bcd1f2db7701c
Josue de la Cruz <arellanox0392@gmail.com>
2025-09-19 14:13:27 -0600

    Update heartbeat_cronjob.php

--- api/heartbeat_cronjob.php
+++ api/heartbeat_cronjob.php
@@ -2,16 +2,19 @@
 include_once "../clases/master_class.php";
 include_once "../clases/correo_class.php";
 
+$master = new Master();
+
 $limit = 40; // minutos sin señal antes de marcar offline
 
 $response = $master->getByProcedure("sp_heartbeats_b",[]);
 
+echo "Fecha y hora local: " . date("Y-m-d H:i:s", time()) . "\n";
 
 foreach ($response as $s) {
     $diff = (time() - strtotime($s["last_signal"])) / 60;
     if ($diff > $limit) {
         // Aquí puedes mandar un correo o alerta
-        echo "⚠️ Servidor {$s["server_id"]} está offline (última señal hace $diff min)\n";
+        echo "⚠️ Servidor {$s["server_id"]} está offline (última señal hace $diff min)\n<br>";
     }    
 }    
 
