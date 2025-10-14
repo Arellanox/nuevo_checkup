8268a4a4222619c55a2c1eb23eaed8e4314f84c6
Ángel Saúl P. C. <pascualcamacho2606@gmail.com>
2025-10-14 11:27:36 -0600

    Update reportes.js

--- vista/menu/maquilas/reportes-asociados/contenido/js/reportes.js
+++ vista/menu/maquilas/reportes-asociados/contenido/js/reportes.js
@@ -76,13 +76,6 @@ let tablaPrincipal = $('#TablaReporteMaquilas').DataTable({
                 }, 'laboratorio_solicitud_maquila_api', { callbackAfter: true }, false, function (data) {
                     let url = data.response.data.url;
 
-                    // Verificar si estás en localhost y si la URL contiene "bimo-lab.com"
-                    if (servidor === 'localhost' && url.includes('bimo-lab.com')) {
-                        // Reemplazar "https://bimo-lab.com" por el origen actual (http://localhost)
-                        const localOrigin = window.location.origin;
-                        url = url.replace(/^https?:\/\/bimo-lab\.com/, localOrigin);
-                    }
-
                     const link = document.createElement('a');
                     link.href = url;
                     link.target = '_blank';
@@ -103,7 +96,14 @@ let tablaPrincipal = $('#TablaReporteMaquilas').DataTable({
                     SERVICIO_ID: null
                 }, 'laboratorio_solicitud_maquila_api', { callbackAfter: true }, false, function (data) {
                     let url = data.response.data.url;
-                    
+
+                    // Verificar si estás en localhost y si la URL contiene "bimo-lab.com"
+                    if (servidor === 'localhost' && url.includes('bimo-lab.com')) {
+                        // Reemplazar "https://bimo-lab.com" por el origen actual (http://localhost)
+                        const localOrigin = window.location.origin;
+                        url = url.replace(/^https?:\/\/bimo-lab\.com/);
+                    }
+
                     const link = document.createElement('a');
                     link.href = url;
                     link.target = '_blank';
