997a65d5620b190ce23ef9acf614e52fb2930751
Josue de la Cruz <arellanox0392@gmail.com>
2025-12-11 13:33:56 -0600

    Update promociones_api.php

--- api/promociones_api.php
+++ api/promociones_api.php
@@ -1,4 +1,9 @@
 <?php
+// --- CORS ---
+header("Access-Control-Allow-Origin: https://bimo.com.mx");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 include_once "../clases/master_class.php";
 require_once "../clases/token_auth.php";
 
