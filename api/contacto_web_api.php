f57f2ca928b0301ebd191e79db82cafbe8025046
Josue de la Cruz <arellanox0392@gmail.com>
2025-12-18 10:04:10 -0600

    Update contacto_web_api.php

--- api/contacto_web_api.php
+++ api/contacto_web_api.php
@@ -8,7 +8,7 @@ include "../clases/correo_class.php";
  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     // Solicitudes preflight OPTIONS
     header('Access-Control-Allow-Origin: http://localhost:3000');
-    header('Access-Control-Allow-Origin: https://bimo.com.mx');
+    header('Access-Control-Allow-Origin: https://www.bimo.com.mx');
     header('Access-Control-Allow-Methods: POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
     header('Access-Control-Max-Age: 86400'); // 1 día
@@ -18,7 +18,7 @@ include "../clases/correo_class.php";
 
 // Para otras solicitudes, como POST
 header('Access-Control-Allow-Origin: http://localhost:3000');
-header('Access-Control-Allow-Origin: https://bimo.com.mx');
+header('Access-Control-Allow-Origin: https://www.bimo.com.mx');
 header('Access-Control-Allow-Methods: POST');
 header('Content-Type: application/json');
  
