5406fc1c557a964753d603532b0bb2732eda1e88
Josue de la Cruz <arellanox0392@gmail.com>
2026-06-10 01:03:11 -0600

    Create cuestionario_nordico_api.php

---
+++ api/cuestionario_nordico_api.php
@@ -0,0 +1,17 @@
+<?php
+require_once "../clases/master_class.php";
+require_once "../clases/token_auth.php";
+
+$tokenVerification = new TokenVerificacion();
+$tokenValido = $tokenVerification->verificar();
+
+$master = new Master();
+$api = $_POST['api'];
+
+switch($api){
+    case 1:
+        $response
+        break;
+}
+
+echo $master->returnApi($response);
